9e3cf3f7ae98fcf8d9157c33190a724f9f633562
Stefano Carrazza <stefano.carrazza@mi.infn.it>
2015-11-07 22:27:02 +0100

    updating derivative expression

--- nnpdfcpp/tools/nndiff/benchmark/nndiff.h
+++ nnpdfcpp/tools/nndiff/benchmark/nndiff.h
@@ -5,7 +5,5 @@ using std::vector;
 
 real nnval(real x_00, vector<real> const& params);
 
-real nndiff(real x_00, vector<real> const& params);
-
-real NNPDFdev(real const& x, vector<real> const& params, 
+real NNPDFdev(real const& x_00, vector<real> const& params, 
 	      real const& a, real const& b, real const& n);
