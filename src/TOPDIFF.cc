aac82e9d4a353375377903c48f1ef6bcb3514fff
Juan Rojo <juan.rojo@cern.ch>
2015-11-26 13:39:17 +0000

    Added TOPDIFF .cc and .h files, for the implementation in the BuildMaster of the ATLAS and CMS ttbar differential distributions at 8 TeV

---
+++ src/TOPDIFF.cc
@@ -0,0 +1,76 @@
+/**
+ 
+This file describes the buildmaster implementation of the
+differential distributions for top quark pair production at 8 TeV
+from ATLAS and CMS
+
+ATLAS => http://arxiv.org/abs/1511.04716
+CMS => http://arxiv.org/abs/1505.04480
+
+There are two different ways to include this data, with
+normalized or with absolute distributions
+
+By modifying this file, one can select the normalized or the absolute
+distributions
+
+*/
+
+#include "TOPDIFF.h"
+
+void TTBARTOTFilter::ReadData()
+{
+
+  // PDG average of the top quark pole mass
+  double const mt = 173.3;
+  
+  // Opening files
+  fstream f1;
+  
+  stringstream datafile("");
+  datafile << dataPath() << "rawdata/"
+	   << fSetName << "/ttbar_totxsec.data";
+  f1.open(datafile.str().c_str(), ios::in);
+  if (f1.fail()) {
+    cerr << "Error opening data file " << datafile.str() << endl;
+      exit(-1);
+  }
+  string line;
+  for (int i = 0; i < fNData; i++)
+    {
+      getline(f1,line);
+      istringstream lstream(line);
+      
+      fKin1[i] = 0.0;
+      fKin2[i] = mt;
+      fKin3[i] = 0.0;
+      
+      int idum=0;
+      lstream >> idum; 
+      if(idum!=(i+1)){
+	cout<<"Error in TOP.cc"<<endl;
+	cout<<"idum = "<<idum<<endl;
+	exit(-10);
+      }
+      
+      lstream >> fData[i]; // Central values
+      lstream >> fStat[i]; // Statistical error
+      lstream >> fSys[i][0].add; // Total systematic uncertainties      
+      lstream >> fSys[i][1].add;  // Lumi
+      lstream >> fSys[i][2].add; // Beam energy uncertainty 
+
+      fSys[i][0].mult = fSys[i][0].add*100/fData[i];
+      fSys[i][0].type = MULT;
+      fSys[i][0].name = "UNCORR";  // for time being treat as uncorrelated
+      
+      fSys[i][1].mult = fSys[i][1].add*100/fData[i];
+      fSys[i][1].type = MULT;
+      fSys[i][1].name = "UNCORR";  // for time being treat as uncorrelated 
+      
+      fSys[i][2].mult = fSys[i][2].add*100/fData[i];
+      fSys[i][2].type = MULT;
+      fSys[i][2].name = "UNCORR";  // for time being treat as uncorrelated   
+    }
+  
+  f1.close();
+}
+
