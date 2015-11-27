92599b79cff91cde4f04d87207ec0f5c1381cf22
Nathan Hartland <nathan.hartland@physics.ox.ac.uk>
2015-11-27 14:32:31 +0000

    removed old stuff

--- src/buildmaster_utils.h
+++ src/buildmaster_utils.h
@@ -27,16 +27,3 @@ void symmetriseErrors(double right, double left, double* sigma, double* delta);
 
 // Generate artificial systematics
 bool genArtSys(int ndata, const double* const* cov, double** artsys);
-
-// Mass number for proton-type data
-const size_t A_ONEPROTON[1] = {1};   // Mass number of contributing FK tables - one FK table
-const size_t A_TWOPROTON[2] = {1,1}; // Mass number of contributing FK tables - two FK tables
-
-
-// Mass number for deuteron-type data
-const size_t A_ONEDEUTERON[1] = {2};   // Mass number of contributing FK tables - one FK table
-const size_t A_TWODEUTERON[2] = {2,2}; // Mass number of contributing FK tables - two FK tables
-
-// Mass number fo proton-deuteron ratio
-const size_t A_PROTONDEUTERON[2] = {1,2};
-const size_t A_DEUTERONPROTON[2] = {2,1};
