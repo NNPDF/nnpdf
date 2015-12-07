3a7b407375026710fbfad51ae962ffa5d02500af
Alberto Guffanti <alberto.guffanti@nbi.dk>
2015-12-07 13:03:01 +0100

    Tentative fix for LHCBZEE2FB.

--- filters/LHCb.cc
+++ filters/LHCb.cc
@@ -561,7 +561,7 @@ void LHCBWMU1FBFilter::ReadData()
  * Z production > e+e- data from the LHCb experiment
  *                8 TeV data
  * Final data from the LHCb preprint: 1503.00963
- * Luminosity uncertainty is a 1.2% in all data points and is quoted separately 
+ * Luminosity uncertainty is a 1.2% in all data points and is quoted separately
  * from other sources of systematic uncertainty
  */
 
@@ -569,12 +569,12 @@ void LHCBZEE2FBFilter::ReadData()
 {
   // Opening files
   fstream f1, f2;
-  
+
   stringstream datafile1("");
   datafile1 << dataPath() << "rawdata/"
   << fSetName << "/lhcb_zrap.data";
   f1.open(datafile1.str().c_str(), ios::in);
-  
+
   if (f1.fail()) {
     cerr << "Error opening data file " << datafile1.str() << endl;
     exit(-1);
@@ -584,19 +584,19 @@ void LHCBZEE2FBFilter::ReadData()
   datafile2 << dataPath() << "rawdata/"
   << fSetName << "/lhcb_zrap.corr";
   f2.open(datafile2.str().c_str(), ios::in);
-  
+
   if (f2.fail()) {
     cerr << "Error opening data file " << datafile2.str() << endl;
     exit(-1);
   }
 
- 
+
   // Initialize
   const int ndata_z = 17;
   const double convfac = 1000.; // Must multiply from pb to fb
   double MZ2 = pow(MZ,2.0);
   double s = 8000.;
-  
+
   double etamin,etamax;
   double sys_corr[fNData],lumi[fNData],tot_unc[fNData];
   double inmat[fNData][fNData];
@@ -605,8 +605,8 @@ void LHCBZEE2FBFilter::ReadData()
   for (int i = 0; i < ndata_z; i++)
     {
       getline(f1,line);
-      istringstream lstream(line);   
-      
+      istringstream lstream(line);
+
       lstream >> etamin >> etamax;
       fKin1[i] = (etamin+etamax) * 0.5;       // <eta>
       fKin2[i] = MZ2;
@@ -617,22 +617,22 @@ void LHCBZEE2FBFilter::ReadData()
       // stat uncertainty
       lstream >> fStat[i];
       fStat[i] *= convfac;
-      
+
       // Uncorrelated systematics
       lstream >> fSys[i][0].add;
       fSys[i][0].add *= convfac;
       fSys[i][0].mult = fSys[i][0].add*100/fData[i];
       fSys[i][0].type = ADD;
       fSys[i][0].name = "UNCORR";
-      
+
       // Correlated and luminosity uncertainties
       lstream >> sys_corr[i] >> lumi[i];
-      sys_corr[i] *= convfac; 
+      sys_corr[i] *= convfac;
       lumi[i] *= convfac;
 
       // Total uncertainty
       tot_unc[i] = pow(sys_corr[i]*sys_corr[i]+fSys[i][0].add*fSys[i][0].add+fStat[i]*fStat[i],0.5);
-      
+
       // Normalization uncertainty is defined both MULT (in %) and ADD (in abs value)
       fSys[i][1].mult = lumi[i];
       fSys[i][1].add = fSys[i][1].mult*fData[i]*1e-2;
@@ -647,23 +647,23 @@ void LHCBZEE2FBFilter::ReadData()
   for (int i = 0; i < ndata_z; i++)
     for (int j = i+1; j<ndata_z; j++)
       inmat[i][j] = inmat[j][i];               // symmetrize
-  
+
   //  Multiply by total systematic uncertainty
   double** covmat = new double*[fNData];
   for(int i = 0; i < fNData; i++)
   {
     covmat[i] = new double[fNData];
     for(int j = 0; j < fNData; j++)
-      // Not sure whether is tot correlated  or total systematics 
-      //      covmat[i][j]=inmat[i][j]*sys_corr[i]*sys_corr[j]; 
-      covmat[i][j]=inmat[i][j]*tot_unc[i]*tot_unc[j]; 
+      // Not sure whether is tot correlated  or total systematics
+      covmat[i][j]=inmat[i][j]*sys_corr[i]*sys_corr[j]; 
+//      covmat[i][j]=inmat[i][j]*tot_unc[i]*tot_unc[j];
   }
 
   // Now generate artificial systematics
   double** syscor = new double*[fNData];
   for(int i = 0; i < fNData; i++)
     syscor[i] = new double[fNData];
-    
+
   if(!genArtSys(fNData,covmat,syscor))
    {
      cerr << " in " << fSetName << endl;
