3e300d9710b3eeae826b6071d4b04b31019d762b
Stefano Carrazza <stefano.carrazza@mi.infn.it>
2015-12-16 14:51:04 +0100

    updating __init__ includes

--- libnnpdf/wrapper/src/__init__.py
+++ libnnpdf/wrapper/src/__init__.py
@@ -2,3 +2,4 @@ from NNPDF.lhapdfset import LHAPDFSet
 from NNPDF.pdfset import PDFSet
 from NNPDF.fastkernel import FKTable
 from NNPDF.thpredictions import ThPredictions
+from NNPDF.commondata import CommonData
