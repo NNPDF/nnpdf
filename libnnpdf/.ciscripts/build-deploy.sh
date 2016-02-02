720f195c8c36707a079e7ac6afcfca548e11ff63
Zahari <zaharid@gmail.com>
2016-02-02 18:44:30 +0100

    Tentative implementation

--- libnnpdf/.ciscripts/build-deploy.sh
+++ libnnpdf/.ciscripts/build-deploy.sh
@@ -8,8 +8,17 @@ if [ $? != 0 ]; then
 	exit 1
 fi
 
+#This seems to be needed for "artifacts" to work.
 cp /yes/conda-bld/linux-64/*.tar.bz2 .
 
+if [ "$CI_BUILD_REF_NAME" != 'master'  ] && ![ "$UPLOAD_NON_MASTER" ]; 
+then
+  	echo "Skiping upload because this is not master and you have not
+	set the UPLOAD_NON_MASTER variable."
+	exit 0
+fi
+
+echo "Uploading package to zigzah"
 KEY=$( mktemp )
 #This is defined in the Gitlab variables, under the Settings Menu.
 echo "$ZIGZAH_SSH_KEY" > "$KEY"
@@ -17,3 +26,8 @@ echo "$ZIGZAH_SSH_KEY" > "$KEY"
 scp -i "$KEY" -o StrictHostKeyChecking=no\
     /yes/conda-bld/linux-64/*.tar.bz2 \
     dummy@zigzah.com:~/conda-pkgs-private/linux-64 
+
+if [ $? == 0 ]; then
+	echo "Upload suceeded"
+else
+	echo "Upload failed"
