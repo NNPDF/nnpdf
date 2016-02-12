714d580c41fcc2b01b40c78ed8906e861fc9bde8
Zahari <zaharid@gmail.com>
2016-02-12 15:09:51 +0100

    Remove trailing spaces

--- validphys2/core.py
+++ validphys2/core.py
@@ -38,11 +38,11 @@ class Environment:
 
 
 class Config(configparser.Config):
-    
+
     @property
     def loadder(self):
         return self.environment.loader
-    
+
     def check_pdf(self, name:str):
         if lhaindex.isinstalled(name):
             try:
@@ -50,17 +50,17 @@ class Config(configparser.Config):
             except NotImplementedError:
                 raise ConfigError(str(e))
             return name
-        raise ConfigError("Bad PDF: {} not installed".format(name), name, 
+        raise ConfigError("Bad PDF: {} not installed".format(name), name,
                           lhaindex.expand_local_names('*'))
-    
+
     def check_theoryID(self, theoryID: (str, int)):
         try:
             return (str(theoryID), self.loadder.check_theoryID(theoryID))
         except FileNotFoundError as e:
-            raise ConfigError(str(e), theoryID, 
-                              self.loadder.available_theories, 
+            raise ConfigError(str(e), theoryID,
+                              self.loadder.available_theories,
                               display_alternatives='all')
-    
+
     #TODO:enable this
     #def check_dataset(self, * , name:str, sysnum:(str, int):
     def check_dataset(self, dataset:dict):
@@ -69,7 +69,7 @@ class Config(configparser.Config):
         except KeyError:
             raise ConfigError("'dataset' must be a mapping with "
                               "'name' and 'sysnum'")
-            
+
         try:
             return name, self.loadder.check_commondata(name, sysnum)
         except DataNotFoundError as e:
@@ -84,12 +84,12 @@ class SysNotFoundError(FileNotFoundError): pass
 
 class Loader():
     """Load various resources from the NNPDF dapa path."""
-    
+
     def __init__(self, datapath):
         if isinstance(datapath, str):
             datapath = pathlib.Path(datapath)
         self.datapath = datapath
-    
+
     @property
     @functools.lru_cache()
     def available_theories(self):
@@ -97,35 +97,35 @@ class Loader():
         theory_token  = 'theory_'
         return {folder.name[len(theory_token):]
                 for folder in self.datapath.glob(theory_token+'*')}
-    
+
     @property
     @functools.lru_cache()
     def available_datasets(self):
-        
+
         data_str = "DATA_"
-        return {file.stem[len(data_str):] for 
+        return {file.stem[len(data_str):] for
                 file in self.commondata_folder.glob(data_str+"*.dat")}
-    
+
     @property
     def commondata_folder(self):
         return self.datapath / 'commondata'
-    
+
     def check_commondata(self, setname, sysnum):
         datafile = self.commondata_folder / ('DATA_' + setname + '.dat')
         if not datafile.exists():
             raise DataNotFoundError(("Could not find Commondata set: '%s'. "
                   "File '%s' does not exist.")
                  % (setname, datafile))
-        sysfile = (self.datapath / 'commondata' / 'systypes' / 
+        sysfile = (self.datapath / 'commondata' / 'systypes' /
                    ('SYSTYPE_%s_%d.dat' % (setname, sysnum)))
-        
+
         if not sysfile.exists():
             raise SysNotFoundError(("Could not find systype %d for "
-                 "dataset '%s'. File %s does not exist.") % (sysnum, setname, 
+                 "dataset '%s'. File %s does not exist.") % (sysnum, setname,
                   sysfile))
-        
+
         return datafile, sysfile
-    
+
     def check_theoryID(self, theoryID):
         theoryID = str(theoryID)
         theopath = self.datapath / ('theory_%s' % theoryID)
@@ -133,20 +133,20 @@ class Loader():
             raise FileNotFoundError(("Could not find theory %s. "
                   "Folder '%s' not found") % (theoryID, theopath) )
         return theopath
-    
+
     def get_commondata(self, setname, sysnum):
         """Get a Commondata from the set name and number"""
         datafile, sysfile = self.check_commondata(setname, sysnum)
         return CommonData.ReadFile(str(datafile), str(sysfile))
-    
+
     def check_fktable(self, theoryID, setname):
         theopath = self.check_theoryID(theoryID)
         fkpath = theopath/ 'fastkernel' / ('FK_%s.dat' % setname)
         if not fkpath.exists():
-          raise FileNotFoundError(("Could not find FKTable for set '%s'. " 
-          "File '%s' not found") % (setname, fkpath) ) 
+          raise FileNotFoundError(("Could not find FKTable for set '%s'. "
+          "File '%s' not found") % (setname, fkpath) )
         return fkpath
-    
+
     def get_fktable(self, theoryID, setname):
 
         fkpath = self.check_fktable(theoryID, setname)
@@ -170,7 +170,7 @@ def get_error_type(pdfname):
                                           " interval %s" % (cl,) )
         else:
             return LHAPDFSet.ER_EIG
-    
+
     if error == "symmhessian":
         if 'ErrorConfLevel' in info:
             cl = info['ErrorConfLevel']
@@ -182,35 +182,35 @@ def get_error_type(pdfname):
                                           " interval %s" % (cl,) )
         else:
             return LHAPDFSet.ER_EIG
-    
-    raise NotImplementedError("Error type for %s: '%s' is not implemented" % 
+
+    raise NotImplementedError("Error type for %s: '%s' is not implemented" %
                               (pdfname, error))
-            
-                
+
+
 
 def load_pdf(name):
     """Return a LHAPDFSet with the correct errro type"""
     err_type = get_error_type(name)
     return LHAPDFSet(name, err_type)
-    
-    
+
+
 #==============================================================================
-#     
+#
 # def load_fkset(dataset, theoryID):
-#     
+#
 #     return FKSet(FKSet.parseOperator("NULL"), [fktable])
-# 
+#
 # def get_dataset(commondata, fktable):
 #     fkset = fktable_to_fkset(fktable)
 #     return DataSet(commondata, fkset)
-# 
-# 
-# 
+#
+#
+#
 # def make_results(loader, setname, theoryid, pdfname):
 #     """High level functions that produces results by parsing the config"""
 #     fktable = loader.get_fktable(theoryid, setname)
 #==============================================================================
-    
+
 def make_results(dataset, theoryID, pdf):
     setname, (cdpath, syspth) = dataset
     cd = CommonData.ReadFile(str(cdpath), str(syspth))
@@ -225,22 +225,22 @@ def make_results(dataset, theoryID, pdf):
     data = DataSet(cd, fkset)
     pdf = load_pdf(pdf)
     th_predictions = ThPredictions(pdf, data)
-    
+
     return DataResult(data), ThPredictionsResult(thlabel, th_predictions)
 
 def plot_results(results, setlabel, normalize_to = None):
-    
+
     cvs = [r.central_value for r in results]
     errors = [r.std_error for r in results]
-    
+
     y_label = "Observable value"
-    
+
     if normalize_to is not None:
         y_label = "Ratio to %s" % normalize_to.label
         norm_cv = normalize_to.central_value
         cvs = [cv/norm_cv for cv in cvs]
         errors = [e/norm_cv for e in errors]
-    
+
     l = len(results)
     if l < 5:
         delta = iter(np.linspace(-0.05*l, 0.05*l, l))
@@ -248,21 +248,21 @@ def plot_results(results, setlabel, normalize_to = None):
         delta = iter(np.linspace(-0.25, 0.25, l))
 
     figure, ax = plt.subplots()
-    
+
     ax.set_title(setlabel)
-    ax.set_ylabel(y_label)    
-    
+    ax.set_ylabel(y_label)
+
     for result, cv, error in zip(results, cvs, errors):
         x = np.arange(1, len(result) + 1) + next(delta)
         ax.errorbar(x, cv, yerr=error,
                          linestyle='none',
                          label=result.label, elinewidth = 2,
                          capsize=10)
-    
+
     ax.set_xticks(range(1,len(result.central_value) + 1))
 
     ax.grid(axis='x')
-    
+
     #TODO: Abtract this out
     dfs = ax.get_yticks() - 1
     l = len(dfs) // 2  + 1 - ((len(dfs) // 2) % 2)
@@ -272,67 +272,67 @@ def plot_results(results, setlabel, normalize_to = None):
 
 
     ax.legend()
-    
+
     return figure
 
 
 
 class DataResult:
-    
+
     def __init__(self, dataobj):
         self.dataobj = dataobj
-    
+
     @property
     def label(self):
         return "CommonData"
-    
+
     @property
     @functools.lru_cache()
     def central_value(self):
         return self.dataobj.get_cv()
-        
+
     @property
     @functools.lru_cache()
     def std_error(self):
         return np.sqrt(np.diag(self.dataobj.get_covmat()))
-    
+
     def __len__(self):
         return len(self.dataobj)
-    
+
     def __getattr__(self, attr):
         return getattr(self.dataobj, attr)
 
 
 class ThPredictionsResult(DataResult):
-    
+
     def __init__(self, thlabel, dataobj):
         self.thlabel = thlabel
         super().__init__(dataobj)
-    
+
     @property
     @functools.lru_cache()
     def std_error(self):
         return self.dataobj.get_error()
-    
+
     @property
     def label(self):
         return "<Theory %s>: %s" % (self.thlabel, self.dataobj.GetPDFName())
-        
-    
+
+
 if __name__ == '__main__':
     #TODO: Oberhaul this to use reportengine properly
 
     parser = argparse.ArgumentParser(
              description = "Validphys developer preview",
              )
-    
+
     parser.add_argument('config_yml',
                         help = "path to the configuration file")
-    
+
     parser.add_argument('-o','--output', help="output folder where to "
                                          "store resulting plots and tables",
                         default='output')
-    
+
     loglevel = parser.add_mutually_exclusive_group()
 
     loglevel.add_argument('-q','--quiet', help="Supress INFO messages",
@@ -340,7 +340,7 @@ if __name__ == '__main__':
 
     loglevel.add_argument('-d', '--debug', help = "Show debug info",
                           action='store_true')
-    
+
     parser.add_argument('-p','--datapath', help="Path where the NNPDF "
                         "data is located",
                         default='../nnpdfcpp/data')
@@ -357,9 +357,9 @@ if __name__ == '__main__':
     logging.basicConfig(format='%(levelname)s: %(message)s', level=level)
 
 
-    
-    environment = Environment(data_path = args.datapath, 
-                              output_path = args.output, 
+
+    environment = Environment(data_path = args.datapath,
+                              output_path = args.output,
                               this_folder = pathlib.Path(__file__).parent
                               )
     with open(args.config_yml) as f:
@@ -371,15 +371,15 @@ if __name__ == '__main__':
             print(e.alternatives_text())
             sys.exit(1)
     results = (dataresult, theoresult)= make_results(**c.params)
-    
+
     plt.style.use(str(environment.this_folder / 'small.mplstyle'))
     fig = plot_results(results, setlabel=c['dataset'][0],
                        normalize_to=dataresult)
-    
-    environment.output_path.mkdir(exist_ok = True)
-    
 
-    
-                       
-    fig.savefig(str(environment.output_path / "result.pdf"), 
+    environment.output_path.mkdir(exist_ok = True)
+
+
+
+
+    fig.savefig(str(environment.output_path / "result.pdf"),
                 bbox_inches='tight')
