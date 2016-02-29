8874ced215a54be786c204907cd580caf5196178
Zahari <zaharid@gmail.com>
2016-02-29 09:30:05 +0100

    Add initial version of conda recipe

---
+++ nnpdfcpp/conda-recipe/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd nnpdfsrc
+autoreconf -i
+./configure --prefix=$PREFIX --enable-pywrap
+#This doesn't wor properly with the debian autotools. 
+#Just use one core
+#make -j${CPU_COUNT}
+make
+make install
