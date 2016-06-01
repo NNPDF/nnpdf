ffdd81fa2df736634a6971cf75506490b4142c1e
Zahari <zaharid@gmail.com>
2016-06-01 15:47:35 +0200

    Require an explicit '_' after the
    We can have dataset names that are substrings of others.

--- validphys2/src/validphys/providers.py
+++ validphys2/src/validphys/providers.py
@@ -7,3 +7,4 @@ Created on Wed Mar  9 15:00:35 2016
 
 from validphys.results import * #analysis:ignore
 from validphys.plots import *   #analysis:ignore
+from validphys.reweighting import *   #analysis:ignore
