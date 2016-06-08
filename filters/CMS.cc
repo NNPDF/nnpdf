a8e2c447e35ad1ee711da13a933cf2d5eab15f38
Stefano Carrazza <stefano.carrazza@cern.ch>
2016-06-08 18:57:53 +0200

    updating systypes for CMS1JETS276TEV

--- filters/CMS.cc
+++ filters/CMS.cc
@@ -610,22 +610,22 @@ void CMS1JET276TEVFilter::ReadData()
 
           // luminosity 1st entry
           fSys[index][0].type = MULT;
-          fSys[index][0].name = "CORR";
+          fSys[index][0].name = "CMS2P76LUMI";
 
           // uncorr uncertainties 2nd entry
-          fSys[index][1].type = ADD;
+          fSys[index][1].type = MULT;
           fSys[index][1].name = "UNCORR";
 
           // add np error
           fSys[index][2].mult = nperr/rescalenp;
-          fSys[index][2].type = ADD;
+          fSys[index][2].type = MULT;
           fSys[index][2].name = "CORR";
 
           // filling corr uncertainties
           for (int isys = 3; isys < 25; isys++)
             {
               f1 >> fSys[index][isys].mult;
-              fSys[index][isys].type = ADD;
+              fSys[index][isys].type = MULT;
               fSys[index][isys].name = "CORR";
             }
 
@@ -666,12 +666,12 @@ void CMS1JET276TEVFilter::ReadData()
           index_bis++;
         }
 
-
       for (int ipt = 0; ipt < bins[iy]; ipt++)
         {
           delete[] statmat[ipt];
           delete[] syscor[ipt];
         }
+
       delete[] statmat;
       delete[] syscor;
 
