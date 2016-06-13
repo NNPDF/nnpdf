a2ca56c5d81f8cbecc8a6cd3f1c2a6751c20e0d6
Zahari <zaharid@gmail.com>
2016-06-13 15:37:44 +0200

    Add support for building with --enable-safemode

--- libnnpdf/conda-recipe/build.sh
+++ libnnpdf/conda-recipe/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 autoreconf -i
-./configure --prefix=$PREFIX --enable-pywrap
+USE_REAL=${NNPDF_USE_DOUBLE+"--enable-safemode"}
+./configure --prefix=$PREFIX --enable-pywrap ${USE_REAL}
 #This doesn't wor properly with the debian autotools. 
 #Just use one core
 #make -j${CPU_COUNT}
