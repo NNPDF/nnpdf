24a16dc48bddd14e5c0e9c452f1ff478c52c30fd
Zahari <zaharid@gmail.com>
2016-07-05 12:11:16 +0200

    Update ATLASWZPTTOT.....
    Take the data from the v2 of the paper.

--- inc/ATLASWZTOT13TEV81PB.h
+++ inc/ATLASWZTOT13TEV81PB.h
@@ -1,31 +1,9 @@
 #pragma once
-// $Id
-//
-// NNPDF++ 2012
-//
-// Authors: Nathan Hartland,  n.p.hartland@ed.ac.uk
-//          Stefano Carrazza, stefano.carrazza@mi.infn.it
-//          Luigi Del Debbio, luigi.del.debbio@ed.ac.uk
-
-
 /*
- * ATLAS W and Z total cross section measurements. The data is mnually
- * copied from http://arxiv.org/pdf/1603.09222v1.pdf. Values in 
- * data.txt are taken from Table 1, second header row "Measured cross
- * section x BR (W->lv, Z->ll) [nb], "Fiducial".
- *
- * This stub is taken from:
- *  TBARTOT
- *  
- * 
+ * See the rawdata folder for details. 
 */
 
-
-
-
-
 #include "buildmaster_utils.h"
-#include <map>
 
 static const dataInfoRaw ATLASWZTOT13TEV81PBinfo = {
   3,          //nData
