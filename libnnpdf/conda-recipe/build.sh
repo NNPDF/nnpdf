710ed52a3a4e3f598892027a3aee872ca97f48c4
Zahari <zaharid@gmail.com>
2016-12-18 19:23:45 +0100

    Remove the safemode stuff

--- libnnpdf/conda-recipe/build.sh
+++ libnnpdf/conda-recipe/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ./autogen.sh
-USE_REAL=${NNPDF_USE_DOUBLE+"--enable-safemode"}
-./configure --prefix=$PREFIX --enable-pywrap ${USE_REAL}
-#This doesn't wor properly with the debian autotools. 
+#USE_REAL=${NNPDF_USE_DOUBLE+"--enable-safemode"}
+./configure --prefix=$PREFIX --enable-pywrap #${USE_REAL}
+#This doesn't wor properly with the debian autotools.
 #Just use one core
 #make -j${CPU_COUNT}
 make
