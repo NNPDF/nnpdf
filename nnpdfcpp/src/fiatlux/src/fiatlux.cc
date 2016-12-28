ca49ca24b36de064a2c4f03fc074c1b47b2f4b7a
Stefano Carrazza <stefano.carrazza@cern.ch>
2016-12-28 16:25:33 +0100

    updating fiatlux code

--- nnpdfcpp/src/fiatlux/src/fiatlux.cc
+++ nnpdfcpp/src/fiatlux/src/fiatlux.cc
@@ -22,8 +22,73 @@ double APFELF2(double const& x, double const& Q)
 }
 
 double APFELFL(double const& x, double const& Q)
-{
+{    
   return APFEL::StructureFunctionxQ("EM", "FL", "total", x, Q);
+}
+
+class lux
+{
+public:
+  lux()
+  {
+    // FiatLux setup
+    _lux = new FiatLux{configPath() + "fiatlux.yml"};
+    _lux->PlugAlphaQED(APFEL::AlphaQED);
+    _lux->PlugStructureFunctions(APFELF2, APFELFL);
+  }
+
+  ~lux()
+  {
+    delete _lux;
+    delete _pdf;
+  }
+
+  void loadPDF(string const& pdfname, int const& replica)
+  {
+    _pdf = LHAPDF::mkPDF(pdfname, replica);
+    const LHAPDF::GridPDF& pdf = *dynamic_cast<const LHAPDF::GridPDF*>(_pdf);
+    _xgrid = pdf.xKnots();
+  }
+
+  FiatLux const& getLux() const { return *_lux; }
+  vector<double> const& getXgrid() const { return _xgrid; }
+
+  double xfxQ(int const& id, double const& x, double const& Q) const
+  {
+    if (id != 22)
+      return _pdf->xfxQ(id, x, Q);
+    else
+      {
+        cout << setprecision(15) << scientific;
+        const auto pht = _lux->EvaluatePhoton(x, Q*Q);
+        cout << x << "\t"
+             << Q << "\t"
+             << pht.elastic << "\t"
+             << pht.inelastic_pf << "\t"
+             << pht.msbar_pf << "\t"
+             << pht.total << "\t"
+             << endl;
+        return pht.total;
+      }
+  }
+
+private:
+  FiatLux* _lux;
+  LHAPDF::PDF* _pdf;
+  vector<double> _xgrid;
+};
+
+lux& luxInstance()
+{
+  static lux l;
+  return l;
+}
+
+extern "C" void externalsetapfel_(const double& x, const double& Q, double *xf)
+{
+  for (int i = 0; i < 13; i++)
+    xf[i] = luxInstance().xfxQ(i-6, x, Q);
+  xf[13] = luxInstance().xfxQ(22, x, Q);
 }
 
 int main(int argc, char **argv)
@@ -50,24 +115,15 @@ int main(int argc, char **argv)
   // Creates the configuration class
   NNPDFSettings settings(configPath() + filename);
   settings.PrintConfiguration("fiatlux.yml");
-
-  // FiatLux setup
-  FiatLux lux{configPath() + "fiatlux.yml"};
-  lux.PlugAlphaQED(APFEL::AlphaQED);
-  lux.PlugStructureFunctions(APFELF2, APFELFL);
+  luxInstance().loadPDF(settings.GetPDFName(), replica);
 
   const int nfmax = stoi(settings.GetTheory(APFEL::kMaxNfPdf));
   const double mb = stod(settings.GetTheory(APFEL::kmb));
   const double mt = stod(settings.GetTheory(APFEL::kmt));
   if (nfmax == 5)
-    lux.InsertInelasticSplitQ({mb, 1e100});
+    luxInstance().getLux().InsertInelasticSplitQ({mb, 1e100});
   else if (nfmax == 6)
-    lux.InsertInelasticSplitQ({mb,mt});
-
-  // allocate grid in x
-  const LHAPDF::PDF* basepdf = LHAPDF::mkPDF(settings.GetPDFName());
-  const LHAPDF::GridPDF& pdf = *dynamic_cast<const LHAPDF::GridPDF*>(basepdf);
-  const auto& xgrid = pdf.xKnots();
+    luxInstance().getLux().InsertInelasticSplitQ({mb,mt});
 
   // APFEL setup
   APFEL::SetParam(settings.GetTheoryMap());  
@@ -78,27 +134,21 @@ int main(int argc, char **argv)
   APFEL::SetReplica(replica);
   APFEL::SetQLimits(1, 1e6);
   APFEL::SetQGridParameters(50, 3);
+  APFEL::SetNumberOfGrids(3);
+  APFEL::SetGridParameters(1,10,3,settings.Get("lhagrid", "xmin").as<double>());
+  APFEL::SetGridParameters(2,5,5,settings.Get("lhagrid", "xmed").as<double>());
+  APFEL::SetGridParameters(3,5,5,0.65);
+  APFEL::LockGrids(true);
   APFEL::InitializeAPFEL_DIS();
   APFEL::CacheStructureFunctionsAPFEL(1); 
 
-  // print results
-  const double q2 = pow(100, 2);
-  cout << endl;
-  cout << "x, q2, elastic, inelastic, msbar, total" << endl;
-  cout << setprecision(15) << scientific;
-  for (const auto& x: xgrid)
-    {
-      const auto pht = lux.EvaluatePhoton(x, q2);
-      cout << x << "\t"
-           << q2 << "\t"
-           << pht.elastic << "\t"
-           << pht.inelastic_pf << "\t"
-           << pht.msbar_pf << "\t"
-           << pht.total << "\t"
-           << endl;
-    }
+  cout << "Computing photon..." << endl;
+  const double q0 = 100.0, q = stod(settings.GetTheory(APFEL::kQ0));
+  APFEL::SetPDFSet("external");
+  APFEL::EvolveAPFEL(q0, q);
 
-  delete basepdf;
+  for (auto const& x: luxInstance().getXgrid())
+    cout << "x=" << x << " Q=" << q << " xpht=" << APFEL::xgamma(x) << endl;
 
   return 0;
 }
