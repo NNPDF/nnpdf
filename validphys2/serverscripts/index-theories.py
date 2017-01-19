fbc03a947ecf29ace97d552775ee05e399a0818a
Zahari <zaharid@gmail.com>
2017-01-19 11:26:10 +0100

    Add server scripts
    These are used to generated indexes for fits and theories at the
    moment they are uploaded.

---
+++ validphys2/serverscripts/index-theories.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Generate an index with the existing fits
+"""
+
+import pathlib
+import json
+
+root = '/home/apfelcomb/WEB/commondatatheory/'
+
+glob = '*.tgz'
+
+indexname = 'theorydata.json'
+
+if __name__ == '__main__':
+    p = pathlib.Path(root)
+    files = p.glob(glob)
+    files = [f.name for f in files]
+    with (p/indexname).open('w') as f:
+        json.dump({'files':files}, f, indent=4)
+
