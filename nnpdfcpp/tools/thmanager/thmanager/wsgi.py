4267b64a95f5b728b054f1904ab93ca000f1b13e
APFELComb <apfelcomb@pcteserver.mi.infn.it>
2017-01-25 17:12:36 +0100

    updating wsgi url

--- nnpdfcpp/tools/thmanager/thmanager/wsgi.py
+++ nnpdfcpp/tools/thmanager/thmanager/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "theoryman.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thmanager.settings")
 
 application = get_wsgi_application()
