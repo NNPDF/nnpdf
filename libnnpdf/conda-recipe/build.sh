30d80ef25a2dc67b388ad130cb9b8530cdf8cecf
Zahari <zaharid@gmail.com>
2017-01-31 17:57:02 +0100

    Try to build without AVX

--- libnnpdf/conda-recipe/build.sh
+++ libnnpdf/conda-recipe/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir build
 cd build
-cmake ../ -DENABLE_PYWRAP=on -DCMAKE_INSTALL_PREFIX=${PREFIX}
+cmake ../ -DENABLE_PYWRAP=on -DCMAKE_INSTALL_PREFIX=${PREFIX} -DAVX_FOUND=OFF -DAVX2_FOUND=OFF
 make
 make install
 
