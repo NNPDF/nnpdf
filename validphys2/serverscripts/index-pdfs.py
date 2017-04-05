0b5ba8cf3b64775ed4c16b6e0d577e204a4cfe73
Zahari <zaharid@gmail.com>
2017-04-05 16:02:08 +0200

    Add index for our custom PDFs
    We may want to download PDFs that are not in the fit format but
    instead just an LHAPDF file (like e.g. CMS or Hessian sets).

---
+++ validphys2/serverscripts/index-pdfs.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Generate an index with the existing internal or unpublished PDFS.
+"""
+
+import pathlib
+import json
+
+root = '/home/nnpdf/WEB/pdfs'
+
+glob = '*gz'
+
+indexname = 'pdfdata.json'
+
+if __name__ == '__main__':
+    p = pathlib.Path(root)
+    files = p.glob(glob)
+    files = [f.name for f in files]
+    with (p/indexname).open('w') as f:
+        json.dump({'files':files}, f, separators=(',',':'))
+
