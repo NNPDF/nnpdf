a313ef409f53a186fb283b48c4192d8e566c3bef
Zahari <zaharid@gmail.com>
2017-04-12 11:43:00 +0200

    Add units in kinematic transforms

--- validphys2/src/validphys/plotoptions/kintransforms.py
+++ validphys2/src/validphys/plotoptions/kintransforms.py
@@ -12,20 +12,20 @@ from numpy import sqrt, ceil, nditer
 def dummy_transform(k1,k2,k3):
     return k1*2, k2*3, k3*4
 
-@utils.new_labels('$y$', '$M$ (GeV)', '$\sqrt(s)$')
+@utils.new_labels('$y$', '$M$ (GeV)', r'$\sqrt{s} (GeV)$')
 def dyp_sqrt_scale(k1,k2,k3):
     return k1, sqrt(k2), k3
 
-@utils.new_labels('$|y|$', '$p_T$ (GeV)', '$\sqrt(s)$')
+@utils.new_labels('$|y|$', '$p_T$ (GeV)', r'$\sqrt{s} (GeV)$')
 def jet_sqrt_scale(k1,k2,k3):
     return k1, sqrt(k2), k3
 
-@utils.new_labels('$x$', '$Q$ (GeV)', '$\sqrt{(s)}$')
+@utils.new_labels('$x$', '$Q$ (GeV)', r'$\sqrt{s} (GeV)$')
 def dis_sqrt_scale(k1,k2,k3):
     ecm = sqrt(k2/(k1*k3))
     return k1, sqrt(k2), ceil(ecm)
 
-@utils.new_labels('$x$', '$Q$ (GeV)', '$\sqrt{(s)}$')
+@utils.new_labels('$x$', '$Q$ (GeV)', r'$\sqrt{s} (GeV)$')
 def nmc_process(k1,k2,k3):
     xBins = [0.0045, 0.008, 0.0125, 0.0175,
     0.025, 0.035, 0.05, 0.07, 0.09, 0.11,
