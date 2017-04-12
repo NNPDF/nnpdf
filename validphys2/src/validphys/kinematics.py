e0fd0b96261e341393d8831b163e371b71144e25
Zahari <zaharid@gmail.com>
2017-04-12 11:44:06 +0200

    Improve the kintable functionality
    Show the fitted and the total number of points and display the limits
    for all three kinematics, instead of guessing heuristically based on
    the x-axis kinematic.
    
    Also allow to supress the kinematic override in the PLOTTING files if
    that's not desired.

--- validphys2/src/validphys/kinematics.py
+++ validphys2/src/validphys/kinematics.py
@@ -7,7 +7,6 @@ Uses the PLOTTING file specification.
 import logging
 
 import pandas as pd
-import numpy as np
 
 from reportengine import collect
 from reportengine.table import table
@@ -16,25 +15,33 @@ from validphys import plotoptions
 
 log = logging.getLogger(__name__)
 
-#TODO: Take commondata and cuts instead of dataset
-def kinlimits(dataset):
-    """Return a mapping with 'dataset', 'var', 'min', 'max' where 'var'
-    corresponds to the x variable as defined in the PLOTTING specification and
-    'min' and 'max' are the minimum and maximum of this variable for all the
-    datapoints.
-    """
-    infos = plotoptions.get_infos(dataset)
+
+def kinlimits(commondata, cuts, use_kinoverride:bool=True):
+    """Return a mapping conaining the number of fitted and used datapoints,
+    as well as the label, minimum and maximum value for each of the three
+    kinematics. If ``use_kinoverride`` is set to False, the PLOTTING files
+    will be ignored and the kinematics will be interpred based on the process
+    type only."""
+    infos = plotoptions.get_infos(commondata, cuts=None, use_plotfiles=use_kinoverride)
     if len(infos)>1:
         log.info("Reading the first info for dataset %s "
-        "and ignoring the others.", dataset)
+        "and ignoring the others.", commondata)
     info = infos[0]
-    kintable = plotoptions.kitable(dataset, info)
-    varlabel = info.xlabel
-    if info.x == 'idat':
-        lmin, lmax = np.nan, np.nan
-    else:
-        lmin, lmax = kintable[info.x].min(), kintable[info.x].max()
-    return {'dataset': dataset, 'var': varlabel, 'min':lmin, 'max':lmax}
+    kintable = plotoptions.kitable(commondata, info)
+    ndata = len(kintable)
+    if cuts:
+        kintable = kintable.ix[cuts.load()]
+    nfitted = len(kintable)
+
+    d = {'dataset': commondata, '$N_{data}$':ndata, '$N_{fitted}$':nfitted}
+    for i, key in enumerate(['k1', 'k2', 'k3']):
+        kmin = kintable[key].min()
+        kmax = kintable[key].max()
+        label = info.kinlabels[i]
+        d[key] = label
+        d[key + ' min'] = kmin
+        d[key + ' max'] = kmax
+    return d
 
 all_kinlimits = collect(kinlimits, ('experiments', 'experiment'))
 
@@ -42,4 +49,7 @@ all_kinlimits = collect(kinlimits, ('experiments', 'experiment'))
 def all_kinlimits_table(all_kinlimits):
     """Return a table with the kinematic limits for the datasets in all
     the experiments."""
-    return pd.DataFrame(all_kinlimits, columns=['dataset', 'var', 'min', 'max'])
+    return pd.DataFrame(all_kinlimits,
+        columns=['dataset', '$N_{data}$', '$N_{fitted}$',
+        'k1', 'k1 min', 'k1 max', 'k2', 'k2 min', 'k2 max', 'k3', 'k3 min', 'k3 max'
+    ])
