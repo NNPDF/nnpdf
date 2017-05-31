9a74249e8d012da1f0676b0b891941c874a25bc7
Zahari <zaharid@gmail.com>
2017-05-31 19:20:45 +0200

    Remove fitpdfs
    This was a bad idea an can be implemented with a collect function
    (though maybe I should have a fitpdf singular).

--- validphys2/src/validphys/config.py
+++ validphys2/src/validphys/config.py
@@ -333,12 +333,6 @@ class Config(report.Config):
         return theory['theoryid']
     """
 
-    #TODO: There should be a build in functionality for this. It is not easy
-    #though. The roundtrip for processing the pdfs is especially ugly.
-    #Should be for reportengine v1.0
-    def produce_fitpdfs(self, fits):
-        return {'pdfs': self.parse_pdfs([fit.as_input()['pdf'] for fit in fits])}
-
 
     @element_of('lumi_channels')
     def parse_lumi_channel(self, ch:str):
