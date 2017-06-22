6ca8895ef2c8036b7a247f25f6218d644ffe23c9
Zahari <zaharid@gmail.com>
2017-06-22 15:58:00 +0200

    Add polynomial fitting tool
    Probably these are too specialized and should be moved to another
    module. Will see.

--- validphys2/src/validphys/plots.py
+++ validphys2/src/validphys/plots.py
@@ -1440,3 +1440,55 @@ def plot_fitted_replicas_as_profiles_matched(fits_pdfs,
     ax.set_xlabel(r'$\alpha_S$')
     ax.set_ylabel(r'$\chi²/N_{dat}$')
     return fig
+
+
+@figure
+def plot_poly_as_fit(fits_pdfs,
+        fits_replica_data_correlated, max_ndiscarded:int=4, polorder:int=2):
+    """Plot a polynomial fit of chi²(as) of `degree polorder`, keeping the
+    replica index matched.
+
+    The ``max_ndiscarded`` parameter defines th number of points
+    discarded by postfit from which we discard the curve.
+    """
+
+    alphas = [pdf.AlphaS_MZ for pdf in fits_pdfs]
+    df = fits_replica_data_correlated
+    def ap(x):
+        x.columns = x.columns.droplevel(0)
+        return (x['training'] + x['validation'])/2
+    table = df.groupby(axis=1, level=0).apply(ap)
+    filt = table.isnull().sum(axis=1) < max_ndiscarded
+    table = table[filt]
+    table = table.as_matrix()
+    fig, ax = plt.subplots()
+
+    minimums = []
+    asarr = np.asarray(alphas)
+    for i, row in enumerate(table):
+        filt =  np.isfinite(row)
+        fit = np.polyfit(asarr[filt], row[filt], polorder)
+        extrema = np.roots(np.polyder(fit))
+
+        #Cast away the zero complex part
+        candidates = np.real(extrema[np.isreal(extrema)])
+        candidates = candidates[(candidates > alphas[0]) & (candidates<alphas[-1])]
+        if len(candidates):
+            minimums.append(candidates[np.argmin(np.polyval(fit, candidates))])
+            color = f'C{i%10}'
+            ax.plot(alphas, np.polyval(fit, alphas), color=color)
+            ax.plot(asarr[filt], row[filt], 'o', color=color)
+    minimums = np.asarray(minimums)
+
+
+    ax.set_xlim(min(alphas), max(alphas))
+    ax.set_ylim(np.nanmin(table), np.nanmax(table))
+    #fig.colorbar(lc, label=r"Preferred $\alpha_S$")
+    plt.title(rf"$\alpha_S$ from order {polorder} polynomial fit = ${np.mean(minimums):.4f} \pm {np.std(minimums):.4f}$ N={len(table)}")
+
+
+
+    #ax.plot(alphas, np.array(table).T, color='#ddddcc')
+    ax.set_xlabel(r'$\alpha_S$')
+    ax.set_ylabel(r'$\chi²/N_{dat}$')
+    return fig
