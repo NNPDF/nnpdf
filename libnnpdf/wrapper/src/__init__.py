966714cd27e27486ffa51cad31ef5db72e10ce4a
Zahari <zaharid@gmail.com>
2017-07-08 12:03:33 +0200

    Build the wrapper in one step and from meson
    Compile a single wrapper file from one source instead of per
    header.
    
    Now the compilations is much faster. While it's not strictly required
    I prefer to have the wrapper definitions in one file, instead of in
    many, because there is no such thing as separated logical units in
    SWIG and instead everything depends on everything else.
    
    Also add "smart" meson definition allowing to rebuild the wrapper
    exactly when one file has changed and avoiding the nefarious
    setuptools setup. At this point meson looks nice, although I had to
    work around several issues. The main problem was getting it to copy
    a bunch of files to a given folder. One problem remaining is that one
    needs to point PKG_CONFIG_PATH to the conda location for things to
    work as expected (setuptools gets the relevant python flags, but meson
    doesn't for some reason).
    
    Also `pip uninstall NNPDF` doesn't work, but it was broken anyway.
    Instead `ninja uninstall` does work.
    
    CMake still works with that setup (but also compiling only one
    wrapper).
    
    One change is that now all the definitions come from one module, so
    there is no such thing as
    
        from NNPDF.thpredictions import ThPredictions
    
    Because this would break validphys and possible other uses of the
    wrappers, we add backwards compatibility modules that print a warning
    and then forward to the main module (and are all symlinks to the same
    file). This should keep things working while they are updated.
    
    Fixes #12.

--- libnnpdf/wrapper/src/__init__.py
+++ libnnpdf/wrapper/src/__init__.py
@@ -1,6 +1,1 @@
-from NNPDF.lhapdfset import LHAPDFSet
-from NNPDF.pdfset import PDFSet
-from NNPDF.fastkernel import FKTable
-from NNPDF.thpredictions import ThPredictions
-from NNPDF.commondata import CommonData
-from NNPDF.nnpdfdb import IndexDB
+from .nnpdf import *
--- libnnpdf/wrapper/src/include/real_typemap.i
+++ libnnpdf/wrapper/src/include/real_typemap.i
@@ -1,7 +1,2 @@
-#ifdef SSE_CONV
 #define REALDOUBLE 0
 %numpy_typemaps(NNPDF::real, NPY_FLOAT , int)
-#else
-#define REALDOUBLE 1
-%numpy_typemaps(NNPDF::real, NPY_DOUBLE , int)
-#endif
