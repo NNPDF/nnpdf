603b7e298eb55899e21f5a1a16faff9a448d2bd5
Stefano Carrazza <stefano.carrazza@cern.ch>
2017-07-14 09:48:37 +0200

    some fixes for build-test

--- libnnpdf/.ciscripts/build-test.sh
+++ libnnpdf/.ciscripts/build-test.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
+
+#Find conda
+source ~/.bashrc
+conda build -q conda-recipe
+if [ $? != 0 ]; then
+	echo failed to build
+	exit 1
+fi
+
+# install cmake
+conda install cmake git
+
+# build library and test
 cmake .
 make
 make catch_test
