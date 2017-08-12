2d075dcd6d5c78288fca88573233a76232953eae
Stefano Carrazza <stefano.carrazza@cern.ch>
2017-08-12 13:16:58 +0200

    reducing interpolation power in order to get smoother photons at Q0

--- nnpdfcpp/src/fiatlux/src/fiatlux.cc
+++ nnpdfcpp/src/fiatlux/src/fiatlux.cc
@@ -312,7 +312,7 @@ int main(int argc, char **argv)
     };
 
   APFEL::SetNumberOfGrids(1);
-  APFEL::SetExternalGrid(1, 115, 5, X1);
+  APFEL::SetExternalGrid(1, 115, 3, X1);
   APFEL::SetFastEvolution(false);
   APFEL::LockGrids(true);
   APFEL::InitializeAPFEL_DIS();
