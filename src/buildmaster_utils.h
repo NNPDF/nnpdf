6234056d2583712fca21bb5790b7e23ed1ff42fb
Nathan Hartland <n.p.hartland@vu.nl>
2017-12-11 13:30:16 +0100

    metadata reading from yaml file

--- src/buildmaster_utils.h
+++ src/buildmaster_utils.h
@@ -19,11 +19,43 @@
 #include <cstdlib>
 
 #include <NNPDF/commondata.h>
-using namespace NNPDF;
+
+namespace Buildmaster{}
 using namespace std;
+using namespace Buildmaster;
+using NNPDF::dataInfoRaw;
+using NNPDF::MULT;
+using NNPDF::ADD;
 
 // symmetrise experimental errors
 void symmetriseErrors(double right, double left, double* sigma, double* delta);
 
 // Generate artificial systematics
 bool genArtSys(int ndata, const double* const* cov, double** artsys);
+
+// Read metadata from YAML file
+NNPDF::dataInfoRaw readMeta(std::string setname);
+
+// Buildmaster wrapper for CommonData
+// So far this only implements the extra constructor for reading META files,
+// still to come would be the new filling API
+namespace Buildmaster
+{
+    // Read metadata file
+    class CommonData: public NNPDF::CommonData
+    {
+      public:
+        // Constructor from metadata file
+        CommonData(std::string setname):
+        NNPDF::CommonData(readMeta(setname))
+        {
+        }
+
+        // Constructor from dataInfoRaw
+        CommonData(NNPDF::dataInfoRaw const& info):
+        NNPDF::CommonData(info)
+        {
+        }
+
+    };
+}
