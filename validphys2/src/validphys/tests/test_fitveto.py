f887a698b4f4ee420b3f70728044dfb68f5a18ec
Nathan Hartland <n.p.hartland@vu.nl>
2018-02-12 13:54:09 +0100

    Importing fitveto.py into validphys, repointing test_fitveto, renaming fitinfo->fitinfos

--- validphys2/src/validphys/tests/test_fitveto.py
+++ validphys2/src/validphys/tests/test_fitveto.py
@@ -5,54 +5,8 @@ from hypothesis.strategies import (floats, integers, tuples, lists,
     booleans)
 from hypothesis.extra.numpy import arrays,  array_shapes
 
+from validphys.fitveto import (distribution_veto, determine_vetoes, NSIGMA_DISCARD)
 from validphys.fitdata import FitInfo
-
-
-#TODO: Import these from some sensible location
-
-NSIGMA_DISCARD = 4
-
-def distribution_veto(dist):
-    """ For a given distribution (a list of floats), returns a boolean mask
-    specifying the passing elements """
-    dist = np.asarray(dist)
-    replica_mask = np.ones_like(dist, dtype=bool)
-    while True:
-        passing = dist[replica_mask]
-        average_pass = np.mean(passing)
-        stderr_pass  = np.std(passing)
-        # NOTE that this has always not been abs
-        # i.e replicas that are lower than the average by more than 4std pass
-        new_mask = (dist - average_pass) < NSIGMA_DISCARD*stderr_pass
-        if sum(new_mask) == sum(replica_mask): break
-        replica_mask = new_mask
-    return replica_mask
-
-def determine_vetoes(fitinfo: list):
-    """ Assesses whether replica fitinfo passes standard NNPDF vetoes
-    Returns a dictionary of vetoes and their passing boolean masks.
-    Included in the dictionary is a 'Total' veto.
-    """
-    # Setup distributions to veto upon
-    # TODO ensure that all replicas have the same amount of arclengths
-    distributions = {"ChiSquared": [i.chi2 for i in fitinfo]}
-    for i in range(0, len(fitinfo[0].arclengths)):
-        distributions["ArcLength_"+str(i)] = [j.arclengths[i] for j in fitinfo]
-
-    # Positivity veto
-    vetoes = {"Positivity": [replica.is_positive for replica in fitinfo]}
-
-    # Distribution vetoes
-    for key in distributions:
-        vetoes[key] = distribution_veto(distributions[key])
-
-    # Determine total veto
-    vetoes["Total"] = np.ones(len(fitinfo), dtype=bool)
-    for key in vetoes:
-        vetoes["Total"] = np.asarray([x & y for (x, y) in
-            zip(vetoes["Total"], vetoes[key])])
-    return vetoes
-
 
 shape1d = array_shapes(max_dims=1, min_side=1, max_side=1000)
 nicefloats = floats(allow_nan=False, allow_infinity=False)
