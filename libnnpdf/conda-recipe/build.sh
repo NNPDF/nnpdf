aa74a4555b8fa28a0510822b920af95fcc4ef7b6
Stefano Carrazza <stefano.carrazza@cern.ch>
2018-02-14 10:58:39 +0100

    towards conda build

--- libnnpdf/conda-recipe/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir build
-cd build
-cmake ../ -DENABLE_PYWRAP=on -DCMAKE_INSTALL_PREFIX=${PREFIX} -DAVX_FOUND=OFF -DAVX2_FOUND=OFF
-make
-make install
-
-make wrapper
--- nnpdfcpp/conda-recipe/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cmake . -DCOMPILE_validphys=OFF -DCOMPILE_mkthpredictions=ON -DCMAKE_INSTALL_PREFIX=${PREFIX}
-make -j${CPU_COUNT}
-make install
-
-cd tools/postfit2
-python setup.py install --single-version-externally-managed --record record.txt
