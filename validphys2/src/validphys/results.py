aee5ce6df9fbbff60c9c3bce1bc2c2d7f22ed0ff
wilsonmr <M.R.Wilson-3@sms.ed.ac.uk>
2018-02-15 11:58:28 +0000

    Added phi_data_experiment and experiments_phi to collect this function for each experiment specified in run card, we then use this new function to plot a bar equal to phi for each experiments in a redefined plot_phi, we also changed the normalisation on phi to match that of validphys1 (divide by N_datapoints)

--- validphys2/src/validphys/results.py
+++ validphys2/src/validphys/results.py
@@ -410,14 +410,19 @@ def abs_chi2_data(results):
     return Chi2Data(th_result.stats_class(chi2s[:, np.newaxis]),
                     central_result, len(data_result))
 
-def phi_data(results):
-    """Calculate phi using values returned by abs_chi2_data """
-    alldata, central, npoints = abs_chi2_data(results)
-    return np.sqrt((alldata.data.mean() - central))
 
 def abs_chi2_data_experiment(experiment_results):
     """Like `abs_chi2_data` but for a whole experiment"""
     return abs_chi2_data(experiment_results)
+
+def phi_data(results):
+    """Calculate phi using values returned by abs_chi2_data """
+    alldata, central, npoints = abs_chi2_data(results)
+    return np.sqrt((alldata.data.mean() - central)/npoints)
+
+def phi_data_experiment(experiment_results):
+    """Like 'phi_data' but for whole experiment"""
+    return phi_data(experiment_results)
 
 def chi2_breakdown_by_dataset(experiment_results, experiment, t0set,
                               prepend_total:bool=True,
@@ -783,6 +788,8 @@ each_dataset_results = collect(results, ('experiments', 'experiment'))
 
 experiments_chi2 = collect(abs_chi2_data_experiment, ('experiments',))
 each_dataset_chi2 = collect(abs_chi2_data, ('experiments', 'experiment'))
+
+experiments_phi = collect(phi_data_experiment, ('experiments',))
 
 #These are convenient ways to iterate and extract varios data from fits
 fits_chi2_data = collect(abs_chi2_data, ('fits', 'fitcontext', 'experiments', 'experiment'))
