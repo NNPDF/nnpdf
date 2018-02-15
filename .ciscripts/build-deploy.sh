9c93ed419ec00cbd0932c46971b4ed1827fc9ada
Stefano Carrazza <stefano.carrazza@cern.ch>
2018-02-15 16:09:28 +0100

    updating meta file
    travis, first attempt
    
    second attempt using gitlab and travis
    
    changing sh permission
    
    moving env flag to global
    
    minor adjustments
    
    more adjustments
    
    debugging gitlab-ci
    
    debugging gitlab-ci
    
    updating ci

---
+++ .ciscripts/build-deploy.sh
@@ -0,0 +1,36 @@
+#/usr/bin/bash
+
+#Find conda
+source ~/.bashrc
+conda build -q conda-recipe
+if [ $? != 0 ]; then
+	echo failed to build
+	exit 1
+fi
+
+if [ "$CI_BUILD_REF_NAME" != 'master'  ] && [ "$UPLOAD_NON_MASTER" == false ]; 
+then
+  	echo "
+Skiping upload because this is not master and you have not
+set the UPLOAD_NON_MASTER variable."
+	exit 0
+fi
+
+#This seems to be needed for "artifacts" to work.
+cp /root/miniconda3/conda-bld/linux-64/*.tar.bz2 .
+
+echo "Uploading package to zigzah"
+KEY=$( mktemp )
+#This is defined in the Gitlab variables, under the Settings Menu.
+echo "$ZIGZAH_SSH_KEY" > "$KEY"
+
+scp -i "$KEY" -o StrictHostKeyChecking=no\
+    /root/miniconda3/conda-bld/linux-64/*.tar.bz2 \
+    dummy@zigzah.com:~/conda-pkgs-private/linux-64 
+
+if [ $? == 0 ]; then
+	echo "Upload suceeded"
+else
+	echo "Upload failed"
+	exit 1
+fi
