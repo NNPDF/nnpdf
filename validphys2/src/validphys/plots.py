c1f98303cf36255a162b26f7783e4c38505d5f4b
wilsonmr <M.R.Wilson-3@sms.ed.ac.uk>
2018-02-19 13:01:36 +0000

    added lines to represnt average for phi, for each PDF just to phi_plot_pdfs for now

--- validphys2/src/validphys/plots.py
+++ validphys2/src/validphys/plots.py
@@ -57,7 +57,7 @@ def plot_phi(experiments, experiments_phi):
     """plots phi for all experiments as a barplot"""
     phi = experiments_phi
     xticks = [experiment.name for experiment in experiments]
-    fig, ax = plotutils.barplot(np.array(phi).transpose(), collabels=xticks, datalabels=[r'$\phi'])
+    fig, ax = plotutils.barplot(phi, collabels=xticks, datalabels=[r'$\phi'])
     ax.set_title(r"$\phi$ for each experiment")
     return fig
 
@@ -67,8 +67,15 @@ def plot_phi_pdfs(experiments, pdfs, experiments_pdfs_phi):
     phi = experiments_pdfs_phi
     phi_labels = [pdf.name for pdf in pdfs]
     xticks = [experiment.name for experiment in experiments]
-    #barplot doesn't work as per description, instead columns of values must = length of collabels, I believe this to be a slight bug but probably easier to fix barplot description not change how it works
     fig, ax = plotutils.barplot(phi, collabels=xticks, datalabels=phi_labels)
+    #test for how barplot plotted bars
+    if ax.yaxis.get_visible():
+        avgline = ax.axvline
+    else:
+        avgline = ax.axhline
+        #for loop seems unneccessary but couldn't label vlines/hlines seperately
+    for i, phi_label in enumerate(phi_labels):
+        avgline(np.mean(phi[i]), 0, 1, label = phi_label + " average", linestyle='solid', color="C" + str(i))
     ax.set_title(r"$\phi$ for each experiment")
     ax.legend()
     return fig
