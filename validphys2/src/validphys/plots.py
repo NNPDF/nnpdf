Fixing plotting bug and defining datapoints which is a string of data values, useful for normalisation

validphys2/src/validphys/plots.py
@@ -1582,15 +1582,16 @@ def plot_covdiff_heatmap(theory_covmat, experiments_covmat):
     return fig
 
 @figure
-def plot_diag_cov_comparison(theory_covmat,experiments_covmat):  ### CHANGE ALL THESE TO NORMS!!
+def plot_diag_cov_comparison(theory_covmat,experiments_covmat, datapoints):  
     """ Plot of sqrt(cov_ii)/data_i for cov = exp versus cov = theory """
+    data = datapoints
     df1 = theory_covmat
     df2 = experiments_covmat
     sqrtdiags1 = np.diag(np.sqrt(df1.as_matrix()))
     sqrtdiags2 = np.diag(np.sqrt(df2.as_matrix()))
     fig,ax = plt.subplots() 
-    ax.plot(sqrtdiags1, '.', label="Theory")
-    ax.plot(sqrtdiags2, '.', label="Experiment")
+    ax.plot(sqrtdiags1/data, '.', label="Theory")
+    ax.plot(sqrtdiags2/data, '.', label="Experiment")
     plt.xlabel("Data points")
     ax.set_ylabel(r"$\frac{\sqrt{cov_{ii}}}{D_i}$")
     ax.set_title("Diagonal elements of normalised theoretical and experimental covariance matrices")
@@ -1598,19 +1599,19 @@ def plot_diag_cov_comparison(theory_covmat,experiments_covmat):  ### CHANGE ALL 
     return fig
 
 @figure
-def plot_diag_cov_impact(theory_covmat,experiments_covmat):  ### CHANGE ALL THESE TO NORMS!!
+def plot_diag_cov_impact(theory_covmat,experiments_covmat, datapoints):  
     """ Plot of ((expcov)^-1_ii)^-0.5 versus ((expcov + thcov)^-1_ii)^-0.5 """
     df1 = theory_covmat
     df2 = experiments_covmat
     matrix1 = df1.as_matrix()
     matrix2 = df2.as_matrix()
-    a = (np.diag(la.inv(matrix1)))**(-0.5)
+    a = (np.diag(la.inv(matrix2)))**(-0.5)
     b = (np.diag(la.inv(matrix1+matrix2)))**(-0.5)
     fig,ax = plt.subplots() 
     ax.plot(a, '.', label="Experiment")
     ax.plot(b, '.', label="Experiment + Theory")
     plt.xlabel("Data points")
-    ax.set_ylabel(r"$\frac{1}{\sqrt{cov^-1_{ii}}}$")
+    ax.set_ylabel(r"$\frac{1}{\sqrt{[cov^{-1}_]{ii}}}$")
     ax.set_title("Impact of adding theory covariance matrix")
     ax.legend()
     return fig