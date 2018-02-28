85f5c5f7b12b1b0a06e8c850bef30363cc46c8ff
Zahari <zaharid@gmail.com>
2018-02-28 12:03:54 +0000

    Run python and cpp tests with conda
    Set up the requirement environment to:
    
     - Be able to test validphys with pytest.
     - Be able to compile and run the catch test, with all the debug flags
       enabled.
    
    This has the added benefit of testing that the debug build actually
    works.

---
+++ conda-recipe/run_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -u
+set -v
+set -e
+
+export CPPFLAGS=$DEBUG_CPPFLAGS
+export CXXFLAGS=$DEBUG_CXXFLAGS
+export CFLAGS=$DEBUG_CFLAGS
+
+mkdir bldtest
+cd bldtest
+cmake .. -DCMAKE_BUILD_TYPE=debug -DENABLE_TESTS=ON
+make catch_test -j
+./libnnpdf/tests/catch_test
