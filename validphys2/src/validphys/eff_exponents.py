91a5c795ce3faf63c11c1745c76e080155fb276a
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2018-03-01 16:05:27 +0100

    attached to the last commit

--- validphys2/src/validphys/eff_exponents.py
+++ validphys2/src/validphys/eff_exponents.py
@@ -15,8 +15,7 @@ from validphys.plots import check_pdf_normalize_to
 from validphys.plots import BandPDFPlotter
 from validphys.plots import PDFPlotter
 
-from validphys.pdfgrids import xgrid
-from validphys.pdfgrids import xplotting_grid
+import validphys.pdfgrids as pdfgrids
 
 log = logging.getLogger(__name__)
 
@@ -33,11 +32,22 @@ class PreprocessingPlotter(PDFPlotter):
 
 class ExponentBandPlotter(BandPDFPlotter, PreprocessingPlotter): pass
 
-def alpha_eff(xplotting_grids):
+
+def alpha_eff(pdfs,xmin=1e-5,Q=1.65,basis='evolution',flavours=None):
+    xpdfs = []
+    xmax=0.1
+    temp = pdfgrids.xgrid(xmin, xmax,'log', 200)
+
+    for pdf in pdfs:
+        xpdf = pdfgrids.xplotting_grid(pdf, Q, xgrid=temp, basis=basis,flavours=flavours)
+        xpdfs.append(xpdf)
+
     alpha_grids=[]
-    for xplotting_grid in xplotting_grids:
+    for xplotting_grid in xpdfs:
         newvalues = xplotting_grid.grid_values # grid_values[replica][flavour][x]
         xgrid = xplotting_grid.xgrid
+
+        print(np.where(newvalues == 0))
 
         newvalues = -np.log(abs(newvalues/xgrid))/np.log(xgrid)
         #newgrid is like the old grid but with updated values
@@ -47,9 +57,16 @@ def alpha_eff(xplotting_grids):
     # xplotting_grid._replace(grid_values=newvalues)
     return alpha_grids #.grid_values
 
-def beta_eff(xplotting_grids):
+def beta_eff(pdfs,xmin=1e-2,Q=1.65,basis='evolution',flavours=None):
+    xpdfs = []
+    xmax=0.9
+    temp = pdfgrids.xgrid(xmin, xmax,'linear', 200)
+
+    for pdf in pdfs:
+        xpdf = pdfgrids.xplotting_grid(pdf, Q, xgrid=temp, basis=basis,flavours=flavours)
+        xpdfs.append(xpdf)
     beta_grids=[]
-    for xplotting_grid in xplotting_grids:
+    for xplotting_grid in xpdfs:
         newvalues = xplotting_grid.grid_values # grid_values[replica][flavour][x]
         xgrid = xplotting_grid.xgrid
 
@@ -64,28 +81,12 @@ def beta_eff(xplotting_grids):
 
 @figuregen
 @check_pdf_normalize_to
-def plot_alphaEff(pdfs, normalize_to:(int,str,type(None))=None,xmin=1e-5,Q=1.65,basis='evolution',flavours=None):
+def plot_alphaEff(pdfs, alpha_eff, normalize_to:(int,str,type(None))=None):
 
-    xpdfs = []
-    xmax=0.1
-    temp_xgrid = xgrid(xmin, xmax,'log', 200)
-
-    for pdf in pdfs:
-        xpdf = xplotting_grid(pdf, Q, xgrid=temp_xgrid, basis=basis,flavours=flavours)
-        xpdfs.append(xpdf)
-
-    yield from ExponentBandPlotter('alpha', pdfs, alpha_eff(xpdfs), 'log', normalize_to)
+    yield from ExponentBandPlotter('alpha', pdfs, alpha_eff, 'log', normalize_to)
 
 @figuregen
 @check_pdf_normalize_to
-def plot_betaEff(pdfs, normalize_to:(int,str,type(None))=None,xmin=1e-2,Q=1.65,basis='evolution',flavours=None):
+def plot_betaEff(pdfs, beta_eff, normalize_to:(int,str,type(None))=None):
 
-    xpdfs = []
-    xmax=0.9
-    temp_xgrid = xgrid(xmin, xmax,'linear', 200)
-
-    for pdf in pdfs:
-        xpdf = xplotting_grid(pdf, Q, xgrid=temp_xgrid, basis=basis,flavours=flavours)
-        xpdfs.append(xpdf)
-
-    yield from ExponentBandPlotter('beta', pdfs, beta_eff(xpdfs), 'linear', normalize_to)
+    yield from ExponentBandPlotter('beta', pdfs, beta_eff, 'linear', normalize_to)
