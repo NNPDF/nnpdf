ff3e97435b2ae49401b06882740945a2bb442244
Zahari <zaharid@gmail.com>
2018-03-08 10:51:21 +0000

    Try to run c++ tests in release mode
    If it compiles the library maybe it will compile the tests?

--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -9,14 +9,10 @@ pytest --pyargs validphys
 
 #Compile in debug mode and run the cpp tests
 
-#Aparently this is undefined in Mac
-export CPPFLAGS=${DEBUG_CPPFLAGS:-}
-export CXXFLAGS=$DEBUG_CXXFLAGS
-export CFLAGS=$DEBUG_CFLAGS
 
 mkdir bldtest
 cd bldtest
-cmake .. -DCMAKE_BUILD_TYPE=debug -DENABLE_TESTS=ON
+cmake .. -DENABLE_TESTS=ON
 make catch_test -j
 ./libnnpdf/tests/catch_test
 
