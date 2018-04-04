7af7e6f9b15eda2ceb2b9d9d4c9a3fe0f8ae8410
Cameron Voisey <voisey@hep.phy.cam.ac.uk>
2018-04-04 15:09:26 +0100

    Add action to compute exp + th cov mat normalised to data

--- validphys2/src/validphys/results.py
+++ validphys2/src/validphys/results.py
@@ -972,6 +972,14 @@ def theory_normcovmat_3pt(theory_covmat_3pt, experiments_data):
 
 
 @table
+def expplusth_normcovmat_3pt(experiments_covmat, theory_covmat_3pt, experiments_data):
+    """Calculates the experiment + theory covariance matrix for 3-point scale variations normalised to data."""
+    df = experiments_covmat + theory_covmat_3pt
+    experiments_data_array = np.array(experiments_data)
+    mat = df/np.outer(experiments_data_array, experiments_data_array)
+    return mat
+
+@table
 def experimentsplustheory_corrmat_3pt(experiments_covmat, theory_covmat_3pt):
     """Calculates the correlation matrix for the experimental  plus theory (3 pt) covariance matrices."""
     exp_df = experiments_covmat
