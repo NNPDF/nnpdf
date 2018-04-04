14d2574318b8f6d2a7513a4238b1a3da0e39733a
Cameron Voisey <voisey@hep.phy.cam.ac.uk>
2018-04-04 15:09:26 +0100

    Add action to compute exp + th cov mat normalised to data

--- validphys2/src/validphys/plots.py
+++ validphys2/src/validphys/plots.py
@@ -1718,6 +1718,20 @@ def plot_thcorrmat_heatmap(theory_corrmat_3pt):
     return fig
 
 @figure
+def plot_normexpplusthcovmat_heatmap(experimentsplustheory_normcovmat_3pt):
+    """Matrix plot of the exp + theory covariance matrix normalised to data"""
+    df = experimentsplustheory_normcovmat_3pt
+    matrix = experimentsplustheory_normcovmat_3pt.as_matrix()
+    fig, ax = plt.subplots()
+    matrixplot = ax.matshow(matrix*100, cmap=cm.Spectral_r, norm=mcolors.SymLogNorm(linthresh=0.1, linscale=10, vmin=-100*matrix.max(), vmax=100*matrix.max()))
+    cbar = fig.colorbar(matrixplot, label="% of data")
+    ax.set_title('Experiment + theory covariance matrix')
+    ticklocs, ticklabels = matrix_plot_labels(df)
+    plt.xticks([])
+    plt.yticks(ticklocs, ticklabels)
+    return fig
+
+@figure
 def plot_expplusthcorrmat_heatmap(experimentsplustheory_corrmat_3pt):
     """Matrix plot of the exp + theory correlation matrix"""
     df = experimentsplustheory_corrmat_3pt
