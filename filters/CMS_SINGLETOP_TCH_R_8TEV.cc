6b650b4eea641afba98a7822dea64f770f06bded
voisey <voisey@hep.phy.cam.ac.uk>
2018-10-13 23:06:42 +0100

    Change systematic treatment in filter

--- filters/CMS_SINGLETOP_TCH_R_8TEV.cc
+++ filters/CMS_SINGLETOP_TCH_R_8TEV.cc
@@ -1,5 +1,5 @@
 /*
-Measurement of the ratio of the single top quark and single antitop quark cross sections in the t-channel @LHC CMS 7 TeV
+Measurement of the ratio of the single top quark and single antitop quark cross sections in the t-channel @LHC CMS 8 TeV
 There is a single point
 
 LHC-CMS 8 TeV
@@ -10,7 +10,7 @@ Archived as: 1403.7366v2
 Published in: JHEP 06 (2014) 090
 R_{t-ch.} = 1.95 ± 0.10 (stat) ± 0.19 (syst)
 
-Here systematic uncertainties are split into 9 different categories (see Table 4 in the paper for the full breakdown and for more details):
+The systematic uncertainty is composed of the following 9 uncertainties (see Table 4 in the paper for the full breakdown and for more details):
 JES, JER, MET, and pileup
 b-tagging and mis-tag
 Leptop reconstruction/trig.
@@ -39,32 +39,27 @@ void CMS_SINGLETOP_TCH_R_8TEVFilter::ReadData()
       exit(-1);
     }
 
-  //Starting filter
+  // Starting filter
+ 
   string line;
   int idum;
   double cme;
-  double fstat_percentage;
 
   getline(f1,line);
   istringstream lstream(line);
   lstream >> idum >> cme;
       
   fKin1[0] = 0.;
-  fKin2[0] = Mt*Mt;          //top mass
-  fKin3[0] = cme*1000;       //sqrt(s)
+  fKin2[0] = Mt*Mt;          // Top mass
+  fKin3[0] = cme*1000;       // Sqrt(s)
 
-  lstream >> fData[0];       //central value
-  lstream >> fstat_percentage; //statistical (percentage) uncertainty
-  fStat[0] = fstat_percentage*fData[0]/100; //convert percentage uncertainty to absolute uncertainty and store
+  lstream >> fData[0];       // Central value
+  lstream >> fStat[0];       // Absolute statistical uncertainty
 
-  for (int i = 0; i < fNSys; i++)
-    {
-       lstream >> fSys[0][i].mult;
-       fSys[0][i].add = fSys[0][i].mult*fData[0]/100; 
-       fSys[0][i].type = MULT;
-       fSys[0][i].name = "UNCORR";      
-    }
+  lstream >> fSys[0][i].add; // Absolute total systematic uncertainty
+  fSys[0][i].mult = fSys[0][i].mult*100/fData[0]; // Multiplicative total systematic uncertainty
+  fSys[0][i].type = MULT;
+  fSys[0][i].name = "UNCORR";      
 
   f1.close();
-
 }
