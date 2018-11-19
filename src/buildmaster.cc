6b5f7eabd421b6ff9fd5e163eb0bd1782fe76df2
Shayan Iranipour <shayan@dhcp-10-240-125-100.cp.wireless.private.cam.ac.uk>
2018-11-19 15:15:05 +0000

    starting implementation of CMSZPCTOT12

--- src/buildmaster.cc
+++ src/buildmaster.cc
@@ -71,6 +71,8 @@
 #include "CMS_SINGLETOP_TCH_TOT_7TEV.h"
 #include "CMS_SINGLETOP_TCH_R_8TEV.h"
 #include "CMS_SINGLETOP_TCH_R_13TEV.h"
+#include "CMSZPCTOT12.h"
+
 
 /**
  * \param argv the filename containing the configuration
@@ -155,6 +157,7 @@ void InitCommonData(vector<CommonData*>& target)
   target.push_back(new CMSZDIFF12Filter());
   target.push_back(new CMS1JET276TEVFilter());
   target.push_back(new CMSWMU8TEVFilter());
+  target.push_back(new CMSZPCTOT12Filter());
 
  // ************************* CMSwc ******************************
 
