0c1b5cfd12382b5dfc6ae962efc89d9852bf0622
Shayan Iranipour <shayan@dhcp-10-240-106-91.cp.wireless.private.cam.ac.uk>
2018-11-20 16:15:30 +0000

    testing code

--- filters/CMSWC13TEV.cc
+++ filters/CMSWC13TEV.cc
@@ -8,12 +8,12 @@
 
 #include "CMSWC13TEV.h"
 
-void CMSWC13TEV::ReadData()
+void CMSWC13TEVFilter::ReadData()
 {
     // Opening file
     fstream f1;
     stringstream datafile("");
-    data file << dataPath() << "rawdata/" << fSetName << "/" << fSetName << ".data";
+    datafile << dataPath() << "rawdata/" << fSetName << "/" << fSetName << ".data";
     f1.open(datafile.str().c_str(), ios::in);
 
     if (f1.fail())
@@ -21,4 +21,25 @@ void CMSWC13TEV::ReadData()
             cerr << "Error opening data file " << datafile.str() << endl;
             exit(-1);
         }
+
+    // Starting filter
+    string line;
+    int idum;
+    double cme;
+
+    getline(f1,line);
+    istringstream lstream(line);
+    lstream >> idum >> cme;
+
+    fKin1[0] = 0.;
+    fKin2[0] = Mt*Mt;          // Top mass
+    fKin3[0] = cme*1000;       // Sqrt(s)
+
+    lstream >> fData[0];       // Central value
+    lstream >> fStat[0];       // Statistical (percentage) uncertainty
+
+    lstream >> fSys[0][0].add; // Absolute total systematic uncertainty
+    fSys[0][0].mult = fSys[0][0].add*100/fData[0]; // Multiplicative total systematic uncertainty
+    fSys[0][0].type = MULT;
+    fSys[0][0].name = "UNCORR";
 }
