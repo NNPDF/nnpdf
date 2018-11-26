c7e799837f397baefdb5384e4da8e82762b8c524
Shayan Iranipour <shayan@dhcp-172-24-141-194.eduroam.wireless.private.cam.ac.uk>
2018-11-26 17:10:20 +0000

    starting implementation of ATLAS_WCHARM_TOT_UNNORM_7TEV

---
+++ inc/ATLAS_WCHARM_TOT_UNNORM_7TEV.h
@@ -0,0 +1,10 @@
+// Authors: Shayan Iranipour,  si292@damtp.cam.ac.uk
+ #pragma once
+ #include "buildmaster_utils.h"
+ class ATLAS_WCHARM_TOT_UNNORM_7TEVFilter: public CommonData
+{
+public: ATLAS_WCHARM_TOT_UNNORM_7TEVFilter():
+  CommonData("ATLAS_WCHARM_TOT_UNNORM_7TEV") { ReadData(); }
+ private:
+  void ReadData();
+};
