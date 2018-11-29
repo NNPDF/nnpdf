7c129db5a9a6c5a0f07b41eb858ab07fd4846927
Shayan Iranipour <shayan@dhcp-172-24-141-14.eduroam.wireless.private.cam.ac.uk>
2018-11-29 17:08:21 +0000

    correcting indentation

--- filters/CMS_ZCHARM_DIFF_UNNORM_8TEV.cc
+++ filters/CMS_ZCHARM_DIFF_UNNORM_8TEV.cc
@@ -19,45 +19,45 @@
   datafile << dataPath() 
        << "rawdata/" << fSetName << "/" << fSetName << ".data";
   f1.open(datafile.str().c_str(), ios::in);
-   if (f1.fail()) 
-    {
-      cerr << "Error opening data file " << datafile.str() << endl;
-      exit(-1);
-    }
+  if (f1.fail()) 
+  {
+    cerr << "Error opening data file " << datafile.str() << endl;
+    exit(-1);
+  }
    //Starting filter
   string line;
   double pTmin, pTmax;          //pT ranges
   double MZ2 = pow(MZ,2.0);     //Z mass
   double s = 13000;             //LHC at 8TeV
 
-   for(int i=0; i<fNData;i++)
+  for(int i=0; i<fNData;i++)
+  {
+    getline(f1,line);
+    istringstream lstream(line);
+    //Reading in an interpretation of each column
+    lstream >> pTmin >> pTmax >> fData[i] >> fStat[i];
+    
+    fKin1[i] = 0.5*(pTmin + pTmax);	   // pT
+    fKin2[i] = MZ2;                      // Mass W squared
+    fKin3[i] = s;                        // sqrt(s)
+
+    //Reading in the systematics
+    for(int k=0; k<fNSys;k++) // Factor of 2 because of +sys -sys format of rawdata
     {
-      getline(f1,line);
-      istringstream lstream(line);
-      //Reading in an interpretation of each column
-      lstream >> pTmin >> pTmax >> fData[i] >> fStat[i];
-      
-      fKin1[i] = 0.5*(pTmin + pTmax);	   // pT
-      fKin2[i] = MZ2;                      // Mass W squared
-      fKin3[i] = s;                        // sqrt(s)
+      lstream >> fSys[i][k].mult;
+      fSys[i][k].type = MULT;
+      if(k == 0){
+        fSys[i][k].name = "CMSLUMI12"; //We treat luminosity as a special case
+      }
+      else{
+        fSys[i][k].name = "CORR";
+      }
 
-      //Reading in the systematics
-      for(int k=0; k<fNSys;k++) // Factor of 2 because of +sys -sys format of rawdata
-      {
-        lstream >> fSys[i][k].mult;
-        fSys[i][k].type = MULT;
-        if(k == 0){
-          fSys[i][k].name = "CMSLUMI12"; //We treat luminosity as a special case
-        }
-        else{
-          fSys[i][k].name = "CORR";
-        }
-
-        //Add the additive uncertainties
-        fSys[i][k].add = fSys[i][k].mult*fData[i]*1e-2;
-      }      
-      
-    }  
+      //Add the additive uncertainties
+      fSys[i][k].add = fSys[i][k].mult*fData[i]*1e-2;
+    }      
+        
+  }  
   
-    f1.close();
+  f1.close();
  }
