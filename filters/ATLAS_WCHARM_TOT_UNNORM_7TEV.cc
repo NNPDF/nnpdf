a2853fc43c6c2121f94ac5461ed6de25e905499e
Shayan Iranipour <shayan@dhcp-172-24-141-245.eduroam.wireless.private.cam.ac.uk>
2018-12-05 16:52:43 +0000

    Working on filter and adding raw data

--- filters/ATLAS_WCHARM_TOT_UNNORM_7TEV.cc
+++ filters/ATLAS_WCHARM_TOT_UNNORM_7TEV.cc
@@ -1,61 +1,73 @@
 /*
 *   Experiment: ATLAS 
-*   Process: proton + proton -> Z + charm
-*   Center of mass energy = 8 TeV
-*   Intergrated luminosity  = 19.7 1/fb
-*   Reference: https://arxiv.org/pdf/1711.02143.pdf
+*   Process: proton + proton -> W + charm
+*   Center of mass energy = 7 TeV
+*   Intergrated luminosity  = 4.6 1/fb
+*   Reference: https://arxiv.org/abs/1402.6263
+*   HEP Data: http://hepdata.cedar.ac.uk/view/ins1282447
 *
 *   The raw data file is formatted as:
-*   pT_min - pT_max - differential cross section - statistical uncertainty - systematic (%)
+*   
 *   The systematics are as follows:
-*   Luminosity, Branching c->l, Branching b->l, D±, D*(2010)±, Pile up, Missing transverse energy
+*   
 */
- #include "ATLAS_WCHARM_TOT_UNNORM_7TEV.h"
- void ATLAS_WCHARM_TOT_UNNORM_7TEVFilter::ReadData()
+#include "ATLAS_WCHARM_TOT_UNNORM_7TEV.h"
+void ATLAS_WCHARM_TOT_UNNORM_7TEVFilter::ReadData()
 {
-  // Opening file
-  fstream f1;
-  stringstream datafile("");
-  datafile << dataPath() 
-       << "rawdata/" << fSetName << "/" << fSetName << ".data";
-  f1.open(datafile.str().c_str(), ios::in);
-   if (f1.fail()) 
-    {
-      cerr << "Error opening data file " << datafile.str() << endl;
+  // Opening files
+  fstream f1, f2;
+  stringstream datafileWP("");
+  datafileWP << dataPath() 
+       << "rawdata/" << fSetName << "/" << fSetName << "_WP.data";
+  f1.open(datafileWP.str().c_str(), ios::in);
+  if (f1.fail()) 
+  {
+      cerr << "Error opening data file " << datafileWP.str() << endl;
       exit(-1);
-    }
-   //Starting filter
+  }
+
+  stringstream datafileWM("");
+  datafileWP << dataPath() 
+       << "rawdata/" << fSetName << "/" << fSetName << "_WM.data";
+  f2.open(datafileWM.str().c_str(), ios::in);
+  if (f2.fail()) 
+  {
+      cerr << "Error opening data file " << datafileWM.str() << endl;
+      exit(-1);
+  }
+  
+  //Starting filter
   string line;
-  double pTmin, pTmax;          //pT ranges
-  double MZ2 = pow(MZ,2.0);     //Z mass
-  double s = 13000;             //LHC at 8TeV
-    for(int i=0; i<fNData;i++)
+  double etamin, etamax;          //pT ranges
+  double MW2 = pow(MW,2.0);     //W mass
+  double s = 7000;             //LHC at 7TeV
+  for(int i=0; i<fNData;i++)
+  {
+    getline(f1,line);
+    istringstream lstream(line);
+    //Reading in an interpretation of each column
+    lstream >> pTmin >> pTmax >> fData[i] >> fStat[i];
+     fData[i] = fData[i]*1000; // changing pb to fb for APPLgrid
+    fStat[i] = fStat[i]*1000; // changing pb to fb for APPLgrid
+    //Defining the kinematic variables
+    fKin1[i] = (pTmin + pTmax)*0.5;    // eta
+    fKin2[i] = MZ2;                      // Mass W squared
+    fKin3[i] = s;                        // sqrt(s)
+     //Reading in the systematics
+    for(int k=0; k<fNSys;k++) // Factor of 2 because of +sys -sys format of rawdata
     {
-      getline(f1,line);
-      istringstream lstream(line);
-      //Reading in an interpretation of each column
-      lstream >> pTmin >> pTmax >> fData[i] >> fStat[i];
-       fData[i] = fData[i]*1000; // changing pb to fb for APPLgrid
-      fStat[i] = fStat[i]*1000; // changing pb to fb for APPLgrid
-      //Defining the kinematic variables
-      fKin1[i] = (pTmin + pTmax)*0.5;    // eta
-      fKin2[i] = MZ2;                      // Mass W squared
-      fKin3[i] = s;                        // sqrt(s)
-       //Reading in the systematics
-      for(int k=0; k<fNSys;k++) // Factor of 2 because of +sys -sys format of rawdata
-      {
-        lstream >> fSys[i][k].mult;
-        fSys[i][k].type = MULT;
-        if(k == 0){
-          fSys[i][k].name = "CMSLUMI12"; //We treat luminosity as a special case
-        }
-        else{
-          fSys[i][k].name = "CORR";
-        }
-         //Add the additive uncertainties
-        fSys[i][k].add = fSys[i][k].mult*fData[i]*1e-2;
-      }      
-       
-    }  
+      lstream >> fSys[i][k].mult;
+      fSys[i][k].type = MULT;
+      if(k == 0){
+        fSys[i][k].name = "CMSLUMI12"; //We treat luminosity as a special case
+      }
+      else{
+        fSys[i][k].name = "CORR";
+      }
+       //Add the additive uncertainties
+      fSys[i][k].add = fSys[i][k].mult*fData[i]*1e-2;
+    }      
+     
+  }  
    f1.close();
- } 
+} 
