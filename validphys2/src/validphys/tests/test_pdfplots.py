e131016cb7c7db502bcc5aa5a743d5ca4fd4759e
Zahari <zaharid@gmail.com>
2019-04-09 15:04:40 +0100

    Add proof of concept of image comparison tests

---
+++ validphys2/src/validphys/tests/test_pdfplots.py
@@ -0,0 +1,15 @@
+import matplotlib
+#This is to fix a weird bug in LHAPDF
+matplotlib.use('agg')
+
+import pytest
+
+from validphys.app import API
+
+@pytest.mark.mpl_image_compare
+def test_plotpdfs():
+    pdfs = ['NNPDF31_nnlo_as_0118']
+    Q = 10
+    flavours = ['g']
+    #plot_pdfs returns a generator with (figure, name_hint)
+    return next(API.plot_pdfs(pdfs=pdfs, Q=Q, flavours=flavours))[0]
