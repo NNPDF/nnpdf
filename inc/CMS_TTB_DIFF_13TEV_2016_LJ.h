c381d0ba5bb20cf2d71c41f808b5539570bb2381
wilsonm <michael.wilson@ed.ac.uk>
2019-05-02 12:36:15 +0100

    added filters for rest of rawdata: norm tpt, then norm/unnorm ttm, tty ty removed whitespaces

--- inc/CMS_TTB_DIFF_13TEV_2016_LJ.h
+++ inc/CMS_TTB_DIFF_13TEV_2016_LJ.h
@@ -19,3 +19,67 @@ class CMS_TTB_DIFF_13TEV_2016_LJ_TPTFilter: public CommonData
  private:
   void ReadData();
 };
+
+class CMS_TTB_DIFF_13TEV_2016_LJ_TRAPFilter: public CommonData
+{
+ public: CMS_TTB_DIFF_13TEV_2016_LJ_TRAPFilter():
+  CommonData("CMS_TTB_DIFF_13TEV_2016_LJ_TRAP") { ReadData(); }
+
+ private:
+  void ReadData();
+};
+
+class CMS_TTB_DIFF_13TEV_2016_LJ_TTMFilter: public CommonData
+{
+ public: CMS_TTB_DIFF_13TEV_2016_LJ_TTMFilter():
+  CommonData("CMS_TTB_DIFF_13TEV_2016_LJ_TTM") { ReadData(); }
+
+ private:
+  void ReadData();
+};
+
+class CMS_TTB_DIFF_13TEV_2016_LJ_TTRAPFilter: public CommonData
+{
+ public: CMS_TTB_DIFF_13TEV_2016_LJ_TTRAPFilter():
+  CommonData("CMS_TTB_DIFF_13TEV_2016_LJ_TTRAP") { ReadData(); }
+
+ private:
+  void ReadData();
+};
+
+//Normalised distributions
+class CMS_TTB_DIFF_13TEV_2016_LJ_TPTNORMFilter: public CommonData
+{
+ public: CMS_TTB_DIFF_13TEV_2016_LJ_TPTNORMFilter():
+  CommonData("CMS_TTB_DIFF_13TEV_2016_LJ_TPTNORM") { ReadData(); }
+
+ private:
+  void ReadData();
+};
+
+class CMS_TTB_DIFF_13TEV_2016_LJ_TRAPNORMFilter: public CommonData
+{
+ public: CMS_TTB_DIFF_13TEV_2016_LJ_TRAPNORMFilter():
+  CommonData("CMS_TTB_DIFF_13TEV_2016_LJ_TRAPNORM") { ReadData(); }
+
+ private:
+  void ReadData();
+};
+
+class CMS_TTB_DIFF_13TEV_2016_LJ_TTMNORMFilter: public CommonData
+{
+ public: CMS_TTB_DIFF_13TEV_2016_LJ_TTMNORMFilter():
+  CommonData("CMS_TTB_DIFF_13TEV_2016_LJ_TTMNORM") { ReadData(); }
+
+ private:
+  void ReadData();
+};
+
+class CMS_TTB_DIFF_13TEV_2016_LJ_TTRAPNORMFilter: public CommonData
+{
+ public: CMS_TTB_DIFF_13TEV_2016_LJ_TTRAPNORMFilter():
+  CommonData("CMS_TTB_DIFF_13TEV_2016_LJ_TTRAPNORM") { ReadData(); }
+
+ private:
+  void ReadData();
+};
