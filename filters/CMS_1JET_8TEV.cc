f0a33105d52887ce42f910d110eef4b3485933eb
tgiani <s1792848@staffmail.ed.ac.uk>
2019-05-13 07:27:13 +0100

    working on filter

--- filters/CMS_1JET_8TEV.cc
+++ filters/CMS_1JET_8TEV.cc
@@ -62,14 +62,15 @@ tot sys per point   64  (those missing will be set to ~0)
 void CMS_1JET_8TEVFilter::ReadData()
 {
 
-  // Opening files
+  //opening files
   fstream rS, rCorr;
 
-  //Bins specification
+  //bins specification
   int nbins = 6;
   double  y[]  = {0.25, 0.75, 1.25, 1.75, 2.25, 2.75 };
   int ndata[]  = {46, 46, 45, 41, 34, 27};    
-  int nstat[] =  {37, 37, 36, 32, 25, 18};   
+  int artsys[] = {37, 37, 36, 32, 25, 18};  //number of artificial systematics for each bin
+  double stat[fNData];   
 
   int realsys = 27;  // number of real systematics
   int n = 0;         // count total number of datapoints
@@ -78,8 +79,7 @@ void CMS_1JET_8TEVFilter::ReadData()
   for(int bin=0; bin < nbins; bin++ )
   {
 
-    cout << " bin ok --------- " << bin << " ------ " << endl;   
-    // start filter
+    cout << " bin  " << bin << " ------ " << endl;   
     string data_file = "/CMS_8TeV_jets_Ybin" + to_string(bin+1) + ".dat";
     string cov_file =  "/CMS_8TeV_jets_Ybin" + to_string(bin+1) + "___CMS_8TeV_jets_Ybin" + to_string(bin+1) + ".dat";
 
@@ -92,7 +92,7 @@ void CMS_1JET_8TEVFilter::ReadData()
       exit(-1);
     }
 
-    // statistics correlation matrix
+    //statistics correlation matrix
     stringstream DataFileCorr("");
     DataFileCorr << dataPath() << "rawdata/" << fSetName << cov_file;
     rCorr.open(DataFileCorr.str().c_str(), ios::in);
@@ -105,134 +105,154 @@ void CMS_1JET_8TEVFilter::ReadData()
     string line;
     double ptmin, ptmax, dum;
 
-    // pT distribution in the rapidity bin
-  
-    // skip the first 42 lines
+    //pT distribution in the rapidity bin
+
+    //skip the first lines
     getline(rS,line);
-    for (int i = 0; i < 42; i++)  
+    for (int i = 0; i < 41; i++)  
       getline(rS,line);
 
     for (int i = n; i < n + ndata[bin]; i++)
     {
-      cout << " bin  " << bin << " ------ " << endl;
-      getline(rS,line);                      // rS reads all a line of the file in a single go
-      istringstream lstream(line);          // the line is split in its columns, so that rS reads a single value in each go
+      getline(rS,line);                     
+      istringstream lstream(line);          
 
       lstream >> dum >> dum >> dum;
       lstream >> ptmin >> ptmax;
       fKin1[i] = (ptmin + ptmax)*0.5;       // pt, central value of the bin
       fKin2[i] = y[bin];                    // y, central value of the bin
-      fKin3[i] = 8000;                         // sqrt(s)
+      fKin3[i] = 8000;                      // sqrt(s)
 
       lstream >> fData[i] >> dum;    
-      cout << i << " " << fData[i] << endl;
-    
-      //** read first systematics here
-      //** read statystic here
 
-      //then read the other systematics in a loop
-      // put the real systematics first. Rememeber they are expressed as percentage
-      for (int l = 0; l < realsys -1; l++)     
+      //Read the first systematic
+      double shift = 0.;
+      double sys1, sys2, right, left;
+      double stmp, dtmp;
+
+      lstream >> sys1 >> sys2;
+      if(sys1<0) {right=sys2; left=sys1;}
+      else {right=sys1; left=sys2;}
+
+      //the sys read from the file are already in relative percentage values	  
+      symmetriseErrors(right,left,&stmp,&dtmp);
+	  
+      fSys[i][0].type = MULT;   //IS THIS CORRECT??
+      fSys[i][0].name = "CORR";
+      fSys[i][0].mult = stmp;
+      fSys[i][0].add  = fSys[i][0].mult*fData[i]/100;
+
+      shift += dtmp;
+
+      //read statistic
+      lstream >> stat[i];
+      fStat[i] = 0; 
+      //cout << fData[i] << " " << stat[i] << endl;
+
+      //read the systematics 
+      for(int j=1; j<realsys -1; j++)
       {
 
-        cout << " sys ok --------- " << bin << " ------ " << endl;
-        lstream >> fSys[i][l].add;  
-        fSys[i][l].mult = fSys[i][l].add/fData[i]*1e2;
-        fSys[i][l].type = MULT;  
-        fSys[i][l].name = "CORR";
+	  double sys1, sys2, right, left;
+	  double stmp, dtmp;
+
+	  lstream >> sys1 >> sys2;
+
+	  if(sys1<0) {right=sys2; left=sys1;}
+	  else {right=sys1; left=sys2;}
+
+	  //the sys read from the file are already in relative percentage values
+	  symmetriseErrors(right,left,&stmp,&dtmp);
+	  
+	  fSys[i][j].type = MULT;   //IS THIS CORRECT??
+	  fSys[i][j].name = "CORR";
+	  fSys[i][j].mult = stmp;
+	  fSys[i][j].add  = fSys[i][j].mult*fData[i]/100;
+
+	  shift += dtmp;
       }
 
+      fData[i]*=(1.0 + shift*0.01); //Shift from asymmetric errors
 
       // Lumi uncertainty
       lstream >> fSys[i][realsys -1].add;  
-      //fSys[i][fNSys-1].add *= pb2fb;
       fSys[i][realsys -1].mult = fSys[i][realsys -1].add/fData[i]*1e2;
       fSys[i][realsys -1].type = MULT;
       fSys[i][realsys -1].name = "CMSLUMI12";
-
-      // Statistical error
-      fStat[i] = 0;
-      lstream >> fStat[i];      // no, the sist is the one called abs_sis read before. this is the percenatge value probably
-      lstream >> dum;           // last column is a dummy variable....?  yes
-      cout << " totstat ok --------- " << bin << " ------ " << endl;   
     }
-
-
-    
+       
     // Defining covariance matrix for the specific bin
-    double** covmat = new double*[nstat[bin]];
-    for (int i = 0; i < nstat[bin]; i++) 
-      covmat[i] = new double[nstat[bin]];
+    double** covmat = new double*[artsys[bin]];
+    for (int i = 0; i < artsys[bin]; i++) 
+      covmat[i] = new double[artsys[bin]];
 
     // Reading Covariance Matrix
     for (int i = 0; i < 16; i++)
       getline(rCorr,line);
-
-
-    for (int i = 0; i < nstat[bin]; i++){ 
- 
-      for (int j = 0; j < nstat[bin]; j++) { 
+     
+    for (int i = 0; i < artsys[bin]; i++){ 
+      for (int j = 0; j < artsys[bin]; j++) { 
         getline(rCorr,line);                       
         rCorr >> dum >> dum >> dum >> dum;
         rCorr >> covmat[i][j];
-        //covmat[j][i]=covmat[j][i]*totstat[j]*totstat[i];    
+        covmat[j][i] *= stat[n+j+9]*stat[n+i+9];    
       }
       getline(rCorr,line);                       
     }
 
-    for (int i = 0; i < nstat[bin]; i++){ 
- 
-      for (int j = 0; j < nstat[bin]; j++) {    
-
+    /*
+    //print the cov mat to debug
+    for (int i = 0; i < artsys[bin]; i++){ 
+      for (int j = 0; j < artsys[bin]; j++) {    
         cout << covmat[i][j] << " ";
-
       }
       cout << endl;
     }
+    */
 
-    if (bin==4) break;
-
-    
-    cout << " reading ok --------- " << bin << " ------ " << endl;   
     // Generate artificial systematics
-    double** syscor = new double*[nstat[bin]];
-    for(int i = 0; i < nstat[bin]; i++)
-      syscor[i] = new double[nstat[bin]];
+    double** syscor = new double*[artsys[bin]];
+    for(int i = 0; i < artsys[bin]; i++)
+      syscor[i] = new double[artsys[bin]];
 
-    cout << " reading ok --------- " << bin << " ------ " << endl;   
-    if(!genArtSys(nstat[bin],covmat,syscor))
+    if(!genArtSys(artsys[bin],covmat,syscor))
     {
        cerr << " in " << fSetName << " : cannot generate artificial systematics" << endl;
        exit(-1);
     }
-    cout << " generated ok --------- " << bin << " ------ " << endl;   
-  
+
     // Copy the artificial systematics in the fSys matrix
-    for (int i = n; i < n + nstat[bin]; i++)
-      for (int l = realsys; l < realsys + nstat[bin]; l++)     // -1 is due to the fact that i have already put the lumninosity uncertainty
+    for (int i = n; i < n + artsys[bin]; i++)
+    {
+      for (int l = realsys; l < realsys + artsys[bin]; l++)  
       {
-        cout << "qui " << endl;
-        fSys[i][l].add  = syscor[i-n][l-n];
+        fSys[i][l].add  = syscor[i-n][l-realsys];
         fSys[i][l].mult = fSys[i][l].add/fData[i]*1e2;
         fSys[i][l].type = MULT;  
         fSys[i][l].name = "CORR";
       }
 
-    for(int i = 0; i < nstat[bin]; i++) 
-      delete[] syscor[i];
-    delete[] syscor;
+      // Put the remaining systematics to ~1e-3
+      for (int l = realsys + artsys[bin]; l < fNSys; l++)
+      {
+        fSys[i][l].add  = 1e-3;
+        fSys[i][l].mult = fSys[i][l].add/fData[i]*1e2;
+        fSys[i][l].type = MULT;  
+        fSys[i][l].name = "CORR";
+      }
+    }     
 
-    
-    for(int i = 0; i < nstat[bin]; i++) 
-      delete[] covmat[i];
+    for(int i = 0; i < artsys[bin]; i++) 
+     delete[] syscor[i];
+    delete[] syscor;
+  
+    for(int i = 0; i < artsys[bin]; i++) 
+     delete[] covmat[i];
     delete[] covmat;
     
-
     rS.close();
-    rCorr.close();
-
+    rCorr.close();    
     n += ndata[bin];
-  } 
-   
+  }    
 }
 
