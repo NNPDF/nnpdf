2832be146f3502538cdf0399c49dac9ca0e89d07
Zahari <zaharid@gmail.com>
2019-05-24 23:40:06 +0100

    Use NNPDF servers to upload packages

--- .ciscripts/build-deploy-osx.sh
+++ .ciscripts/build-deploy-osx.sh
@@ -19,15 +19,15 @@ set the UPLOAD_NON_MASTER variable."
 	exit 0
 fi
 
-echo "Uploading package to zigzah"
+echo "Uploading package to the NNPDF server"
 #Idiotic mac mktemp
 KEY=$( mktemp  "${TMPDIR:-/tmp}/key.XXXXXXXXX" )
 #This is defined in the Gitlab variables, under the Settings Menu.
-echo "$ZIGZAH_SSH_KEY" | base64 --decode > "$KEY"
+echo "$NNPDF_SSH_KEY" | base64 --decode > "$KEY"
 
 scp -i "$KEY" -o StrictHostKeyChecking=no\
     ${CONDAPATH}/conda-bld/${OUTPUT_ARCH}/*.tar.bz2 \
-    dummy@zigzah.com:~/${OUTPUT_CHANNEL}/${OUTPUT_ARCH}
+    dummy@packages.nnpdf.science:~/packages/${OUTPUT_CHANNEL}/${OUTPUT_ARCH}
 
 if [ $? == 0 ]; then
 	echo "Upload suceeded"
