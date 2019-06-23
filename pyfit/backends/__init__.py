cc75797e3395a47635f22153f6d503ac6ca3fa96
juacrumar <juacrumar@lairen.eu>
2019-06-23 12:37:54 +0200

    deleted forgotten file

--- pyfit/backends/__init__.py
+++
@@ -1,8 +0,0 @@
-from backends.keras_backend.debug_mode import set_initial_state, clear_backend_state
-from backends.keras_backend.MetaLayer import MetaLayer
-from backends.keras_backend.MetaModel import MetaModel
-from backends.keras_backend.base_layers import concatenate, Lambda, base_layer_selector
-from backends.keras_backend import losses
-from backends.keras_backend import operations
-
-print("Using Keras backend")
