cb176e9e3378fc6adda0ab80c47570989b028725
siranipour <si292@cam.ac.uk>
2019-08-29 10:40:45 +0200

    implementing tab autocomplete

---
+++ autocomplete.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import sys
+
+from importlib import import_module
+
+from reportengine.utils import get_providers
+from validphys.app import providers
+
+
+def candidates():
+    dictionary = [get_providers(import_module(m)) for m in providers]
+    functions = [item for sublist in [list(i.keys()) for i in dictionary] 
+             for item in sublist]    
+    return functions
+
+
+def completion_hook(cmd, curr_word, prev_word):
+    potential_matches = candidates()
+    matches = [k for k in potential_matches if k.startswith(curr_word)]
+    return matches
+
+
+def main():
+    if sys.argv[-1] not in ["-h", "--help"]:
+        return
+
+    results = completion_hook(*sys.argv[1:])
+    if len(results):
+          print("\n".join(results))
+
+
+if __name__ == "__main__":
+    main()
