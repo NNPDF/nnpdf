453963eb51e57b5656645742d366640c812b845c
siranipour <si292@cam.ac.uk>
2019-09-05 12:46:42 +0100

    Writing providers to a text file

--- autocomplete.py
+++ autocomplete.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+from os import path
 
 from importlib import import_module
 
@@ -8,9 +9,16 @@ from validphys.app import providers
 
 
 def candidates():
-    dictionary = [get_providers(import_module(m)) for m in providers]
-    functions = [item for sublist in [list(i.keys()) for i in dictionary] 
-             for item in sublist]    
+    if not path.exists("./providers.txt"):
+        dictionary = [get_providers(import_module(m)) for m in providers]
+        functions = [item for sublist in [list(i.keys()) for i in dictionary]
+                    for item in sublist]
+        with open("providers.txt", "w") as stream:
+            for function in functions:
+                stream.write(f"{function}\n")
+    else:
+        with open("providers.txt", "r") as stream:
+            functions = stream.read().split("\n")
     return functions
 
 
@@ -26,7 +34,7 @@ def main():
 
     results = completion_hook(*sys.argv[1:])
     if len(results):
-          print("\n".join(results))
+        print("\n".join(results))
 
 
 if __name__ == "__main__":
