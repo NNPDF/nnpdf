f33110295db2d2728c710449e642d7cca06fa7b8
Emanuele Nocera <noceral@tplxlap211.nat.physics.ox.ac.uk>
2019-11-03 00:24:11 +0100

    Removed top-pair pt distribution, corrected missing factor in the invariant mass distribution

--- filters/ATLAS_TOPDIFF_DILEPT_8TEV.cc
+++ filters/ATLAS_TOPDIFF_DILEPT_8TEV.cc
@@ -7,15 +7,23 @@ Record in: INSPIRE
 Record in: HEPData https://www.hepdata.net/record/ins1477814
 
 Description of the measurement 
-Measurements of normalized differential cross-sections of top quark pair (tt¯) production are presented as a function of the mass, the transverse momentum and the rapidity of the tt¯ system in proton-proton collisions at center-of-mass energies of s√ = 8 TeV. The dataset corresponds to an integrated luminosity of 20.2 fb−1 at 8 TeV, recorded with the ATLAS detector at the Large Hadron Collider. Events with top quark pair signatures are selected in the dilepton final state, requiring exactly two charged leptons and at least two jets with at least one of the jets identified as likely to contain a b-hadron. The measured distributions are corrected for detector effects and selection efficiency to cross-sections at the parton level.
+Measurements of normalized differential cross-sections of top quark pair (tt¯) 
+production are presented as a function of the mass, the transverse momentum and 
+the rapidity of the tt¯ system in proton-proton collisions at center-of-mass 
+energies of s√ = 8 TeV. The dataset corresponds to an integrated luminosity of 
+20.2 fb−1 at 8 TeV, recorded with the ATLAS detector at the Large Hadron 
+Collider. Events with top quark pair signatures are selected in the dilepton 
+final state, requiring exactly two charged leptons and at least two jets with 
+at least one of the jets identified as likely to contain a b-hadron. 
+The measured distributions are corrected for detector effects and selection 
+efficiency to cross-sections at the parton level.
 
 Description of the buildmaster implementation
 Normalized and unnormalised cross sections for the distributions 
 (dilepton channel)
 differential in the following variables are implemented:
-1) top quark pair invariant mass;                  
-2) top quark pair transverse momentum;             
-3) top quark pair rapidity.       
+1) top quark pair invariant mass;                   
+2) top quark pair rapidity.       
 
 Raw data and full breakdown of systematic uncertainties are from HepData:
 https://www.hepdata.net/record/ins1477814
@@ -23,11 +31,12 @@ N: normalised
 U: unnormalised
 
 1N) TABS 4, 16 HepData; TAB 5 in the paper
-2N) TABS 5, 17 HepData; TAB 5 in the paper
-3N) TABS 6, 18 HepData; TAB 5 in the paper
+2N) TABS 6, 18 HepData; TAB 5 in the paper
 1U) TABS 10, 22 HepData
-2U) TABS 11, 23 HepData
-3U) TABS 12, 24 HepData
+2U) TABS 12, 24 HepData
+
+Missing lumi : 1.9%
+
 
 */
  
@@ -89,6 +98,7 @@ void  ATLAS_TOPDIFF_DILEPT_8TEV_TTMNORMFilter::ReadData()
       fStat[i] = 0;
       
       lstream >> fData[i]; //normalized differential distribution
+      fData[i] *= 1e-3;
 
       for(int j=0; j<4; j++)
 	{
@@ -113,6 +123,7 @@ void  ATLAS_TOPDIFF_DILEPT_8TEV_TTMNORMFilter::ReadData()
     for(int j=0; j<fNData; j++)
     {
       lstream >> covmat[i][j];
+      covmat[i][j] *= 1e-6;
     }
   }
 
@@ -132,7 +143,7 @@ void  ATLAS_TOPDIFF_DILEPT_8TEV_TTMNORMFilter::ReadData()
       for(int j=0; j<fNData; j++)
 	{
 	  fSys[i][j].add  = syscor[i][j];
-	  fSys[i][j].mult  = fSys[i][j].add*1e2/fData[i];
+	  fSys[i][j].mult = fSys[i][j].add*1e2/fData[i];
 	  fSys[i][j].type = ADD;
 	  fSys[i][j].name = "CORR";
 	}
@@ -158,130 +169,7 @@ void  ATLAS_TOPDIFF_DILEPT_8TEV_TTMNORMFilter::ReadData()
 
 // -------------------------------------------------------------------
 
-//2) Distribution differential in top quark pair transverse momentum
-void  ATLAS_TOPDIFF_DILEPT_8TEV_TTPTNORMFilter::ReadData()
-{
-  //Opening files
-  fstream f1, f2;
-
-  //Central values and statistical uncertainties
-  stringstream datafile("");
-  datafile << dataPath() 
-	   << "rawdata/ATLAS_TOPDIFF_DILEPT_8TEV_TTPTNORM/tab5.txt";
-  f1.open(datafile.str().c_str(), ios::in);
-
-  if (f1.fail()) 
-    {
-      cerr << "Error opening data file " << datafile.str() << endl;
-      exit(-1);
-    }
-
-  //Covariance matrix
-  stringstream covfile("");
-  covfile << dataPath()
-	  << "rawdata/ATLAS_TOPDIFF_DILEPT_8TEV_TTPTNORM/tab17.txt";
-  f2.open(covfile.str().c_str(), ios::in);
-
-  if (f2.fail()) 
-    {
-      cerr << "Error opening covariance matrix file " << datafile.str() << endl;
-      exit(-1);
-    }
-  
-
-  //Read central values 
-  string line;
-  for(int i=0; i<8; i++)
-    {
-      getline(f1,line);
-    }
-
-  for(int i=0; i<fNData; i++)
-    {
-      getline(f1,line);
-      istringstream lstream(line);
-      double adum;
-      double pttt;
-      lstream >> pttt;
-      lstream >> adum;
-      lstream >> adum;
-      
-      fKin1[i] = pttt;   
-      fKin2[i] = Mt*Mt;       
-      fKin3[i] = 8000;     //sqrt(s)
-      fStat[i] = 0;
-      
-      lstream >> fData[i]; //normalized differential distribution
-
-      for(int j=0; j<4; j++)
-	{
-	  lstream >> adum;
-	}
-    }
-
-  //Read covariance matrix
-  for(int i=0; i<9; i++)
-    {
-      getline(f2,line);
-    }
-
-  double** covmat = new double*[fNData];
-  for(int i=0; i<fNData; i++)
-  {
-    covmat[i] = new double[fNData];
-    getline(f2,line);
-    istringstream lstream(line);
-    double adum;
-    lstream >> adum >> adum >> adum;
-    for(int j=0; j<fNData; j++)
-    {
-      lstream >> covmat[i][j];
-    }
-  }
-
-
-  //Generate artificial systematics
-  double** syscor = new double*[fNData];
-  for(int i = 0; i < fNData; i++)
-    syscor[i] = new double[fNData];
-
-  if(!genArtSys(fNData,covmat,syscor))
-    {
-      throw runtime_error("Couldn't generate artificial systematics for " + fSetName);
-    }
-
-  for(int i=0; i<fNData; i++)
-    {
-      for(int j=0; j<fNData; j++)
-	{
-	  fSys[i][j].add  = syscor[i][j];
-	  fSys[i][j].mult  = fSys[i][j].add*1e2/fData[i];
-	  fSys[i][j].type = ADD;
-	  fSys[i][j].name = "CORR";
-	}
-    }
-
-
-  // Clean-up
-  for (int i=0; i<fNData; i++)
-    delete[] syscor[i];
-
-  delete[] syscor;
-
-  for(int i=0; i<fNData; i++)
-    delete[] covmat[i];
-
-  delete[] covmat;
-
-
-  f1.close();
-  f2.close();
-
-}
-
-// -------------------------------------------------------------------
-
-//1) Distribution differential in top quark pair rapidity
+//2) Distribution differential in top quark pair rapidity
 void  ATLAS_TOPDIFF_DILEPT_8TEV_TTRAPNORMFilter::ReadData()
 {
   //Opening files
@@ -529,130 +417,7 @@ void  ATLAS_TOPDIFF_DILEPT_8TEV_TTMFilter::ReadData()
 
 // -------------------------------------------------------------------
 
-//2) Distribution differential in top quark pair transverse momentum
-void  ATLAS_TOPDIFF_DILEPT_8TEV_TTPTFilter::ReadData()
-{
-  //Opening files
-  fstream f1, f2;
-
-  //Central values and statistical uncertainties
-  stringstream datafile("");
-  datafile << dataPath() 
-	   << "rawdata/ATLAS_TOPDIFF_DILEPT_8TEV_TTPT/tab11.txt";
-  f1.open(datafile.str().c_str(), ios::in);
-
-  if (f1.fail()) 
-    {
-      cerr << "Error opening data file " << datafile.str() << endl;
-      exit(-1);
-    }
-
-  //Covariance matrix
-  stringstream covfile("");
-  covfile << dataPath()
-	  << "rawdata/ATLAS_TOPDIFF_DILEPT_8TEV_TTPT/tab23.txt";
-  f2.open(covfile.str().c_str(), ios::in);
-
-  if (f2.fail()) 
-    {
-      cerr << "Error opening covariance matrix file " << datafile.str() << endl;
-      exit(-1);
-    }
-  
-
-  //Read central values 
-  string line;
-  for(int i=0; i<8; i++)
-    {
-      getline(f1,line);
-    }
-
-  for(int i=0; i<fNData; i++)
-    {
-      getline(f1,line);
-      istringstream lstream(line);
-      double adum;
-      double pttt;
-      lstream >> pttt;
-      lstream >> adum;
-      lstream >> adum;
-      
-      fKin1[i] = pttt;   
-      fKin2[i] = Mt*Mt;       
-      fKin3[i] = 8000;     //sqrt(s)
-      fStat[i] = 0;
-      
-      lstream >> fData[i]; 
-
-      for(int j=0; j<4; j++)
-	{
-	  lstream >> adum;
-	}
-    }
-
-  //Read covariance matrix
-  for(int i=0; i<9; i++)
-    {
-      getline(f2,line);
-    }
-
-  double** covmat = new double*[fNData];
-  for(int i=0; i<fNData; i++)
-  {
-    covmat[i] = new double[fNData];
-    getline(f2,line);
-    istringstream lstream(line);
-    double adum;
-    lstream >> adum >> adum >> adum;
-    for(int j=0; j<fNData; j++)
-    {
-      lstream >> covmat[i][j];
-    }
-  }
-
-
-  //Generate artificial systematics
-  double** syscor = new double*[fNData];
-  for(int i = 0; i < fNData; i++)
-    syscor[i] = new double[fNData];
-
-  if(!genArtSys(fNData,covmat,syscor))
-    {
-      throw runtime_error("Couldn't generate artificial systematics for " + fSetName);
-    }
-
-  for(int i=0; i<fNData; i++)
-    {
-      for(int j=0; j<fNData; j++)
-	{
-	  fSys[i][j].add  = syscor[i][j];
-	  fSys[i][j].mult  = fSys[i][j].add*1e2/fData[i];
-	  fSys[i][j].type = ADD;
-	  fSys[i][j].name = "CORR";
-	}
-    }
-
-
-  // Clean-up
-  for (int i=0; i<fNData; i++)
-    delete[] syscor[i];
-
-  delete[] syscor;
-
-  for(int i=0; i<fNData; i++)
-    delete[] covmat[i];
-
-  delete[] covmat;
-
-
-  f1.close();
-  f2.close();
-
-}
-
-// -------------------------------------------------------------------
-
-//1) Distribution differential in top quark pair rapidity
+//2) Distribution differential in top quark pair rapidity
 void  ATLAS_TOPDIFF_DILEPT_8TEV_TTRAPFilter::ReadData()
 {
   //Opening files
--- inc/ATLAS_TOPDIFF_DILEPT_8TEV.h
+++ inc/ATLAS_TOPDIFF_DILEPT_8TEV.h
@@ -13,15 +13,6 @@
 // ******************************************************
 
 //Normalised distributions
-class ATLAS_TOPDIFF_DILEPT_8TEV_TTPTNORMFilter: public CommonData
-{
-public: ATLAS_TOPDIFF_DILEPT_8TEV_TTPTNORMFilter():
-  CommonData("ATLAS_TOPDIFF_DILEPT_8TEV_TTPTNORM") { ReadData(); }
-
-private:
-  void ReadData();
-};
-
 class ATLAS_TOPDIFF_DILEPT_8TEV_TTRAPNORMFilter: public CommonData
 {
 public: ATLAS_TOPDIFF_DILEPT_8TEV_TTRAPNORMFilter():
@@ -41,15 +32,6 @@ private:
 };
 
 //Unnormalised distributions
-class ATLAS_TOPDIFF_DILEPT_8TEV_TTPTFilter: public CommonData
-{
-public: ATLAS_TOPDIFF_DILEPT_8TEV_TTPTFilter():
-  CommonData("ATLAS_TOPDIFF_DILEPT_8TEV_TTPT") { ReadData(); }
-
-private:
-  void ReadData();
-};
-
 class ATLAS_TOPDIFF_DILEPT_8TEV_TTRAPFilter: public CommonData
 {
 public: ATLAS_TOPDIFF_DILEPT_8TEV_TTRAPFilter():
