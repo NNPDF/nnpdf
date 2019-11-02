c4fac877dc5313e95bf5b8e3183c710e20a7e5c7
Emanuele Nocera <noceral@tplxlap211.nat.physics.ox.ac.uk>
2019-11-03 00:25:27 +0100

    Removed top pair distribution

--- src/buildmaster.cc
+++ src/buildmaster.cc
@@ -325,11 +325,9 @@ void InitCommonData(vector<CommonData*>& target)
   target.push_back(new ATLASTOPDIFF8TEVTTMNORMFilter());
 
   target.push_back(new ATLAS_TOPDIFF_DILEPT_8TEV_TTMFilter());
-  target.push_back(new ATLAS_TOPDIFF_DILEPT_8TEV_TTPTFilter());
   target.push_back(new ATLAS_TOPDIFF_DILEPT_8TEV_TTRAPFilter());
 
   target.push_back(new ATLAS_TOPDIFF_DILEPT_8TEV_TTMNORMFilter());
-  target.push_back(new ATLAS_TOPDIFF_DILEPT_8TEV_TTPTNORMFilter());
   target.push_back(new ATLAS_TOPDIFF_DILEPT_8TEV_TTRAPNORMFilter());
 
   // ************************ CMS TTBAR DIFF TeV  ***************
