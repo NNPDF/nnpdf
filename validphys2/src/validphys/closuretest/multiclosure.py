1167452d05c76e017f8e073f03bb02609783e4db
wilsonm <michael.wilson@ed.ac.uk>
2020-01-23 12:32:16 +0000

    added multiclosure pdf plots

--- validphys2/src/validphys/closuretest/multiclosure.py
+++ validphys2/src/validphys/closuretest/multiclosure.py
@@ -7,9 +7,17 @@ averaged across multiple fits or a single replica proxy fit
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from reportengine import collect
 from reportengine.table import table
+from reportengine.figure import figuregen
+from reportengine.checks import make_argcheck
+
+from validphys.pdfgrids import xplotting_grid, check_basis
+from validphys.pdfbases import Basis
+from validphys.core import PDF
+from validphys.checks import check_scale
 
 fits_bias_experiment = collect("bias_experiment", ("fits", "fitcontext"))
 
@@ -56,3 +64,65 @@ def experiments_bias_variance_ratio(
     )
     df.columns = ["bias/variance"]
     return df
+
+@make_argcheck(check_basis)
+def histogram_pdfgrid(
+    pdf:PDF, Q:(float,int), basis:(str, Basis)='flavour',
+    flavours:(list, tuple, type(None))=None):
+    xgrid = np.array([0.05, 0.1, 0.2]) # internal grid, defined in 3.0 paper
+    return xplotting_grid(
+        pdf, Q, xgrid=xgrid, basis=basis, flavours=flavours)
+
+@make_argcheck(check_basis)
+def xi_pdfgrid(
+    pdf:PDF, Q:(float,int), basis:(str, Basis)='flavour',
+    flavours:(list, tuple, type(None))=None):
+    xgrid_log = np.logspace(-5, -1, num=10, endpoint=False)
+    xgrid_lin = np.linspace(0.1, 1, num=10, endpoint=False)
+    xgrid = np.concatenate((xgrid_log, xgrid_lin), axis=0)
+    return xplotting_grid(
+        pdf, Q, xgrid=xgrid, basis=basis, flavours=flavours)
+
+fits_xi_pdfgrid = collect("xi_pdfgrid", ("fits", "fitpdf"))
+underlying_xi_pdfgrid = collect("xi_pdfgrid", ("fitunderlyinglaw",))
+
+@table
+def xi_1sigma(fits_xi_pdfgrid, underlying_xi_pdfgrid):
+    fit_replica_grids = np.array([xpg.grid_values for xpg in fits_xi_pdfgrid])
+    fit_central_grids = fit_replica_grids.mean(axis=1, keepdims=True)
+    law_grid = underlying_xi_pdfgrid[0].grid_values[0][np.newaxis, np.newaxis, ...]
+    #print(law_grid.shape, fit_replica_grids.shape, fit_central_grids.shape)
+    central_diff = law_grid - fit_central_grids
+    sigma_diff = fit_replica_grids - fit_central_grids
+    variance = ((sigma_diff)**2).mean(axis=1, keepdims=True)#.mean(axis=0)
+    sigma = np.sqrt(variance)
+    indicator = np.array(abs(central_diff) < sigma, dtype=float) #fit, 1, flav, x
+    flavs = pd.Index([
+        underlying_xi_pdfgrid[0].basis.elementlabel(fl)
+        for fl in underlying_xi_pdfgrid[0].flavours],
+        name="flavour"
+    )
+    xgridindex = pd.Index(underlying_xi_pdfgrid[0].xgrid, name="x")
+    df = pd.DataFrame(
+        indicator.mean(axis=0)[0, ...],
+        index=flavs,
+        columns=xgridindex,
+    )
+    return df
+
+@figuregen
+@check_scale('xscale', allow_none=True)
+def plot_xi_1sigma(xi_1sigma, Q, xscale):
+    x = xi_1sigma.columns.values
+    xi_data = xi_1sigma.values
+    for i, fl in enumerate(xi_1sigma.index.values):
+        fig, ax = plt.subplots()
+        ax.plot(x, xi_data[i, :], '*', label=r"$\xi_{1\sigma}$ = "+f"{xi_data[i, :].mean()}")
+        ax.axhline(0.68, linestyle=":", color="k", label="expected value")
+        ax.set_ylim([0, 1])
+        ax.set_title(r"$\xi_{1\sigma}$"+f" for Q={Q}, ${fl}$ PDF.")
+        ax.set_xlabel("x")
+        ax.set_ylabel(r"$\xi_{1\sigma}$")
+        ax.set_xscale(xscale)
+        ax.legend()
+        yield fig
