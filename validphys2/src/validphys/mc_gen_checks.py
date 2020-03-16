6eb28735d1f680997c9fa342017e53595531547e
RosalynLP <rosalynlaurap@gmail.com>
2020-03-16 17:03:55 +0000

    function to plot deviation from mean

--- validphys2/src/validphys/mc_gen_checks.py
+++ validphys2/src/validphys/mc_gen_checks.py
@@ -16,7 +16,6 @@ from scipy.stats import moment as mom
 from NNPDF import Experiment, RandomGenerator
 from reportengine.table import table
 from reportengine.figure import figure
-
 log = logging.getLogger(__name__)
 
 
@@ -52,19 +51,39 @@ def art_rep_generation(experiments, nreplica:int, experiments_index):
         return real_data, art_replicas, normart_replicas, art_data
 
 def per_point_art_rep_generation(experiments, nreplica:int, experiments_index):
-    """Generates artificial replicas separately for each data point"""
+    """Generates the nreplica pseudodata replicas for a given experiment"""
 
     RandomGenerator.InitRNG(0,0)
 
     for exp in experiments:
+        #Since we are going to modify the experiments, we copy them
+        #(and work on the copies) to avoid all
+        #sorts of weirdness with other providers. We don't want this to interact
+        #with ExperimentSpec at all, because it could do funny things with the
+        #cache when calling load(). We need to copy this yet again, for each
+        # of the noisy replicas.
         real_exp = Experiment(exp.load())
-        replica_exp = Experiment(real_exp)
 
-    from IPython import embed
-    embed()
-    return 0
+        art_replicas = []
+        normart_replicas = []
+        real_data = real_exp.get_cv()
+
+        # producing replicas
+        for i in range(nreplica):
+            replica_exp = Experiment(real_exp)
+            for point in range(len(replica_exp.get_cv())):
+                replica_exp.MakePerPointReplica(point)
+            artrep = replica_exp.get_cv()
+            normartrep = artrep/real_data
+            art_replicas.append(artrep)
+            normart_replicas.append(normartrep)
+
+        art_data = np.mean(art_replicas, axis=0)
+
+        return real_data, art_replicas, normart_replicas, art_data
+
 @figure
-def art_data_residuals(art_rep_generation, nreplica:int):
+def art_data_residuals(art_rep_generation, nreplica:int, color="green"):
 
     #pass
     """
@@ -76,7 +95,7 @@ def art_data_residuals(art_rep_generation, nreplica:int):
     normresiduals = residuals/real_data
     fig, ax = plt.subplots()
 
-    ax.hist(normresiduals,bins=50,histtype='step', stacked=True, fill=False)
+    ax.hist(normresiduals,bins=50,histtype='step', stacked=True, fill=False, color=color)
 
     ax.set_ylabel(r'Data points')
     ax.set_xlabel(r'$(D^0-<D^{(r)}>)/D^0$')
@@ -84,9 +103,13 @@ def art_data_residuals(art_rep_generation, nreplica:int):
 
     return fig
 
+@figure
+def per_point_art_data_residuals(per_point_art_rep_generation, nreplica:int):
+    return art_data_residuals(per_point_art_rep_generation, nreplica, color="orange")
+
 
 @figure
-def art_data_distribution(art_rep_generation, nreplica:int):
+def art_data_distribution(art_rep_generation, nreplica:int, title='Artificial Data Distribution', color="green"):
     """
     Plot of the distribution of pseudodata.
     """
@@ -95,16 +118,21 @@ def art_data_distribution(art_rep_generation, nreplica:int):
     normart_data = art_data/real_data
     fig, ax = plt.subplots()
 
-    ax.hist(normart_data, bins=50, histtype='step', stacked=True, fill=False)
+    ax.hist(normart_data, bins=50, histtype='step', stacked=True, fill=False, color=color)
 
     ax.set_ylabel(r'Data points')
     ax.set_xlabel(r'$<D^{(r)}>/D^0$')
-    ax.set_title(r'Artificial Data Distribution')
+    ax.set_title(title)
 
     return fig
 
 @figure
-def art_data_moments(art_rep_generation, nreplica:int):
+def per_point_art_data_distribution(per_point_art_rep_generation, nreplica:int):
+    return art_data_distribution(per_point_art_rep_generation, nreplica, title='Uncorrelated Artificial Data Distribution', color="orange")
+
+
+@figure
+def art_data_moments(art_rep_generation, nreplica:int, color="green"):
     """
     Returns the moments of the distributions per data point, as a histogram.
     """
@@ -122,11 +150,15 @@ def art_data_moments(art_rep_generation, nreplica:int):
         for i, datapoint, normartdatapoint in zip(range(len(artrep_array.T)), artrep_array.T, normart_data):
             moment = mom(datapoint, moment=momno)
             moms.append(moment)
-        ax.hist(moms, bins=50, histtype='step', stacked=True, fill=False)
+        ax.hist(moms, bins=50, histtype='step', stacked=True, fill=False, color=color)
         ax.set_ylabel("Data points")
         ax.set_xlabel(f"Moment {momno}")
 
     return fig
+
+@figure
+def per_point_art_data_moments(per_point_art_rep_generation, nreplica:int):
+    return art_data_moments(per_point_art_rep_generation, nreplica, color="orange")
 
 @figure
 def art_data_comparison(art_rep_generation, nreplica:int):
@@ -159,6 +191,7 @@ def art_data_comparison(art_rep_generation, nreplica:int):
 
     return fig
 
+
 @figure
 def one_art_data_residuals(art_rep_generation, nreplica:int, experiments):
 
@@ -189,6 +222,26 @@ def one_art_data_residuals(art_rep_generation, nreplica:int, experiments):
     ax.set_ylabel(r'replicas')
     ax.set_xlabel(r'$(D^{(r)}_{0} - D^0_{0})/D^0_{0}$')
     ax.set_title(r'Residual for Data Point 0')
+
+    return fig
+
+@figure
+def plot_deviation_from_mean(art_rep_generation, per_point_art_rep_generation, nreplica:int, experiments):
+    real_data, art_replicas, normart_replicas, art_data = art_rep_generation
+    ppreal_data, ppart_replicas, ppnormart_replicas, ppart_data = per_point_art_rep_generation
+
+    fig, ax = plt.subplots()
+
+    residuals = (real_data - art_data)/np.std(art_replicas, axis=0)
+    ppresiduals = (real_data - ppart_data)/np.std(ppart_replicas, axis=0)
+
+    ax.plot(ppresiduals, color="orange", label="Uncorrelated")
+    ax.plot(residuals, color="green", label="Correlated")
+    ax.legend()
+    ax.set_xlabel("Datapoint index")
+    ax.set_ylabel(r"$(<D> - D_0)/\sigma$")
+    ax.set_title("Deviation from the mean")
+    ax.hlines(0, ax.get_xlim()[0], ax.get_xlim()[1], linestyle="-", color="black")
 
     return fig
 
