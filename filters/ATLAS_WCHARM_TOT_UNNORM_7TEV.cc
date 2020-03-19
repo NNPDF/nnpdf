f98d8448bf8c5c6dd2ffcfc2113702692f8f0757
siranipour <si292@cam.ac.uk>
2020-03-19 14:27:31 +0000

    Adding systematics uncertainties

--- filters/ATLAS_WCHARM_TOT_UNNORM_7TEV.cc
+++ filters/ATLAS_WCHARM_TOT_UNNORM_7TEV.cc
@@ -3,12 +3,11 @@
 *   Process: proton + proton -> W + charm
 *   Center of mass energy = 7 TeV
 *   Intergrated luminosity  = 4.6 1/fb
-*   Reference: https://arxiv.org/abs/1402.6263
-*   HEP Data: http://hepdata.cedar.ac.uk/view/ins1282447
+*   Reference: https://link.springer.com/content/pdf/10.1007/JHEP05(2014)068.pdf
+*   HEP Data: https://www.hepdata.net/record/ins1282447
+*             with data taken from Tables 1, 12, and 13
 *
-*   The raw data file is formatted as:
-*   
-*   The systematics are as follows:
+*   The two raw data file correspond to c and cbar jets.
 *   
 */
 #include "ATLAS_WCHARM_TOT_UNNORM_7TEV.h"
@@ -27,7 +26,7 @@ void ATLAS_WCHARM_TOT_UNNORM_7TEVFilter::ReadData()
   }
 
   stringstream datafileWM("");
-  datafileWP << dataPath() 
+  datafileWM << dataPath() 
        << "rawdata/" << fSetName << "/" << fSetName << "_WM.data";
   f2.open(datafileWM.str().c_str(), ios::in);
   if (f2.fail()) 
@@ -38,28 +37,36 @@ void ATLAS_WCHARM_TOT_UNNORM_7TEVFilter::ReadData()
 
   //Starting filter
   string line;
-  double etamin, etamax;          //pT ranges
-  double MW2 = pow(MW,2.0);     //W mass
+  double etamin, etamax;       //rapidity binning
+  double MW2 = pow(MW, 2.0);   //W mass
   double s = 7000;             //LHC at 7TeV
+  double fSystematics[fNSys];
   for(int i=0; i<fNData;i++)
   {
     getline(f1,line);
     istringstream lstream(line);
     //Reading in an interpretation of each column
-    lstream >> pTmin >> pTmax >> fData[i] >> fStat[i];
+    lstream >> etamin >> etamax >> fData[i] >> fStat[i];
+    for(int k=0; k<fNSys; k++)
+    {
+      lstream >> fSystematics[k];
+    }      
+
     fData[i] = fData[i]*1000; // changing pb to fb for APPLgrid
     fStat[i] = fStat[i]*1000; // changing pb to fb for APPLgrid
     //Defining the kinematic variables
-    fKin1[i] = (pTmin + pTmax)*0.5;    // eta
-    fKin2[i] = MZ2;                      // Mass W squared
+    fKin1[i] = (etamin + etamax)*0.5;    // eta
+    fKin2[i] = MW2;                      // Mass W squared
     fKin3[i] = s;                        // sqrt(s)
-     //Reading in the systematics
-    for(int k=0; k<fNSys;k++) // Factor of 2 because of +sys -sys format of rawdata
+
+    //Reading in the systematics
+    for(int k=0; k<fNSys; k++)
     {
-      lstream >> fSys[i][k].mult;
+      fSys[i][k].mult = fSystematics[k];
+      std::cout << fSys[i][k].mult << std::endl;
       fSys[i][k].type = MULT;
       if(k == 0){
-        fSys[i][k].name = "CMSLUMI12"; //We treat luminosity as a special case
+        fSys[i][k].name = "UNCORR"; //We treat luminosity as a special case
       }
       else{
         fSys[i][k].name = "CORR";
