794b3928d5a68253668f8b6b2f7e12e25fb8157b
RosalynLP <33020850+RosalynLP@users.noreply.github.com>
2020-03-19 12:04:36 +0000

    Update experiments.h

--- libnnpdf/src/NNPDF/experiments.h
+++ libnnpdf/src/NNPDF/experiments.h
@@ -31,6 +31,7 @@ namespace NNPDF
     ~Experiment();                  //!< Experiment destructor
 
     void MakeReplica();                           //!< Shift the exp data and produces art. data
+    void MakePerPointReplica();
     void MakeClosure(const std::vector<ThPredictions>& predictions, bool const& noise);
     void MakeClosure(PDFSet* pdf, bool const& noise);         //!< Make fake exp data using theory predictions
 
