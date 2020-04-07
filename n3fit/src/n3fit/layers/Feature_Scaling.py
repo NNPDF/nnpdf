d911154153daac60a26feb47d04be57405ccf236
Roy Stegeman <roystegeman@live.nl>
2020-04-07 13:16:09 +0200

    got rid of sklearn

--- n3fit/src/n3fit/layers/Feature_Scaling.py
+++ n3fit/src/n3fit/layers/Feature_Scaling.py
@@ -8,20 +8,22 @@ class Feature_Scaling(MetaLayer):
     """
         Applies a Normalisation of the x-grid distribution. 
     """
-
+    
     def __init__(self, scale_features=True, **kwargs):
         self.scale_features = scale_features
         if self.scale_features:
+            self.feature_range = (-1,1)
             fake_x = np.concatenate(
                 (np.logspace(-6, -3, num=50, endpoint=False), np.linspace(1e-3, 1, num=50))
             )
-            self.scaler = MinMaxScaler(feature_range=(0.5, 1), copy=False)
-            self.scaler.fit(fake_x.reshape(-1,1))
+            self.scale_ = (self.feature_range[1] - self.feature_range[0])
+            self.min_ = self.feature_range[0] - fake_x.min() * self.scale_
 
         super().__init__(**kwargs)
 
     def call(self, x_raw):
         x = x_raw
         if self.scale_features:
-            self.scaler.transform(x.tensor_content.reshape(-1, 1))
+            x *= self.scale_
+            x += self.min_
         return x
