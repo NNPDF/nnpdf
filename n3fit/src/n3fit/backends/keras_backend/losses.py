b1900bfc652663110e643f248702aa5e813fcee0
Roy Stegeman <roystegeman@live.nl>
2020-04-08 21:02:52 +0200

    weight datapoints based on datapoint density

--- n3fit/src/n3fit/backends/keras_backend/losses.py
+++ n3fit/src/n3fit/backends/keras_backend/losses.py
@@ -4,9 +4,12 @@
 
 import tensorflow as tf
 from tensorflow.keras import backend as K
+from validphys.loader import FallbackLoader as Loader
+import numpy as np
 
+l = Loader() 
 
-def l_invcovmat(invcovmat_np):
+def l_invcovmat(invcovmat_np, losstype="validation", exp_name=None, spec_dict=None):
     """
     Returns a loss function such that:
     L = \sum_{ij} (yt - yp)_{i} invcovmat_{ij} (yt - yp)_{j}
@@ -15,9 +18,30 @@ def l_invcovmat(invcovmat_np):
 
     def true_loss(y_true, y_pred):
         # (yt - yp) * covmat * (yt - yp)
-        tmp = y_true - y_pred
-        right_dot = tf.tensordot(invcovmat, K.transpose(tmp), axes=1)
-        return tf.tensordot(tmp, right_dot, axes=1)
+        # NOTE: if isinstance isn't here we get a NameError: trainmask is not defined 
+        if losstype == 'training' and isinstance(spec_dict, dict):
+            xgrid_experiment = []
+            for dataset_dict in spec_dict.get('datasets'):
+                exp_name=dataset_dict.get('name')
+                datasetspec = l.check_dataset(exp_name, theoryid=53, cuts="internal") 
+                dataset = datasetspec.load()
+                xgrid_dataset = dataset.get_kintable()[:, 0]
+                xgrid_experiment = np.concatenate((xgrid_experiment, xgrid_dataset))
+            trmask = spec_dict.get('trmask')
+            xgrid_training =  []
+            for num, i in enumerate(trmask.reshape(-1)):
+                if i:
+                    xgrid_training.append(xgrid_experiment[num])
+            xgrid_training = np.array(xgrid_training)
+            weight_function = np.sqrt((xgrid_training/5e-6)**0.4)
+            tmp = y_true - y_pred
+            right_dot = tf.tensordot(invcovmat, K.transpose(tmp/weight_function), axes=1)
+            chi2 = tf.tensordot(tmp/weight_function, right_dot, axes=1)
+        else:
+            tmp = y_true - y_pred
+            right_dot = tf.tensordot(invcovmat, K.transpose(tmp), axes=1)
+            chi2 = tf.tensordot(tmp, right_dot, axes=1)
+        return chi2
 
     return true_loss
 
--- n3fit/src/n3fit/layers/Feature_Scaling.py
+++ n3fit/src/n3fit/layers/Feature_Scaling.py
@@ -1,53 +1,51 @@
 import numpy as np
 
 from n3fit.backends import MetaLayer
-
+from tensorflow.keras import backend as K
+import tensorflow as tf
 
 class Feature_Scaling(MetaLayer):
     """
         Applies a Normalisation of the x-grid distribution.
     """
-    
-    def __init__(self, 
-        scaler=False, 
-        feature_range = (-1,1), 
-        with_mean=True, 
-        with_std=True, 
-        **kwargs):
-        
-        scaler_list = ['MinMaxScaler', 'StandardScaler']
+
+    def __init__(
+        self, scaler=False, feature_range=(-1, 1), with_mean=True, with_std=True, **kwargs
+    ):
+
+        scaler_list = ["MinMaxScaler", "StandardScaler", "Test"]
 
         # name scalar type
         self.scaler = scaler
         if self.scaler != False:
-            if self.scaler not in scaler_list: 
-                raise ValueError(f"Feature Scaling does not support \'{self.scaler}\'.")
+            if self.scaler not in scaler_list:
+                raise ValueError(f"Feature Scaling does not support '{self.scaler}'.")
 
         # input options for MinMaxScaler
-        feature_range = feature_range 
+        feature_range = feature_range
 
         # input options for StandardScalar
         self.with_mean = with_mean
         self.with_std = with_std
 
         # Replicate distrubtion on x-grid of the experimental data
-        if self.scaler != False:
-            fake_x = np.concatenate(
-                (np.logspace(-6, -3, num=50, endpoint=False), np.linspace(1e-3, 1, num=50))
-            )
+        # if self.scaler != False:
+        #     fake_x = np.concatenate(
+        #         (np.logspace(-6, -3, num=50, endpoint=False), np.linspace(1e-3, 1, num=50))
+        #     )
 
         # List of scalers
-        if self.scaler == 'MinMaxScaler':
-            self.scale_ = (feature_range[1] - feature_range[0])
+        if self.scaler == "MinMaxScaler":
+            self.scale_ = feature_range[1] - feature_range[0]
             self.min_ = feature_range[0] - fake_x.min() * self.scale_
-        elif self.scaler == 'StandardScaler':
+        elif self.scaler == "StandardScaler":
             if self.with_mean:
                 self.mean_ = np.mean(fake_x)
-            else: 
+            else:
                 self.mean_ = 0
             if self.with_std:
                 self.std_ = np.std(fake_x)
-            else: 
+            else:
                 self.std_ = 1
 
         super().__init__(**kwargs)
@@ -55,13 +53,20 @@ class Feature_Scaling(MetaLayer):
     def call(self, x_raw):
         x = x_raw
 
-        if self.scaler == 'MinMaxScaler':
+        def log10(x):
+            numerator = K.log(x)
+            denominator = K.log( tf.constant(10, dtype=numerator.dtype))
+            return numerator/denominator
+
+        if self.scaler == "MinMaxScaler":
             x *= self.scale_
             x += self.min_
-        elif self.scaler == 'StandardScaler':
+        elif self.scaler == "StandardScaler":
             if self.with_mean:
                 x -= self.mean_
             if self.with_std:
                 x /= self.std_
+        elif self.scaler == "Test":
+            x =  log10(x*1e6)/6
 
         return x
