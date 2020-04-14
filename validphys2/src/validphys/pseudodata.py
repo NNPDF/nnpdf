b280d392206708b98ea9d20dad391f44cbdb55de
siranipour <si292@cam.ac.uk>
2020-04-14 17:01:25 +0100

    Correcting for mapping between fitted replicas and postfit replicas We now write the pseudodata for all fitted replicas to a dictionary which is then mapped from by using the FromMCReplica found in the header file

--- validphys2/src/validphys/pseudodata.py
+++ validphys2/src/validphys/pseudodata.py
@@ -13,15 +13,23 @@ import numpy as np
 log = logging.getLogger(__name__)
 
 
-def get_pseudodata(fit, pdf, experiments, t0pdfset=None, NPROC=None):
+def fitted_pseudodata(fit, experiments, num_fitted_replicas, t0pdfset=None, NPROC=None):
     """A function to obtain information about the pseudodata that went
-        into an N3FIT fit. Note this code runs in parallel to increase efficiency.
+        into an N3FIT fit. Note:
+            - this function returns the pseudodata for the replicas
+              pre-postfit. Postfit discards some replicas and rearranges
+              the order. The correpsondence is done by the get_pseudodata
+              function.
+            - this code runs in parallel to increase efficiency.
 
         Parameters
         ----------
         fit: validphys.core.FitSpec
         experiments:
             List of validphys.core.ExeperimentSpec
+        num_nnfit_replicas: int
+            Provided for by validphys.fitdata. Equal to the number of
+            pre-postfit replicas.
         t0pdfset: validphys.core.PDF
         NPROC: int
             Integer specifying how many cores to run on. Default is
@@ -65,10 +73,9 @@ def get_pseudodata(fit, pdf, experiments, t0pdfset=None, NPROC=None):
     else:
         t0pdfset = None
 
-    # Note: len(pdf) = num replicas + 1
-    # due to rep 0, which we do not consider
-    # here.
-    replica = range(1, len(pdf))
+    # The + 1 coming from the fact that we wish to
+    # include the last replica
+    replica = range(1, num_fitted_replicas + 1)
 
     trvlseed, nnseed, mcseed, genrep = [
         fit.as_input().get("fitting").get(i)
@@ -77,7 +84,7 @@ def get_pseudodata(fit, pdf, experiments, t0pdfset=None, NPROC=None):
 
     seeds = initialize_seeds(replica, trvlseed, nnseed, mcseed, genrep)
 
-    def task(L, mcseeds, trvlseeds):
+    def task(d, mcseeds, trvlseeds, replicas):
         all_exp_infos = [[] for _ in range(len(mcseeds))]
         for exp in experiments:
             all_exp_dicts = reader.common_data_reader(
@@ -85,10 +92,11 @@ def get_pseudodata(fit, pdf, experiments, t0pdfset=None, NPROC=None):
             )
             for i, exp_dict in enumerate(all_exp_dicts):
                 all_exp_infos[i].append(exp_dict)
-        L.extend(all_exp_infos)
+        for i, j in zip(all_exp_infos, replicas):
+            d[j] = i
 
     with mp.Manager() as manager:
-        L = manager.list()
+        d = manager.dict()
 
         if NPROC is None:
             NPROC = mp.cpu_count()
@@ -102,18 +110,26 @@ def get_pseudodata(fit, pdf, experiments, t0pdfset=None, NPROC=None):
         # from type int to numpy int and thus require being converted back
         batched_mcseeds = np.array_split(seeds.mcseeds, NPROC)
         batched_trvlseeds = np.array_split(seeds.trvlseeds, NPROC)
-        for mc_batch, trvl_batch in zip(batched_mcseeds, batched_trvlseeds):
+        batched_replica_num = np.array_split(replica, NPROC)
+        for mc_batch, trvl_batch, replica_batch in zip(
+            batched_mcseeds, batched_trvlseeds, batched_replica_num
+        ):
             p = mp.Process(
                 target=task,
                 args=(
-                    L,
+                    d,
                     list([int(i) for i in mc_batch]),
                     list([int(i) for i in trvl_batch]),
+                    list([int(i) for i in replica_batch]),
                 ),
             )
             p.start()
             processes.append(p)
         for p in processes:
             p.join()
-        pseudodata_dicts = [i for i in L]
+        pseudodata_dicts = dict(d)
     return pseudodata_dicts
+
+
+def get_pseudodata(fitted_pseudodata, fitted_replica_indexes):
+    return [fitted_pseudodata[i] for i in fitted_replica_indexes]
