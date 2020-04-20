b7892a848674c95393d6483e790afed172ce3215
Roy Stegeman <roystegeman@live.nl>
2020-04-20 17:45:09 +0200

    add gussian and flat input scaling

--- n3fit/src/n3fit/layers/Feature_Scaling.py
+++ n3fit/src/n3fit/layers/Feature_Scaling.py
@@ -4,16 +4,40 @@ from n3fit.backends import MetaLayer
 from tensorflow.keras import backend as K
 import tensorflow as tf
 
+fk_dis_datasets=[    
+    'NMCPD_D',
+    # 'NMCPD_P',
+    'NMC',
+    'SLACP',
+    'SLACD',
+    'BCDMSP',
+    'BCDMSD',
+    'CHORUSNU',
+    'CHORUSNB',
+    'NTVNUDMN',
+    'NTVNBDMN',
+    'HERACOMBNCEM',
+    'HERACOMBNCEP460',
+    'HERACOMBNCEP575',
+    'HERACOMBNCEP820',
+    'HERACOMBNCEP920',
+    'HERACOMBCCEM',
+    'HERACOMBCCEP',
+    'HERAF2CHARM',
+    'H1HERAF2B',
+    'ZEUSHERAF2B',
+    ]
+
 class Feature_Scaling(MetaLayer):
     """
         Applies a Normalisation of the x-grid distribution.
     """
 
     def __init__(
-        self, scaler=False, feature_range=(-1, 1), with_mean=True, with_std=True, **kwargs
+        self, scaler=False, **kwargs
     ):
 
-        scaler_list = ["MinMaxScaler", "StandardScaler", "Test"]
+        scaler_list = ["MinMaxScaler", "GaussianScaler"]
 
         # name scalar type
         self.scaler = scaler
@@ -21,37 +45,32 @@ class Feature_Scaling(MetaLayer):
             if self.scaler not in scaler_list:
                 raise ValueError(f"Feature Scaling does not support '{self.scaler}'.")
 
-        # input options for MinMaxScaler
-        feature_range = feature_range
+        def load_fk_tables(datasets):
+            l = Loader() 
+            fk_xgrids = np.array([])
+            for fk_dataset in datasets:
+                print(f'loading {fk_dataset}')
+                fk = l.check_fktable(setname=fk_dataset, theoryID=52, cfac=[])
+                res = load_fktable(fk)
+                fk_xgrids = np.concatenate([fk_xgrids, res.xgrid]) 
+            return fk_xgrids
 
-        # input options for StandardScalar
-        self.with_mean = with_mean
-        self.with_std = with_std
+        if self.scaler == 'MinMaxScaler':
+            scaled_xgrids = load_fk_tables(fk_dis_datasets)
+            self.max_ /= scaled_xgrids.max()
 
-        # Replicate distrubtion on x-grid of the experimental data
-        # if self.scaler != False:
-        #     fake_x = np.concatenate(
-        #         (np.logspace(-6, -3, num=50, endpoint=False), np.linspace(1e-3, 1, num=50))
-        #     )
-
-        # List of scalers
-        if self.scaler == "MinMaxScaler":
-            self.scale_ = feature_range[1] - feature_range[0]
-            self.min_ = feature_range[0] - fake_x.min() * self.scale_
-        elif self.scaler == "StandardScaler":
-            if self.with_mean:
-                self.mean_ = np.mean(fake_x)
-            else:
-                self.mean_ = 0
-            if self.with_std:
-                self.std_ = np.std(fake_x)
-            else:
-                self.std_ = 1
+        
+        elif self.scaler == 'GaussianScaler':
+            scaled_xgrids = load_fk_tables(fk_dis_datasets)
+            min_ = scaled_xgrids.min()
+            self.max_ = scaled_xgrids.max()
+            self.mean = scaled_xgrids.mean()
+            self.scale_ = (1- -1)/(self.max_ - min_)
 
         super().__init__(**kwargs)
 
     def call(self, x_raw):
-        x = x_raw
+        scaled_xgrids = x_raw
 
         def log10(x):
             numerator = K.log(x)
@@ -59,17 +78,18 @@ class Feature_Scaling(MetaLayer):
             return numerator/denominator
 
         if self.scaler == "MinMaxScaler":
-            pass
-        elif self.scaler == "StandardScaler":
-            if self.with_mean:
-                x -= self.mean_
-            if self.with_std:
-                x /= self.std_
-        elif self.scaler == "Test":
-            x = log10(x*1e3)/3
-            # x -= 1.07e-6
-            # x /= (0.75-1.07e-6)
-            # x *= (1 - -1)
-            # x += -1
+            scaled_xgrids = scaled_xgrids + scaled_xgrids**0.4 + scaled_xgrids**0.3 + 0.5*scaled_xgrids**0.2
+            scaled_xgrids /= self.max_
+            scaled_xgrids *= 2
+            scaled_xgrids -= 1
 
-        return x
+        elif self.scaler == "GaussianScaler":
+            scaled_xgrids = scaled_xgrids + scaled_xgrids**0.4 + scaled_xgrids**0.3 + 0.5*scaled_xgrids**0.2
+            
+            scaled_xgrids /= self.max_
+            scaled_xgrids *= 2
+            scaled_xgrids -= 1
+            
+            scaled_xgrids = np.exp((scaled_xgrids-self.mean_)**2 ) * scaled_xgrids
+                
+        return scaled_xgrids
