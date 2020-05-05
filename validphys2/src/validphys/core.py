4fbe4894610285ad10d83ceb5c3b9792509525f9
Rosalyn <rosalynlaurap@gmail.com>
2020-05-05 10:49:26 +0100

    copying code from PR476 for data loading

--- validphys2/src/validphys/core.py
+++ validphys2/src/validphys/core.py
@@ -232,7 +232,25 @@ class PDF(TupleComp):
         else:
             raise RuntimeError("Unknown error type")
 
+def load_dataset(datafile):
+    """Reads commondata file for dataset_name and returns a panda DataFrame with:
+        entry   process kin1    kin2    kin3    data    stat    \
+            sys.add.0   sys.mult.0 .... sys.add.N   sys.mult.N
+    """
+    # read raw commondata file
+    table = pd.read_csv(datafile, sep=r'\s+', skiprows=1, header=None)
 
+    # remove NaNs
+    # TODO: replace commondata files with bad formatting
+    table.dropna(axis='columns', inplace=True)
+
+    # build header
+    header = ['entry', 'process', 'kin1', 'kin2', 'kin3', 'data', 'stat']
+    for i in range((table.shape[1]-len(header))//2):
+        header += [f'sys.add.{i+1}', f'sys.mult.{i+1}']
+    table.columns = header
+    table.set_index('entry', inplace=True)
+    return table
 
 kinlabels_latex = CommonData.kinLabel_latex.asdict()
 _kinlabels_keys = sorted(kinlabels_latex, key=len, reverse=True)
