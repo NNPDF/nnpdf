765f333995f89ac67b09c83cc1076d85ef3edad8
Emanuele Nocera <noceral@tplxlap211.nat.physics.ox.ac.uk>
2020-05-05 13:40:04 +0200

    Added ATLAS WZ diboson measurements, 13 TeV

--- src/buildmaster.cc
+++ src/buildmaster.cc
@@ -101,6 +101,7 @@
 #include "CMS_TTBAR_2D_DIFF.h"
 #include "ATLAS_TTB_DIFF_8TEV_LJ.h"
 #include "ATLAS_Z_3D_8TEV.h"
+#include "ATLAS_WZ_13TEV.h"
 
 /**
  * \param argv the filename containing the configuration
@@ -421,6 +422,13 @@ void InitCommonData(vector<CommonData*>& target)
   target.push_back(new CMS_hxsec_RunII_diff_pTHFilter());
   target.push_back(new CMS_hxsec_RunII_diff_pTH_ggHFilter());
   target.push_back(new CMS_hxsec_RunII_diff_yHFilter());
+
+  // *********************** DIBOSON *********************************
+  target.push_back(new ATLAS_WZ_13TEV_pTZFilter());
+  target.push_back(new ATLAS_WZ_13TEV_pTWFilter());
+  target.push_back(new ATLAS_WZ_13TEV_mTWZFilter());
+  target.push_back(new ATLAS_WZ_13TEV_phiWZFilter());
+  target.push_back(new ATLAS_WZ_13TEV_totWZFilter());
 
   // *********************** ATLAS Z (8 TeV) 3D **********************************
   target.push_back(new ATLAS_Z_3D_EMU_CRAP_8TEVFilter());
