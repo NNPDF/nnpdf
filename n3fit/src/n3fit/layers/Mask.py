b8aec37c2ccdcaa830c576add65224f5449e4482
juacrumar <juacrumar@gmail.com>
2020-05-05 14:35:41 +0200

    make each experiment to be its own model

--- n3fit/src/n3fit/layers/Mask.py
+++ n3fit/src/n3fit/layers/Mask.py
@@ -18,13 +18,22 @@ class Mask(MetaLayer):
             numpy array with the boolean mask to be applied
         c: bool
             constant multiplier for every output
+        batch_it: bool
+            whether to add a size=1 dimension at the beginning
+        unbatch: bool
+            whether to remove the first dimension (size=1) at the beginning
+            if given sets batch_it to False
+        axis: int
+            axis in which to apply the mask
     """
 
-    def __init__(self, bool_mask, c=1.0, batch_it = True, **kwargs):
+    def __init__(self, bool_mask, c=1.0, batch_it = True, unbatch = False, axis = None, **kwargs):
         self.output_dim = np.count_nonzero(bool_mask)
         self.mask = bool_mask
         self.c = c
-        self.batch_it = batch_it
+        self.batch_it = batch_it and not unbatch
+        self.axis = axis
+        self.unbatch = unbatch
         super().__init__(**kwargs)
 
     def build(self, input_shape):
@@ -33,8 +42,9 @@ class Mask(MetaLayer):
         super(Mask, self).build(input_shape)
 
     def meta_call(self, prediction_in):
-        ret = op.boolean_mask(self.kernel * prediction_in, self.mask)
+        ret = op.boolean_mask(self.kernel * prediction_in, self.mask, axis = self.axis)
         if self.batch_it:
-            return op.batchit(ret)
-        else:
-            return ret
+            ret = op.batchit(ret)
+        if self.unbatch:
+            ret = op.unbatch(ret)
+        return ret
