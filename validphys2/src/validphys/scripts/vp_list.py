2644d121360715e3935e6bb6318961aec228b6d4
wilsonm <michael.wilson@ed.ac.uk>
2020-05-08 13:45:28 +0100

    cleaned up script and added option to just check locally or remotely

--- validphys2/src/validphys/scripts/vp_list.py
+++ validphys2/src/validphys/scripts/vp_list.py
@@ -4,9 +4,9 @@ vp-list
 Script which lists available resources locally and remotely
 
 """
-import argparse
 import sys
-import inspect
+import argparse
+from functools import partial
 import re
 import logging
 
@@ -18,6 +18,29 @@ from validphys.config import ConfigError
 log = logging.getLogger()
 log.setLevel(logging.INFO)
 log.addHandler(colors.ColorHandler())
+
+REMOTE_TOKEN = "downloadable_"
+LOCAL_TOKEN = "available_"
+
+
+class ListAction(argparse.Action):
+    def __init__(self, *args, loader, **kwargs):
+        self.loader = loader
+        super().__init__(*args, **kwargs)
+
+    def __call__(self, parser, *args, **kwargs):
+        attrs = dir(self.loader)
+        available = [
+            attr.lstrip(LOCAL_TOKEN) for attr in attrs if attr.startswith(LOCAL_TOKEN)
+        ]
+        downloadable = [
+            attr.lstrip(REMOTE_TOKEN) for attr in attrs if attr.startswith(REMOTE_TOKEN)
+        ]
+        print(
+            "You can check the availability (locally and remotely) of the following resources:\n- "
+            + "\n- ".join(list({*available, *downloadable}))
+        )
+        parser.exit()
 
 
 def atoi(text):
@@ -36,6 +59,9 @@ def natural_keys(text):
     return [atoi(c) for c in re.split(r"(\d+)", text)]
 
 
+sane_order = partial(sorted, key=natural_keys)
+
+
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
@@ -43,29 +69,42 @@ def main():
         type=str,
         help=(
             "The type of resource to check availability for "
-            "(locally and remotely). Alternatively type `list` to see what "
-            "resources can be checked"
+            "(locally and remotely). See --list for a list of resource types."
         ),
     )
+    parser.add_argument(
+        "--list",
+        action=ListAction,
+        loader=L,
+        nargs=0,
+        help="List available resources and exit.",
+    )
+    g = parser.add_mutually_exclusive_group()
+    g.add_argument(
+        "-r",
+        "--remote-only",
+        dest="remote",
+        action="store_true",
+        default=False,
+        help="Only list remote resources",
+    )
+    g.add_argument(
+        "-l",
+        "--local-only",
+        dest="local",
+        action="store_true",
+        default=False,
+        help="Only list local resources",
+    )
     args = parser.parse_args()
-    l = L()
-    attrs = inspect.getmembers(l)
-    available = {
-        key.lstrip("available_"): val
-        for (key, val) in attrs
-        if key.startswith("available")
-    }
-    downloadable = {
-        key.lstrip("downloadable_"): val
-        for (key, val) in attrs
-        if key.startswith("downloadable")
-    }
-    if args.resource == "list":
-        log.info(
-            "You can check the availability (locally and remotely) of the following resources:\n- "
-            + "\n- ".join(list({*available.keys(), *downloadable.keys()}))
-        )
-        sys.exit(0)
+    attrs = dir(L)
+
+    available = [
+        attr.lstrip(LOCAL_TOKEN) for attr in attrs if attr.startswith(LOCAL_TOKEN)
+    ]
+    downloadable = [
+        attr.lstrip(REMOTE_TOKEN) for attr in attrs if attr.startswith(REMOTE_TOKEN)
+    ]
 
     if args.resource not in available and args.resource not in downloadable:
         e = ConfigError(
@@ -76,16 +115,21 @@ def main():
         log.error(e)
         print("Alternatively run `vp-list list` to see resources which can be checked.")
         sys.exit(1)
-    if args.resource in available and available[args.resource]:
-        log.info(
-            f"The following {args.resource} are available locally:\n- "
-            + "\n- ".join(sorted(available[args.resource], key=natural_keys))
-        )
-    if args.resource in downloadable and downloadable[args.resource]:
-        log.info(
-            f"The following {args.resource} are downloadable:\n- "
-            + "\n- ".join(sorted(downloadable[args.resource], key=natural_keys))
-        )
+    l = L()
+    if not args.remote:
+        local_res = getattr(l, LOCAL_TOKEN + args.resource, None)
+        if args.resource in available and local_res:
+            log.info("The following %s are available locally", args.resource)
+            print("- " + "\n- ".join(sane_order(local_res)))
+        else:
+            log.info("No %s are available locally.", args.resource)
+    if not args.local:
+        remote_res = getattr(l, REMOTE_TOKEN + args.resource, None)
+        if args.resource in downloadable and remote_res:
+            log.info("The following %s are downloadable:", args.resource)
+            print("- " + "\n- ".join(sane_order(remote_res)))
+        else:
+            log.info("No %s are available to download.", args.resource)
 
 
 if __name__ == "__main__":
