08ba1d419aef49f74d4d21b57cfb6356a02dd5ff
Rosalyn <rosalynlaurap@gmail.com>
2020-05-13 14:59:07 +0100

    moving coredata.py to correct loc

--- validphys2/src/validphys/coredata.py
+++ validphys2/src/validphys/coredata.py
@@ -117,3 +117,43 @@ class CFactorData:
     description: str
     central_value: np.array
     uncertainty: np.array
+    
+@dataclasses.dataclass(eq=False)
+class CommonData:
+    """
+    Data contained in Commondata files, relevant cuts applied.
+    Parameters
+    ----------
+    setname: str
+        Name of the dataset
+    ndata: int
+        Number of data points
+    data: array of floats with length ndata
+        Data values
+    commondataproc: str
+        Process type, one of 21 options. 
+    nkin: int
+        Number of kinematics specified
+    kinematics: list of str with length nkin
+        Kinematic variables kin1, kin2, kin3 ...
+    nsys: int
+        Number of systematics
+    sysid: list of str with length nsys
+        ID for systematic
+    stat: array of floats with length ndata
+        Statistical uncertainties on each data point
+        (separate ADD and MULT here?)
+    sys: array of floats with dimensions ndat x nsys
+        Systematic uncertainties on each data point
+        (separate ADD and MULT here?)   
+    """
+    setname: str
+    ndata: int
+    data: np.array 
+    commondataproc: str
+    nkin: int 
+    kinematics: list(str)
+    nsys: int
+    sysid: list(str)
+    stat: np.array
+    sys: np.array 
