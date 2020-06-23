7e329c67db9e5f5d7047777dce9315a68f9b7e46
tgiani <s1792848@staffmail.ed.ac.uk>
2020-06-23 09:32:25 +0100

    removing EVOL basis

--- validphys2/src/validphys/pdfbases.py
+++ validphys2/src/validphys/pdfbases.py
@@ -377,30 +377,12 @@ evolution = Basis.from_mapping({
     'g'        : {'g':1},
     'photon'   : {'photon':1},
     },
-    aliases = {'gluon':'g', 'singlet': r'\Sigma', 'sigma':r'\Sigma'},
+    aliases = {'gluon':'g', 'singlet': r'\Sigma', 'sng': r'\Sigma', 'sigma': r'\Sigma',
+               'v': 'V', 'v3': 'V3', 'v8': 'V8', 't3': 'T3', 't8': 'T8', 't15': 'T15'},
     default_elements=(r'\Sigma', 'V', 'T3', 'V3', 'T8', 'V8', 'T15', 'gluon', )
 )
 
-EVOL = Basis.from_mapping({
-        r'\Sigma': {
-            'u': 1, 'ubar': 1, 'd': 1, 'dbar': 1, 's': 1, 'sbar': 1,
-            'c': 1, 'cbar': 1, 'b': 1, 'bbar': 1, 't': 1, 'tbar': 1},
-        'g': {'g': 1},
-
-        'V': {
-            'u': 1, 'ubar': -1, 'd': 1, 'dbar': -1, 's': 1, 'sbar': -1,
-            'c': 1, 'cbar': -1, 'b': 1, 'bbar': -1, 't': 1, 'tbar': -1},
-
-        'V3': {'u': 1, 'ubar': -1, 'd': -1, 'dbar': 1},
-        'V8': {'u': 1, 'ubar': -1, 'd': 1, 'dbar': -1, 's': -2, 'sbar': +2},
-
-        'T3': {'u': 1, 'ubar': 1, 'd': -1, 'dbar': -1},
-        'T8': {'u': 1, 'ubar': 1, 'd': 1, 'dbar': 1, 's': -2, 'sbar': -2},
-        'T15': {'u': 1, 'ubar': 1, 'd': 1, 'dbar': 1, 's': 1, 'sbar': 1, 'c':-3, 'cbar':-3},
-    },
-    aliases = {'gluon':'g', 'singlet': r'\Sigma', 'sng': r'\Sigma', 'sigma': r'\Sigma',
-               'v': 'V', 'v3': 'V3', 'v8': 'V8', 't3': 'T3', 't8': 'T8', 't15': 'T15'},
-    default_elements=(r'\Sigma', 'gluon', 'V', 'V3', 'V8', 'T3', 'T8', 'T15', ))
+EVOL = evolution
 
 NN31IC = Basis.from_mapping(
     {
@@ -454,7 +436,7 @@ r'\bar{d}': {'dbar':1},
 
 def rotation(flav_info, fitbasis):
     """Return a rotation matrix R_{ij} which takes from one
-    of the possible fitting basis (EVOL, NN31IC, FLAVOUR) to the evolution basis,
+    of the possible fitting basis (evolution, NN31IC, FLAVOUR) to the evolution basis,
     (sigma, g, v, v3, v8, t3, t8, cp), corresponding to NN31IC.
     Denoting the rotation matrix as R_{ij} i is the flavour index and j is the evolution index. 
     The evolution basis (NN31IC) is defined as  
@@ -495,7 +477,7 @@ def rotation(flav_info, fitbasis):
         cp = {'u': 0, 'ubar': 0, 'd': 0, 'dbar': 0, 's': 0, 'sbar': 0, 'c': 2, 'g': 0 }
         g = {'u': 0, 'ubar': 0, 'd': 0, 'dbar': 0, 's': 0, 'sbar': 0, 'c': 0, 'g': 1 }
 
-    elif fitbasis == 'EVOL':
+    elif fitbasis == 'EVOL' or fitbasis == 'evolution':
         sng = {'sng': 1, 'v': 0, 'v3': 0, 'v8': 0, 't3': 0, 't8': 0, 't15': 0, 'g': 0 }
         v = {'sng': 0, 'v': 1, 'v3': 0, 'v8': 0, 't3': 0, 't8': 0, 't15': 0, 'g': 0 }
         v3 = {'sng': 0, 'v': 0, 'v3': 1, 'v8': 0, 't3': 0, 't8': 0, 't15': 0, 'g': 0 }
