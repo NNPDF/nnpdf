132257718422fb6eae794d85447a4ce1f87c72b6
siranipour <si292@cam.ac.uk>
2020-06-25 11:33:20 +0100

    Removing nsys = 0 test from test_commondataparser.py

--- validphys2/src/validphys/tests/test_commondataparser.py
+++ validphys2/src/validphys/tests/test_commondataparser.py
@@ -16,11 +16,6 @@ def test_basic_commondata_loading():
     # Test systype loading
     assert res.nsys == 25
     assert isinstance(res.systype_table, pd.DataFrame)
-    # Test a dataset with no systematics
-    emptysyscd = l.check_commondata(setname="CMSWCHARMRAT")
-    emptysysres = load_commondata(emptysyscd)
-    assert emptysysres.nsys == 0
-    assert emptysysres.systype_table.empty is True
 
 
 def test_commondata_with_cuts():
