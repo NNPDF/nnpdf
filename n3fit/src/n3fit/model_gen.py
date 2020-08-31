4227a55434c5fcd1943b62986b6b394888b94825
RoyStegeman <roystegeman@live.nl>
2020-08-31 23:25:51 +0200

    savepoint

--- n3fit/src/n3fit/model_gen.py
+++ n3fit/src/n3fit/model_gen.py
@@ -491,10 +491,13 @@ def pdfNN_layer_generator(
         add_log = Lambda(lambda x: operations.concatenate([x, operations.op_log(x)], axis=-1))
 
     def dense_me(x):
-        """Takes an input tensor `x` and applies all layers
-        from the `list_of_pdf_layers` in order"""
+        """ Takes an input tensor `x` and applies all layers
+        from the `list_of_pdf_layers` in order """
+
         if inp == 1:
-            x = 2*x-1
+            import ipdb; ipdb.set_trace()
+            # x = 2*x-1
+            # x = 1*x
             x0 = tf.keras.backend.ones_like(x)
             curr_fun = list_of_pdf_layers[0](x)
             curr_fun0 = list_of_pdf_layers[0](x0)
@@ -505,10 +508,11 @@ def pdfNN_layer_generator(
             for dense_layer in list_of_pdf_layers[1:]:
                 curr_fun = dense_layer(curr_fun)
                 curr_fun0 = dense_layer(curr_fun0)
-                curr_fun = tf.keras.layers.subtract([curr_fun, curr_fun0])
+            # curr_fun = tf.keras.layers.subtract([curr_fun, curr_fun0])
         else:
             for dense_layer in list_of_pdf_layers[1:]:
                 curr_fun = dense_layer(curr_fun)
+
         return curr_fun
 
     # Preprocessing layer (will be multiplied to the last of the denses)
