0a7fff8189e79ceabfba6043d63588b033a7aa54
wilsonmr <M.R.Wilson-3@sms.ed.ac.uk>
2021-01-20 12:13:16 +0000

    specify version of tf to pin the build, skip tests in run_test rather than in meta.yaml (which didn't work)

--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -6,6 +6,15 @@ set -e
 #Python tests for the installed validphys package
 pytest --pyargs --mpl validphys
 
+platformstr=`uname`
+
+# skip n3fit tests on mac
+if [[ "$platformstr" != "Darwin" ]]; then
+    pytest --pyargs n3fit
+else
+    echo "Skipping tests on Mac"
+fi
+
 mkdir bldtest
 cd bldtest
 cmake .. -DENABLE_TESTS=ON -DBURN_TAG=OFF
