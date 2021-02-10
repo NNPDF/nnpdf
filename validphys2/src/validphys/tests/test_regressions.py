2d3bc2474e1e5e6b9a23fd1986e67b26afb808cc
siranipour <si292@cam.ac.uk>
2021-02-10 19:31:24 +0000

    Parquet regression tests

--- validphys2/src/validphys/tests/test_regressions.py
+++ validphys2/src/validphys/tests/test_regressions.py
@@ -19,10 +19,6 @@ import NNPDF
 from validphys import results
 from validphys.api import API
 from validphys.tests.test_covmats import CORR_DATA
-from validphys.tableloader import (parse_data_cv, parse_exp_mat, load_perreplica_chi2_table,
-                                   sane_load, load_fits_chi2_table)
-from validphys.tests.test_covmats import CORR_DATA
-
 
 
 log = logging.getLogger(__name__)
@@ -30,31 +26,28 @@ log = logging.getLogger(__name__)
 REGRESSION_FOLDER = pathlib.Path(__file__).with_name('regressions')
 
 #TODO: Move these to a library
-def compare_tables(produced_table, storage_path, loader_func):
+def compare_tables(produced_table, storage_path):
     """Test that the ``produced_table`` is equal (as in allclose) to
     the one loaded from the `storage_path` using the `loader_func`"""
     if not storage_path.exists():
         savetable(produced_table, storage_path)
         #Fail test
         assert False, "Storage path does not exist"
-    stored_table = loader_func(storage_path)
-    assert_frame_equal(produced_table, stored_table)
+    stored_table = pd.read_parquet(storage_path)
+    assert_frame_equal(produced_table, stored_table, check_dtype=False)
 
-def make_table_comp(loader_func):
+def make_table_comp(f):
     """Compare the dataframe that the decorated function outputs with
     a file with the same name as the function and extension csv, loaded
     using the provided `loader_func`"""
-    def decorator(f):
-        @functools.wraps(f)
-        def f_(*args, **kwargs):
-            filename = f'{f.__name__}.csv'
-            produced_table = f(*args, **kwargs)
-            compare_tables(produced_table, REGRESSION_FOLDER/filename, loader_func)
-        return f_
-    return decorator
+    @functools.wraps(f)
+    def f_(*args, **kwargs):
+        filename = f'{f.__name__}.parquet'
+        produced_table = f(*args, **kwargs)
+        compare_tables(produced_table, REGRESSION_FOLDER/filename)
+    return f_
 
-
-@make_table_comp(parse_data_cv)
+@make_table_comp
 def test_mcreplica(data_config):
     config = dict(data_config)
     config["dataset_inputs"] = CORR_DATA
@@ -64,8 +57,7 @@ def test_mcreplica(data_config):
     rep = API.indexed_make_replica(**config, seed=seed)
     return rep
 
-
-@make_table_comp(parse_exp_mat)
+@make_table_comp
 def test_expcovmat(data_config):
     mat = API.groups_covmat_no_table(**data_config)
     covmats = []
@@ -76,23 +68,23 @@ def test_expcovmat(data_config):
     assert np.allclose(mat.values, othermat)
     return mat
 
-@make_table_comp(parse_exp_mat)
+@make_table_comp
 def test_t0covmat(data_witht0_config):
     return API.groups_covmat_no_table(**data_witht0_config)
 
-@make_table_comp(parse_exp_mat)
+@make_table_comp
 def test_expsqrtcovmat(data_config):
     return API.groups_sqrtcovmat(**data_config)
 
-@make_table_comp(parse_exp_mat)
+@make_table_comp
 def test_t0sqrtcovmat(data_witht0_config):
     return API.groups_sqrtcovmat(**data_witht0_config)
 
-@make_table_comp(parse_exp_mat)
+@make_table_comp
 def test_pdf_plus_exp_covmat(data_config):
     return API.groups_covmat_no_table(use_pdferr=True, **data_config)
 
-@make_table_comp(sane_load)
+@make_table_comp
 def test_predictions(data_config):
     # TODO: ideally we would change the baseline to just be corresponding columns
     # of `experiment_result_table`, however sane_load expects just a single level
@@ -102,25 +94,25 @@ def test_predictions(data_config):
     th = res_tab.iloc[:, 2:].values
     return pd.DataFrame(th, columns=map(str, range(th.shape[1])))
 
-@make_table_comp(sane_load)
+@make_table_comp
 def test_dataset_t0_predictions(data_witht0_config):
     # TODO: As in `test_predictions`
     res_tab = API.group_result_table_no_table(**data_witht0_config)
     th = res_tab.iloc[:, 2:].values
     return pd.DataFrame(th, columns=map(str, range(th.shape[1])))
 
-@make_table_comp(sane_load)
+@make_table_comp
 def test_cv(data_config):
     # TODO: As in `test_predictions`
     res_tab = API.group_result_table_no_table(**data_config)
     data_values = res_tab.iloc[:, 0].values[:, np.newaxis]
     return pd.DataFrame(data_values, columns=['CV'])
 
-@make_table_comp(load_perreplica_chi2_table)
+@make_table_comp
 def test_replicachi2data(data_witht0_config):
     return API.perreplica_chi2_table(**data_witht0_config)
 
-@make_table_comp(load_fits_chi2_table)
+@make_table_comp
 def test_datasetchi2(data_singleexp_witht0_config):
     # This is a bit hacky but avoids requiring a fit
     exps = API.groups_data(**data_singleexp_witht0_config)
