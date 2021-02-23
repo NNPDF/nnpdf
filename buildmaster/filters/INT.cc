a6c8d1252db5112401c20e85cac09b34d88ca578
enocera <enocera@nikhef.nl>
2021-02-23 13:49:07 +0000

    Integrability cleanup

--- buildmaster/filters/INT.cc
+++ buildmaster/filters/INT.cc
@@ -9,32 +9,20 @@
 void register_integrability(vector<CommonData*>& list)
 {
   // Integrability sets
-  const std::array<std::string, 5>  INTsets = { 
+  const std::array<std::string, 4>  INTsets543 = { 
     "INTEGXT3_543",
-    "INTEGXT8_543",
     "INTEGXV_543",
     "INTEGXV3_543",
     "INTEGXV8_543"};
-  for (auto set : INTsets)
+  const std::array<std::string, 2>  INTsets = { 
+    "INTEGXT3",
+    "INTEGXT8"};
+
+  
+  for (auto set : INTsets543)
     list.push_back(new IntFilter543(set));
-}
-
-void IntFilter::ReadData()
-{
-  const double q2int = 2.7225; //GeV2
-
-  fKin1[0] = 1e-9;
-  fKin1[1] = 1e-8;
-  fKin1[2] = 1e-7;
-
-  for (int i=0; i< fNData; i++)
-  {
-    fData[i] = 0;
-    fStat[i] = 0;
-    fKin2[i] = q2int;
-    fKin3[i] = 0;
-  }
-
+  for (auto set : INTsets)
+    list.push_back(new IntFilter(set));
 }
 
 void IntFilter543::ReadData()
@@ -52,5 +40,20 @@ void IntFilter543::ReadData()
     fKin2[i] = q2int;
     fKin3[i] = 0;
   }
-
 }
+
+void IntFilter::ReadData()
+{
+  const double q2int = 2.7225; //GeV2
+
+  fKin1[0] = 1e-9;
+
+  for (int i=0; i< fNData; i++)
+  {
+    fData[i] = 0;
+    fStat[i] = 0;
+    fKin2[i] = q2int;
+    fKin3[i] = 0;
+  }
+
+}  
