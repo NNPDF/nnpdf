c2233cec3461b940bd75768a2fe274c08583433d
siranipour <si292@cam.ac.uk>
2021-03-02 11:38:23 +0000

    Checking plotting file structure using validobj

--- validphys2/src/validphys/plotoptions/core.py
+++ validphys2/src/validphys/plotoptions/core.py
@@ -4,7 +4,11 @@ Created on Fri Mar 11 19:27:44 2016
 
 @author: Zahari Kassabov
 """
+import dataclasses
+import enum
+from functools import partial
 import logging
+import typing
 
 import numpy as np
 import pandas as pd
@@ -12,12 +16,15 @@ import numbers
 
 from reportengine.floatformatting import format_number
 from reportengine.configparser import Config, ConfigError, named_element_of
+from reportengine.compat import yaml
 from reportengine.utils import get_functions, ChainMap
 
 from NNPDF import CommonData, DataSet
 from validphys.core import CommonDataSpec, DataSetSpec, Cuts, InternalCutsWrapper
 from validphys.plotoptions.utils import apply_to_all_columns, get_subclasses
 from validphys.plotoptions import labelers, kintransforms, resulttransforms
+
+from validobj import parse_input, ValidationError
 
 log = logging.getLogger(__name__)
 
@@ -26,6 +33,49 @@ default_labels = ('idat', 'k1', 'k2', 'k3')
 labeler_functions = get_functions(labelers)
 transform_functions = get_subclasses(kintransforms, kintransforms.Kintransform)
 result_functions = get_functions(resulttransforms)
+
+
+def parse_yaml_inp(inp, spec):
+    """Helper function to parse yaml using the `validobj` library and print
+    useful error messages in case of a parsing error.
+
+    https://validobj.readthedocs.io/en/latest/examples.html#yaml-line-numbers
+    """
+    try:
+        return parse_input(inp, spec)
+    except ValidationError as e:
+        current_exc = e
+        current_inp = inp
+        error_text_lines = []
+        while current_exc:
+            if hasattr(current_exc, 'wrong_field'):
+                wrong_field = current_exc.wrong_field
+                # Mappings compping from ``round_trip_load`` have an
+                # ``lc`` attribute that gives a tuple of
+                # ``(line_number, column)`` for a given item in
+                # the mapping.
+                line = current_inp.lc.item(wrong_field)[0]
+                error_text_lines.append(f"Problem processing key at line {line}:")
+                current_inp = current_inp[wrong_field]
+            elif hasattr(current_exc, 'wrong_index'):
+                wrong_index = current_exc.wrong_index
+                # Similarly lists allow to retrieve the line number for
+                # a given item.
+                line = current_inp.lc.item(wrong_index)[0]
+                current_inp = current_inp[wrong_index]
+                error_text_lines.append(f"Problem processing list item at line {line}:")
+            elif hasattr(current_exc, 'unknown'):
+                unknown_lines = []
+                for u in current_exc.unknown:
+                    unknown_lines.append((current_inp.lc.item(u)[0], u))
+                unknown_lines.sort()
+                for line, key in unknown_lines:
+                    error_text_lines.append(
+                        f"Unknown key {key!r} defined at line {line}:"
+                    )
+            error_text_lines.append(str(current_exc))
+            current_exc = current_exc.__cause__
+        raise ValidationError('\n'.join(error_text_lines)) from e
 
 
 def get_info(data, *, normalize=False, cuts=None, use_plotfiles=True):
@@ -61,8 +111,6 @@ def get_info(data, *, normalize=False, cuts=None, use_plotfiles=True):
 
     info = PlotInfo.from_commondata(data, cuts=cuts, normalize=normalize)
     return info
-
-
 
 class PlotInfo:
     def __init__(
@@ -156,18 +204,22 @@ class PlotInfo:
         if commondata.plotfiles:
             for file in commondata.plotfiles:
                 with open(file) as f:
-                    config = PlotConfigParser.from_yaml(f, commondata, cuts=cuts)
-                try:
-                    config_params = config.process_all_params()
-                except ConfigError:
-                    log.error(f"Error in plotting file: {file}")
-                    raise
-
+                    processed_input = yaml.safe_load(f)
+                    pf = parse_yaml_inp(processed_input, PlottingFile)
+                    config_params = pf.__dict__
+                    # Remove the sentinel None values
+                    config_params = {k : v for k, v in config_params.items() if v is not None}
                 plot_params = plot_params.new_child(config_params)
+            if 'kinematics_override' in plot_params:
+                # Annoying way that enum works. To get
+                # the callable we need the value attribute
+                plot_params['kinematics_override'] = plot_params['kinematics_override'].value
+            if 'result_transform' in plot_params:
+                plot_params['result_transform'] = plot_params['result_transform'].value
             if normalize and 'normalize' in plot_params:
                 #We might need to use reportengine.namespaces.resolve here
                 plot_params = plot_params.new_child(config_params['normalize'])
-            if not 'dataset_label' in plot_params:
+            if 'dataset_label' not in plot_params:
                 log.warning(f"'dataset_label' key not found in {file}")
                 plot_params['dataset_label'] = commondata.name
 
@@ -175,10 +227,64 @@ class PlotInfo:
             plot_params = {'dataset_label':commondata.name}
 
         kinlabels = commondata.plot_kinlabels
-        if 'kinematics_override' in plot_params:
-            kinlabels = plot_params['kinematics_override'].new_labels(*kinlabels)
+        kinlabels = plot_params['kinematics_override'].new_labels(*kinlabels)
 
         return cls(kinlabels=kinlabels, **plot_params)
+
+
+class KinLabel(enum.Enum):
+    k1 = enum.auto()
+    k2 = enum.auto()
+    k3 = enum.auto()
+
+class Scale(enum.Enum):
+    linear = enum.auto()
+    log = enum.auto()
+    symlog = enum.auto()
+
+class TransformFunctions(enum.Enum):
+    # https://docs.python.org/3/library/enum.html#timeperiod
+    _ignore_ = 'TransformFunctions name func'
+    TransformFunctions = vars()
+    for name, func in transform_functions.items():
+        TransformFunctions[name] = func()
+
+class ResultTransformations(enum.Enum):
+    # https://docs.python.org/3/library/enum.html#timeperiod
+    _ignore_ = 'ResultTransformations name func'
+    ResultTransformations = vars()
+    for name, func in result_functions.items():
+        # https://stackoverflow.com/questions/40338652/how-to-define-enum-values-that-are-functions
+        ResultTransformations[name] = partial(func)
+
+@dataclasses.dataclass
+class PlottingFile:
+    dataset_label: typing.Optional[str] = None
+    experiment: typing.Optional[str] = None
+    nnpdf31_process: typing.Optional[str] = None
+    data_reference: typing.Optional[str] = None
+    theory_reference: typing.Optional[str] = None
+    process_description: typing.Optional[str] = None
+    y_label: typing.Optional[str] = None
+    x_label: typing.Optional[str] = None
+
+    kinematics_override: typing.Optional[TransformFunctions] = None
+
+    result_transform: typing.Optional[ResultTransformations] = None
+
+    # TODO: change this to x: typing.Optional[KinLabel] = None
+    # but this currently fails CI because some datasets have
+    # a kinlabel of $x_1$ or " "!!
+    x: typing.Optional[str] = None
+
+    x_scale: typing.Optional[Scale] = None
+    y_scale: typing.Optional[Scale] = None
+
+    line_by: typing.Optional[list] = None
+    figure_by: typing.Optional[list] = None
+
+    normalize: typing.Optional[typing.Mapping[str, typing.Union[list, str]]] = None
+    extra_labels: typing.Optional[typing.Mapping[str, typing.List]] = None
 
 
 class PlotConfigParser(Config):
