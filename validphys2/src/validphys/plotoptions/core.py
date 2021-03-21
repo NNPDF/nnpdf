8d73f946dd7306caad4f6457674400ddc03901d3
siranipour <si292@cam.ac.uk>
2021-03-21 15:59:17 +0000

    Moving parse_yaml_input to utils

--- validphys2/src/validphys/plotoptions/core.py
+++ validphys2/src/validphys/plotoptions/core.py
@@ -22,8 +22,7 @@ from NNPDF import CommonData, DataSet
 from validphys.core import CommonDataSpec, DataSetSpec, Cuts, InternalCutsWrapper
 from validphys.plotoptions.utils import apply_to_all_columns, get_subclasses
 from validphys.plotoptions import labelers, kintransforms, resulttransforms
-
-from validobj import parse_input, ValidationError
+from validphys.utils import parse_yaml_inp
 
 log = logging.getLogger(__name__)
 
@@ -32,50 +31,6 @@ default_labels = ('idat', 'k1', 'k2', 'k3')
 labeler_functions = get_functions(labelers)
 transform_functions = get_subclasses(kintransforms, kintransforms.Kintransform)
 result_functions = get_functions(resulttransforms)
-
-
-def parse_yaml_inp(inp, spec):
-    """Helper function to parse yaml using the `validobj` library and print
-    useful error messages in case of a parsing error.
-
-    https://validobj.readthedocs.io/en/latest/examples.html#yaml-line-numbers
-    """
-    try:
-        return parse_input(inp, spec)
-    except ValidationError as e:
-        current_exc = e
-        current_inp = inp
-        error_text_lines = []
-        while current_exc:
-            if hasattr(current_exc, 'wrong_field'):
-                wrong_field = current_exc.wrong_field
-                # Mappings compping from ``round_trip_load`` have an
-                # ``lc`` attribute that gives a tuple of
-                # ``(line_number, column)`` for a given item in
-                # the mapping.
-                line = current_inp.lc.item(wrong_field)[0]
-                error_text_lines.append(f"Problem processing key at line {line}:")
-                current_inp = current_inp[wrong_field]
-            elif hasattr(current_exc, 'wrong_index'):
-                wrong_index = current_exc.wrong_index
-                # Similarly lists allow to retrieve the line number for
-                # a given item.
-                line = current_inp.lc.item(wrong_index)[0]
-                current_inp = current_inp[wrong_index]
-                error_text_lines.append(f"Problem processing list item at line {line}:")
-            elif hasattr(current_exc, 'unknown'):
-                unknown_lines = []
-                for u in current_exc.unknown:
-                    unknown_lines.append((current_inp.lc.item(u)[0], u))
-                unknown_lines.sort()
-                for line, key in unknown_lines:
-                    error_text_lines.append(
-                        f"Unknown key {key!r} defined at line {line}:"
-                    )
-            error_text_lines.append(str(current_exc))
-            current_exc = current_exc.__cause__
-        raise ValidationError('\n'.join(error_text_lines)) from e
-
 
 def get_info(data, *, normalize=False, cuts=None, use_plotfiles=True):
     """Retrieve and process the plotting information for the input data (which could
--- validphys2/src/validphys/utils.py
+++ validphys2/src/validphys/utils.py
@@ -6,6 +6,51 @@ Created on Sun Mar 13 21:12:41 2016
 """
 import numpy as np
 
+from validobj import parse_input, ValidationError
+
+
+def parse_yaml_inp(inp, spec):
+    """Helper function to parse yaml using the `validobj` library and print
+    useful error messages in case of a parsing error.
+
+    https://validobj.readthedocs.io/en/latest/examples.html#yaml-line-numbers
+    """
+    try:
+        return parse_input(inp, spec)
+    except ValidationError as e:
+        current_exc = e
+        current_inp = inp
+        error_text_lines = []
+        while current_exc:
+            if hasattr(current_exc, 'wrong_field'):
+                wrong_field = current_exc.wrong_field
+                # Mappings compping from ``round_trip_load`` have an
+                # ``lc`` attribute that gives a tuple of
+                # ``(line_number, column)`` for a given item in
+                # the mapping.
+                line = current_inp.lc.item(wrong_field)[0]
+                error_text_lines.append(f"Problem processing key at line {line}:")
+                current_inp = current_inp[wrong_field]
+            elif hasattr(current_exc, 'wrong_index'):
+                wrong_index = current_exc.wrong_index
+                # Similarly lists allow to retrieve the line number for
+                # a given item.
+                line = current_inp.lc.item(wrong_index)[0]
+                current_inp = current_inp[wrong_index]
+                error_text_lines.append(f"Problem processing list item at line {line}:")
+            elif hasattr(current_exc, 'unknown'):
+                unknown_lines = []
+                for u in current_exc.unknown:
+                    unknown_lines.append((current_inp.lc.item(u)[0], u))
+                unknown_lines.sort()
+                for line, key in unknown_lines:
+                    error_text_lines.append(
+                        f"Unknown key {key!r} defined at line {line}:"
+                    )
+            error_text_lines.append(str(current_exc))
+            current_exc = current_exc.__cause__
+        raise ValidationError('\n'.join(error_text_lines)) from e
+
 def split_by(it, crit):
     """Split ``it`` in two lists, the first is such that ``crit`` evaluates to
     True and the second such it doesn't. Crit can be either a function or an
