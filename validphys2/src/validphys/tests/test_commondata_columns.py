64779fd0e8ae9b1de703a208bb6d2f95fea33395
wilsonm <michael.wilson@ed.ac.uk>
2021-03-31 18:14:11 +0100

    update atlas photon buildmaster, to make add and mult columns self consistent. Add to test

--- validphys2/src/validphys/tests/test_commondata_columns.py
+++ validphys2/src/validphys/tests/test_commondata_columns.py
@@ -23,12 +23,15 @@ def _cd_column_comparison(cd):
     np.testing.assert_allclose(
         cv.to_numpy()[:, np.newaxis] * mult.to_numpy() * 1e-2,
         add.to_numpy(),
+        rtol=1e-05, # use the allclose values (less stringent tolerances)
+        atol=1e-08,
     )
 
 DS_NAMES = [
     "LHCBZEE2FB_40",
     "LHCBW36PB_40",
     "CDFZRAP_NEW",
+    "ATLASPHT15"
 ]
 
 L = Loader()
