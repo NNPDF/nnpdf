2042479e10bc35c619e9bd4306f0cdcea8c65390
enocera <enocera@nikhef.nl>
2021-05-05 14:18:42 +0100

    Updated treatment of systematic ucnertainties after consultation with Amanda Cooper-Sarkar; updated K-factors; ready to be merged

--- buildmaster/filters/ATLAS_WMU_8TEV.cc
+++ buildmaster/filters/ATLAS_WMU_8TEV.cc
@@ -22,8 +22,12 @@ fiducial phase space as for the integrated measurement.
 These are measured in 11 bins of absolute muon pseudorapidity between 0 and 
 2.4 with bin edges at 0, 0.21, 0.42, 0.63, 0.84, 1.05, 1.37, 1.52, 1.74, 1.95, 
 2.18, and 2.4. The charge asymmetry is not implemented. Systematic uncertainties
-are correlated between W+ and W-. An extra systematic uncertainty due to the
-ATLAS luminosity at 8 TeV is implemented.
+are correlated between W+ and W-, except the following: F/B compatibility;
+Muon recoEFF stat; MJ - Method uncorr; MJ - uncorr; MC stat unc. These
+uncertainties are treated as fully uncorrelated. An extra systematic 
+uncertainty due to the ATLAS luminosity at 8 TeV is implemented.
+Note that central values are taken from Table 3 of the Hepdata entry, while the
+breakdown of uncertainties is taken from Tables 5a and 5b.
 */
 
 #include "ATLAS_WMU_8TEV.h"
@@ -32,9 +36,9 @@ ATLAS luminosity at 8 TeV is implemented.
 
 void ATLAS_WMU_8TEVFilter::ReadData()
 {
-  fstream f1;
+  fstream f1, f2, f3;
 
-  //Central values and uncertainties: W+
+  //Central values: W+ and W-
   stringstream datafile("");
   datafile << dataPath()
 	   << "rawdata/ATLAS_WMU_8TEV/HEPData-ins1729240-v1-Table_3.csv";
@@ -45,130 +49,26 @@ void ATLAS_WMU_8TEVFilter::ReadData()
       cerr << "Error opening data file " << datafile.str() << endl;
       exit(-1);
     }
-  
-  //Read central value
-  string line;
-  for(int i=0; i<12; i++)
-    {
-      getline(f1,line);
-    }
-  
-  double ddum;
-  char comma;
 
-  for(int i=0; i<fNData/2; i++)
-    {
-      getline(f1,line);
-      istringstream lstream(line);
-      lstream >> fKin1[i]        >> comma
-	      >> ddum            >> comma
-	      >> ddum            >> comma
-	      >> fData[i];       //in [pb]
-	
-      fData[i] *= 1000.; //Convert to [fb]
-      fKin2[i] = MW*MW;
-      fKin3[i] = 8000.;  //GeV
-
-      //Statistical uncertainty
-      lstream >> comma >> fStat[i]
-	      >> comma >> ddum;
-      fStat[i] = fStat[i]/100. * fData[i]; //convert to absolute value
-      
-      //Systematic uncertainties
-      for(int j=0; j<fNSys-1; j++)
-	{
-	  lstream >> comma >> fSys[i][j].mult
-		  >> comma >> ddum;
-	  
-	  fSys[i][j].add = fSys[i][j].mult/100. * fData[i];
-	  fSys[i][j].type = ADD;
-	  fSys[i][j].name = "CORR";
-	}
-
-      //Luminosity uncertainty
-      fSys[i][5].mult = 1.9;
-      fSys[i][5].add = fSys[i][5].mult/100. * fData[i];
-      fSys[i][5].type = MULT;
-      fSys[i][5].name = "ATLASLUMI12";
-
-      //Uncorrelated uncertainties
-      fSys[i][1].name = "UNCORR";
-      fSys[i][3].name = "UNCORR";
-    }
-
-  for(int i=0; i<5; i++)
-    {
-      getline(f1,line);
-    }
-  
-  for(int i=fNData/2; i<fNData; i++)
-    {
-      getline(f1,line);
-      istringstream lstream(line);
-      lstream >> fKin1[i]        >> comma
-	      >> ddum            >> comma
-	      >> ddum            >> comma
-	      >> fData[i];       //in [pb]
-	
-      fData[i] *= 1000.; //Convert to [fb]
-      fKin2[i] = MW*MW;
-      fKin3[i] = 8000.;  //GeV
-
-      //Statistical uncertainty
-      lstream >> comma >> fStat[i]
-	      >> comma >> ddum;
-      fStat[i] = fStat[i]/100. * fData[i]; //convert to absolute value
-      
-      //Systematic uncertainties
-      for(int j=0; j<fNSys-1; j++)
-	{
-	  lstream >> comma >> fSys[i][j].mult
-		  >> comma >> ddum;
-	  
-	  fSys[i][j].add = fSys[i][j].mult/100. * fData[i];
-	  fSys[i][j].type = ADD;
-	  fSys[i][j].name = "CORR";
-	}
-
-      //Luminosity uncertainty
-      fSys[i][5].mult = 1.9;
-      fSys[i][5].add = fSys[i][5].mult/100. * fData[i];
-      fSys[i][5].type = MULT;
-      fSys[i][5].name = "ATLASLUMI12";
-
-      //Uncorrelated uncertainties
-      fSys[i][1].name = "UNCORR";
-      fSys[i][3].name = "UNCORR";
-    }
-  
-  f1.close();
-}
-
-
-/*
-void ATLAS_WMU_8TEVFilter::ReadData()
-{
-  fstream f1, f2;
-
-  //Central values and uncertainties: W+
+  //Uncertainties: W+
   stringstream datafile_WP("");
   datafile_WP << dataPath()
 	   << "rawdata/ATLAS_WMU_8TEV/HEPData-ins1729240-v1-Table_5a.csv";
-  f1.open(datafile_WP.str().c_str(), ios::in); 
+  f2.open(datafile_WP.str().c_str(), ios::in); 
 
-  if (f1.fail())
+  if (f2.fail())
     {
       cerr << "Error opening data file " << datafile_WP.str() << endl;
       exit(-1);
     }
 
-  //Central values and uncertainties: W-
+  //Uncertainties: W-
   stringstream datafile_WM("");
   datafile_WM << dataPath()
 	   << "rawdata/ATLAS_WMU_8TEV/HEPData-ins1729240-v1-Table_5b.csv";
-  f2.open(datafile_WM.str().c_str(), ios::in); 
+  f3.open(datafile_WM.str().c_str(), ios::in); 
 
-  if (f2.fail())
+  if (f3.fail())
     {
       cerr << "Error opening data file " << datafile_WM.str() << endl;
       exit(-1);
@@ -180,11 +80,13 @@ void ATLAS_WMU_8TEVFilter::ReadData()
     {
       getline(f1,line);
       getline(f2,line);
+      getline(f3,line);
     }
   
   double ddum;
   char comma;
 
+  //W+
   for(int i=0; i<fNData/2; i++)
     {
       getline(f1,line);
@@ -198,77 +100,101 @@ void ATLAS_WMU_8TEVFilter::ReadData()
       fKin2[i] = MW*MW;
       fKin3[i] = 8000.;  //GeV
 
+      //Statistical uncertainty
+      lstream >> comma >> fStat[i]
+	      >> comma >> ddum;
+      fStat[i] = fStat[i]/100. * fData[i]; //convert to absolute value
+
       //Systematic uncertainties
+      getline(f2,line);
+      istringstream jstream(line);
+      jstream >> ddum >> comma
+	      >> ddum >> comma
+	      >> ddum >> comma
+	      >> ddum;
+
       for(int j=0; j<fNSys-1; j++)
 	{
-	  lstream >> comma >> fSys[i][j].mult
+	  jstream >> comma >> fSys[i][j].mult
 		  >> comma >> ddum;
 	  
 	  fSys[i][j].add = fSys[i][j].mult/100. * fData[i];
 	  fSys[i][j].type = ADD;
 	  fSys[i][j].name = "CORR";
 	}
-
-      //Statistical uncertainty
-      lstream >> comma >> fStat[i];
-      fStat[i] = fStat[i]/100. * fData[i]; //convert to absolute value
-
+      
       //Luminosity uncertainty
       fSys[i][47].mult = 1.9;
       fSys[i][47].add = fSys[i][47].mult/100. * fData[i];
       fSys[i][47].type = MULT;
       fSys[i][47].name = "ATLASLUMI12";
-
+      
       //Uncorrelated uncertainties
-      fSys[i][6].name  = "UNCORR";
-      fSys[i][43].name = "UNCORR";
-      fSys[i][44].name = "UNCORR";
-      fSys[i][46].name = "UNCORR";
+      fSys[i][10].name = "UNCORR"; //sys Muon recoEFF stat
+      fSys[i][43].name = "UNCORR"; //sys MJ method uncorr
+      fSys[i][44].name = "UNCORR"; //sys MJ uncorr
+      fSys[i][45].name = "UNCORR"; //sys F/B compatibility
+      fSys[i][46].name = "UNCORR"; //sys MC stat unc
+
     }
-  
+
+  for(int i=0; i<5; i++)
+    {
+      getline(f1,line);
+    }
+
+  //W-
   for(int i=fNData/2; i<fNData; i++)
     {
-      getline(f2,line);
+      getline(f1,line);
       istringstream lstream(line);
       lstream >> fKin1[i]        >> comma
 	      >> ddum            >> comma
 	      >> ddum            >> comma
 	      >> fData[i];       //in [pb]
-      
+	
       fData[i] *= 1000.; //Convert to [fb]
       fKin2[i] = MW*MW;
       fKin3[i] = 8000.;  //GeV
-      
-      //Systematic uncertainties
+
+      //Statistical uncertainty
+      lstream >> comma >> fStat[i]
+	      >> comma >> ddum;
+      fStat[i] = fStat[i]/100. * fData[i]; //convert to absolute value
+
+      //Systematic ucnertainties
+      getline(f3,line);
+      istringstream jstream(line);
+      jstream >> ddum >> comma
+	      >> ddum >> comma
+	      >> ddum >> comma
+	      >> ddum;
+
       for(int j=0; j<fNSys-1; j++)
 	{
-	  lstream >> comma >> fSys[i][j].mult
+	  jstream >> comma >> fSys[i][j].mult
 		  >> comma >> ddum;
+	  
 	  fSys[i][j].add = fSys[i][j].mult/100. * fData[i];
 	  fSys[i][j].type = ADD;
 	  fSys[i][j].name = "CORR";
 	}
-      
-      //Statistical uncertainty
-      lstream >> comma >> fStat[i];
-      fStat[i] = fStat[i]/100. * fData[i]; //convert to absolute value
       
       //Luminosity uncertainty
       fSys[i][47].mult = 1.9;
       fSys[i][47].add = fSys[i][47].mult/100. * fData[i];
       fSys[i][47].type = MULT;
       fSys[i][47].name = "ATLASLUMI12";
-
+      
       //Uncorrelated uncertainties
-      fSys[i][6].name  = "UNCORR";
-      fSys[i][43].name = "UNCORR";
-      fSys[i][44].name = "UNCORR";
-      fSys[i][46].name = "UNCORR";
+      fSys[i][10].name = "UNCORR"; //sys Muon recoEFF stat
+      fSys[i][43].name = "UNCORR"; //sys MJ method uncorr
+      fSys[i][44].name = "UNCORR"; //sys MJ uncorr
+      fSys[i][45].name = "UNCORR"; //sys F/B compatibility
+      fSys[i][46].name = "UNCORR"; //sys MC stat unc
     }
   
   f1.close();
   f2.close();
-  
+  f3.close();
 }
-
-*/
