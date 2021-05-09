c1f48e015a6e329520cfbb75b9c319c3b12cc8cb
enocera <enocera@nikhef.nl>
2021-05-09 17:47:16 +0100

    Cleanup

--- buildmaster/filters/FTDY.cc
+++ buildmaster/filters/FTDY.cc
@@ -1152,29 +1152,33 @@ void DYE866R_sh_iteFilter::ReadData()
   f3.close();
 }
 
-/**
- * Drell Yan experiment E906/SeaQuest
- * 
- * Name_exp  : E906
- * Reference : The Asymmetry of Antimatter in the Proton
- * ArXiv     : 2103.04024
- * Published : Nature volume 590, pages 561–565(2021)
- * Hepdata   : n/a 
- * Data for the dimesionless ratio between fixed-target DY cross sections with hydrogen and 
- * deuterium targets ( sigma^pd / 2*sigma^pp ) have been collected with a proton beam at an 
- * energy of 120 GeV and are given as 6 points in different bins with average <xt>, <xb>, <M> 
- * and <PT> (Bjorken-x of the target/beam, invariant mass and transverse momentum of the DY pair). 
- * Systematic uncertainties are given as a single source of fully correlated systematic, and 
- * the covariance matrix for the statistical uncertainties is also given.
- * 
- * The data are stored in rawdata/DTE906/data_paper.dat, according to the original format as in 
- * the paper. They are converted in data for the distribution differential in hadronic rapidity 
- * and invariant mass, using the definition of hadronic rapidity given in Eqs. (4.6) of 
- * https://arxiv.org/pdf/1009.5691.pdf. No Jacobian is required, since it cancels out between the
- * numerator and the denominator.
- * 
- * Implemented by TG March 2021
- **/
+/*
+  Drell Yan experiment E906/SeaQuest
+  
+  Name_exp  : E906
+  Reference : The Asymmetry of Antimatter in the Proton
+  ArXiv     : 2103.04024
+  Published : Nature volume 590, pages 561–565(2021)
+  Hepdata   : n/a 
+  Data for the dimesionless ratio between fixed-target DY cross sections with 
+  hydrogen and deuterium targets ( sigma^pd / 2*sigma^pp ) have been collected 
+  with a proton beam at an energy of 120 GeV and are given as 6 points in 
+  different bins with average <xt>, <xb>, <M> and <PT> (Bjorken-x of the 
+  target/beam, invariant mass and transverse momentum of the DY pair). 
+  Systematic uncertainties are given as a single source of fully correlated 
+  systematic, and the covariance matrix for the statistical uncertainties is 
+  also given.
+  
+  The data are stored in rawdata/DTE906/data_paper.dat, according to the 
+  original format as in the paper. They are converted in data for the 
+  distribution differential in hadronic rapidity and invariant mass, using the 
+  definition of hadronic rapidity given in Eqs. (4.6) of 
+  https://arxiv.org/pdf/1009.5691.pdf. 
+  No Jacobian is required, since it cancels out between the numerator and the 
+  denominator.
+  
+  Implemented by TG March 2021
+*/
 
 void DYE906RFilter::ReadData()
 {  
