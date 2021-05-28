a595bc45d90b3850b3a985a4db14b1a5f1ab8d32
wilsonm <michael.wilson@ed.ac.uk>
2021-05-28 16:47:07 +0100

    remove chi2 check from vp tests because requires filtered fit

--- conda-recipe/run_fit_test.sh
+++ conda-recipe/run_fit_test.sh
@@ -7,3 +7,5 @@ pytest --pyargs n3fit
 
 # check filter works
 vp-setupfit --legacy nnpdfcpp/config/testconfig.yml
+#Check that chi2check run
+chi2check testconfig NNPDF31_nnlo_as_0118
--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -14,9 +14,5 @@ cmake .. -DENABLE_TESTS=ON -DBURN_TAG=OFF
 make catch_test -j
 ./libnnpdf/tests/catch_test
 
-#Check that chi2check run
-chi2check testconfig NNPDF31_nnlo_as_0118
-
-
 #Print linkage data
 conda inspect linkages -p $PREFIX $PKG_NAME
