3b73e91ea4c60005f263092df7b479099a661595
RoyStegeman <roystegeman@live.nl>
2021-11-19 08:47:27 +0100

    read process from PLOTTING_*.yml files instead of hardcoding

--- validphys2/src/validphys/theorycovariance/theorycovarianceutils.py
+++ validphys2/src/validphys/theorycovariance/theorycovarianceutils.py
@@ -4,8 +4,10 @@ theorycovariance.py
 Low level utilities for theorycovariance module
 """
 import logging
+import yaml
 
 from reportengine.checks import make_argcheck, check
+from validphys.loader import Loader
 log = logging.getLogger(__name__)
 
 
@@ -90,72 +92,18 @@ def check_fit_dataset_order_matches_grouped(
                 "errors when running fits with theory covmat. Datasets should "
                 f"be ordered by {processed_metadata_group} in the runcard." 
             )
-    
+
 def process_lookup(name):
     """Produces a dictionary with keys corresponding to dataset names
     and values corresponding to process types. Process types are
     regrouped into the five categories 'Drell-Yan', 'Top', Jets',
     'DIS NC' and 'DIS CC'.
-
-
-    The implementation relies on hardcoding the process type for
-    each dataset internally. If a dataset is not registered,
-    'UNKNOWN' is returned.
     """
 
-    process_dictionary = {	"ATLASZPT8TEVMDIST": 			       "DY",
-				"ATLASZPT8TEVYDIST":			       "DY",
-				"CMSZDIFF12":				       "DY",
-				"ATLAS1JET11":				       "JETS",
-				"CMSJETS11":				       "JETS",
-                                "CDFR2KT":				       "JETS",
-				"CMSTOPDIFF8TEVTTRAPNORM":		       "TOP",
-				"ATLASTOPDIFF8TEVTRAPNORM":		       "TOP",
-				"ATLASTTBARTOT":			       "TOP",
-				"CMSTTBARTOT":				       "TOP",
-				"DYE605":				       "DY",
-				"DYE886P":				       "DY",
-				"DYE886R":				       "DY",
-				"ATLASWZRAP36PB":		               "DY",
-				"ATLASZHIGHMASS49FB":			       "DY",
-				"ATLASLOMASSDY11EXT":			       "DY",
-				"ATLASWZRAP11":				       "DY",
-				"CMSWEASY840PB":			       "DY",
-				"CMSWMASY47FB":				       "DY",
-				"CMSDY2D11":				       "DY",
-				"CMSWMU8TEV":				       "DY",
-				"CMSWCHARMRAT":				       "DY",
-				"LHCBZ940PB":				       "DY",
-				"LHCBZEE2FB":				       "DY",
-				"LHCBWZMU7TEV":				       "DY",
-				"LHCBWZMU8TEV":				       "DY",
-				"D0WEASY":				       "DY",
-				"D0WMASY":				       "DY",
-				"D0ZRAP":				       "DY",
-				"CDFZRAP":				       "DY",
-				"H1HERAF2B":				       "DIS NC",
-				"HERACOMBCCEM":				       "DIS CC",
-				"HERACOMBCCEP":				       "DIS CC",
-				"HERACOMBNCEM":				       "DIS NC",
-				"HERACOMBNCEP460":			       "DIS NC",
-				"HERACOMBNCEP575":			       "DIS NC",
-				"HERACOMBNCEP820":	 		       "DIS NC",
-				"HERACOMBNCEP920":			       "DIS NC",
-				"HERAF2CHARM":				       "DIS NC",
-				"ZEUSHERAF2B":				       "DIS NC",
-				"NMCPD":				       "DIS NC",
-				"NMC":					       "DIS NC",
-				"SLACP":				       "DIS NC",
-				"SLACD":				       "DIS NC",
-				"BCDMSP":				       "DIS NC",
-				"BCDMSD":				       "DIS NC",
-				"CHORUSNU":				       "DIS CC",
-				"CHORUSNB":				       "DIS CC",
-				"NTVNUDMN":				       "DIS CC",
-				"NTVNBDMN":				       "DIS CC"	}
+    cd = Loader().check_commondata(setname=name)
+    data_plotting = open(cd.plotfiles[1])
+    with open(data_plotting.name, 'r') as stream:
+        data_loaded = yaml.safe_load(stream)
+    proc = data_loaded['nnpdf31_process']
 
-    proc = process_dictionary.get(name)
-    if not proc:
-        log.warn(f'Unknown process type for dataset {name}')
-        return 'UNKNOWN'
     return proc
