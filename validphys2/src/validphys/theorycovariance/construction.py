01f49dab89bbd1fe2aecce59e06da4a4a33d4bdd
andreab1997 <andreab1997@yahoo.it>
2022-04-04 16:37:26 +0200

    Changed construction of thcovmat

--- validphys2/src/validphys/theorycovariance/construction.py
+++ validphys2/src/validphys/theorycovariance/construction.py
@@ -356,40 +356,149 @@ def covs_pt_prescrip(
     l = len(theoryids)
     start_proc = process_starting_points
     process_info = combine_by_type
-    covmats = defaultdict(list)
+    batches_list = []
     for name1 in process_info.theory:
-        for name2 in process_info.theory:
-            central1, *others1 = process_info.theory[name1]
-            deltas1 = list((other - central1 for other in others1))
-            central2, *others2 = process_info.theory[name2]
-            deltas2 = list((other - central2 for other in others2))
-            if l == 3:
-                if point_prescription == "3f point":
-                    s = covmat_3fpt(name1, name2, deltas1, deltas2)
-                elif point_prescription == "3r point":
-                    s = covmat_3rpt(name1, name2, deltas1, deltas2)
-                else:
-                    s = covmat_3pt(name1, name2, deltas1, deltas2)
-            elif l == 5:
-                # 5 point --------------------------------------------------------------
-                if fivetheories == "nobar":
-                    s = covmat_5pt(name1, name2, deltas1, deltas2)
-                # 5bar-point -----------------------------------------------------------
-                else:
-                    s = covmat_5barpt(name1, name2, deltas1, deltas2)
-            #  ---------------------------------------------------------------------
-            elif l == 7:
-                # Outdated 7pts implementation: left for posterity ---------------------
-                if seventheories == "original":
-                    s = covmat_7pt_orig(name1, name2, deltas1, deltas2)
-                # 7pt (Gavin) ----------------------------------------------------------
-                else:
-                    s = covmat_7pt(name1, name2, deltas1, deltas2)
-            elif l == 9:
-                s = covmat_9pt(name1, name2, deltas1, deltas2)
-            start_locs = (start_proc[name1], start_proc[name2])
-            covmats[start_locs] = s
-    return covmats
+        central, *others = process_info.theory[name1]
+        deltas = np.array(list((other - central for other in others)))
+        zeros = np.array(list(cen - cen for cen in central))
+        fact05 = np.array([deltas[0], deltas[1], deltas[2]])
+        fact1 = np.array([deltas[3], zeros, deltas[4]])
+        fact2 = np.array([deltas[5], deltas[6], deltas[7]])
+        shifts_fin = np.array([fact05,fact1,fact2])
+        batches_list.append(np.transpose(shifts_fin,axes=[2,1,0]))
+    shifts = shifts_vec(batches_list)
+    thcmat = thcovmat(shifts) 
+    return thcmat
+
+def shifts_vec(raw: list[np.ndarray]) -> list[np.ndarray]:
+    """Pump more dimensions into shifts.
+
+    Since renormalization scale for each process has a different meaning, the
+    most intrinsic and intuitive way to represent this difference is to put them
+    on different dimensions:
+
+    - a renormalization dimension is non-trivial only for the process that scale
+      is relative to
+    - a trivial dimension has the meaning of not affecting that datum
+    - trivial dimension can be exploited by `numpy` with the broadcasting
+      semantic
+
+    Parameters
+    ----------
+    raw : list[np.ndarray]
+        sequence of raw shifts, of dimension ``(n,3,3)``, like those generated
+        by :func:`raw_shifts`
+
+    Returns
+    -------
+    list[np.ndarray]
+        a list of batches, blown up with new dimensions to separate on different
+        dimensions the renormalization scales related to different processes
+
+    """
+    upgraded = []
+    n = len(raw)
+    dims = list(np.arange(n) + 2)
+    for i, batch in enumerate(raw):
+        newdims = dims.copy()
+        newdims.remove(i + 2)
+        upgraded.append(np.expand_dims(batch, newdims))
+
+    return upgraded
+
+
+def thcovmat(shifts: list[np.ndarray]) -> np.ndarray:
+    """Generate theory covariance matrix from upgraded vector of shifts.
+
+    Exploit `numpy` broadcasting to apply Eq.(4.2) of arXiv:1906.10698
+    literally.
+
+    The prescription used is always the 9-point one, so the other prescriptions
+    have to be implemented on the input and the output of this funcions, i.e.:
+
+    - the input has to be masked, to replace elements that should be missing
+      with 0s (it is trivial that the effect is the same)
+    - an overall normalization
+
+    Note
+    ----
+    In order to apply exactly Eq.(4.2), all the renormalization scale dimensions
+    should be non-trivial, and the broadcasting semantics should be implemented
+    for all dimensions and on the vector itself, such that a single rectangular
+    `np.ndarray` is obtained.
+
+    This of course is very inefficient for memory and operations, since
+    :math:`n-2` renormalization scales are always not involved while generating
+    a block for 2 given processes.
+
+    So for the :math:`n-2` scales it would correspond to an overall degeneracy
+    factor that would simplify with normalization, so we can simply skip (in
+    `numpy` this is done by the contraction on a dimension of size 1 for both
+    arrays, resulting in the removal of that dimension).
+
+    The remaining scales are at most 2, but not always 2, changing from
+    on-diagonal or off-diagonal blocks. The difference has to be compensated
+    with as a further degeneracy factor for the on-diagonal blocsk.
+
+    A minimal example is the case of only to process: there would be two shift
+    arrays, one each, of shapes ``[n1, nf, nr, 1]`` and ``[n2, nf, 1, nr]``. The
+    internal dimensions on data is irrelevant, so we can drop, as well as the
+    factorization scale one (that is always the same).
+    For definiteness let's consider ``nr = 3``, so we'll have two shifts arrays
+    of shapes ``[3,1]`` and ``[1, 3]``.
+
+    - off-diagonal: the contraction is done with ``[3,1]`` and ``[1,3]``, and
+      both the 1 dimensions are broadcasted to 3 before contraction, leading to
+      a 9 elements contraction
+    - on-diagonal: a contraction is ``[3, 1]`` with ``[3, 1]``, so the second
+      dimension is not broadcasted, leading to only 3 elements, but it should be
+      according to the meaning of Eq.(4.2); for this reason, a degeneracy
+      factor of 3 is multiplied, in order to make it homogeneous with the
+      off-diagonal blocks
+
+
+    Parameters
+    ----------
+    raw : list[np.ndarray]
+        sequence of upgraded shifts, like those generated by :func:`shifts_vec`
+
+    Returns
+    -------
+    np.ndarray
+        matrix of "covariances" generated out of shifts, whose shape is ``(N,
+        N)``, where ``N`` is the number of all data points (i.e. ``N =
+        sum(len(s) for s in shifts)``)
+
+    Raises
+    ------
+    ValueError
+      if not all the processes have the same number of points for their own
+      renormalization scales
+
+    """
+    blockmat = []
+    sumdims = list(np.arange(len(shifts) + 1) + 2)
+
+    # for each process there is only one non-trivial renormalization scale, so
+    # the other dimensions have to be 1
+    murs = np.array(shifts[0].shape[2:]).prod()
+    if not all(np.array(proc_shift.shape[2:]).prod() == murs for proc_shift in shifts):
+        raise ValueError(
+            "All the different renormalization scales should have the"
+            " same number of points"
+        )
+
+    for bi in shifts:
+        blockrow = []
+        for bj in shifts:
+            degeneracy = murs if bi is bj else 1.0
+            blockrow.append(
+                np.einsum(bi, [0, *sumdims], bj, [1, *sumdims], [0, 1]) * degeneracy
+            )
+        blockmat.append(blockrow)
+
+    return np.block(blockmat)
+           
 
 
 @table
