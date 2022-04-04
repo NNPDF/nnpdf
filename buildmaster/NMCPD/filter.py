3c55f9910f678517b1d7b0c19107767b92658944
enocera <enocera@ed.ac.uk>
2022-04-04 23:36:54 +0100

    Updated filter

--- buildmaster/NMCPD/filter.py
+++ buildmaster/NMCPD/filter.py
@@ -13,6 +13,7 @@ def filter_NMCPD():
     data_central = []
     kin = []
     error = []
+    error_nuc = []
     
     for i in tables:
         hepdata_tables="rawdata/HEPData-ins426595-"+version+"-Table_"+str(i)+".yaml"
@@ -35,12 +36,20 @@ def filter_NMCPD():
             error_value = {'stat_1': input['dependent_variables'][0]['values'][j]['errors'][0]['symerror'], 'syst_1': input['dependent_variables'][0]['values'][j]['errors'][1]['symerror']}
             error.append(error_value)
 
+            error_value_nuc = {'nuclear': input['dependent_variables'][0]['values'][j]['errors'][0]['symerror']}
+            error_nuc.append(error_value_nuc)
+
     error_definition = {'stat_1': {'description': "total statistical uncertainty", 'treatment': "ADD", 'type': "UNCORR"},
                         'syst_1': {'description': "total systematic uncertainty",  'treatment': "ADD", 'type': "CORR"}}
+
+    error_definition_dw = {'nuclear': {'description': "nuclear uncertainty (deweighted)", 'treatment': "ADD", 'type': "NUC_DW"}}
+    error_definition_sh = {'nuclear': {'description': "nuclear uncertainty (shifted)", 'treatment': "ADD", 'type': "NUC_SH"}}
     
     data_central_yaml  = { 'data_central' : data_central }
     kinematics_yaml    = { 'bins' : kin }
     uncertainties_yaml = { 'definition': error_definition, 'bins' : error }
+    uncertainties_dw_yaml = { 'definition': error_definition_dw, 'bins' : error_nuc }
+    uncertainties_sh_yaml = { 'definition': error_definition_sh, 'bins' : error_nuc }
     
     with open('data.yaml', 'w') as file:
          yaml.dump(data_central_yaml, file, sort_keys=False)
@@ -50,5 +59,11 @@ def filter_NMCPD():
 
     with open('uncertainties.yaml', 'w') as file:
         yaml.dump(uncertainties_yaml, file, sort_keys=False)
+
+    with open('uncertainties_dw.yaml', 'w') as file:
+        yaml.dump(uncertainties_dw_yaml, file, sort_keys=False)
+
+    with open('uncertainties_sh.yaml', 'w') as file:
+        yaml.dump(uncertainties_sh_yaml, file, sort_keys=False)    
          
 filter_NMCPD()
