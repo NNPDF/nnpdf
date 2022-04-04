d2e7471b0e6697386c3d7555467b7a230059f8a1
enocera <enocera@ed.ac.uk>
2022-04-04 23:29:13 +0100

    Revised prototype data set

--- buildmaster/NMCPD/filter.py
+++ buildmaster/NMCPD/filter.py
@@ -1,5 +1,6 @@
 #Filter for NMCPD
 
+import sys
 import yaml
 
 def filter_NMCPD():
@@ -10,12 +11,8 @@ def filter_NMCPD():
     tables  = metadata['hepdata']['tables']
 
     data_central = []
-    kin_avg = []
-    kin_min = []
-    kin_max = []
-    stat_error = []
-    sys_error = []
-    index = 1
+    kin = []
+    error = []
     
     for i in tables:
         hepdata_tables="rawdata/HEPData-ins426595-"+version+"-Table_"+str(i)+".yaml"
@@ -27,38 +24,31 @@ def filter_NMCPD():
         sqrts = float(input['dependent_variables'][0]['qualifiers'][3]['value'])
         
         for j in range(len(values)):
-            data_central_value = {'index' : index, 'value' : input['dependent_variables'][0]['values'][j]['value']}      
+            
+            data_central_value = input['dependent_variables'][0]['values'][j]['value']    
             data_central.append(data_central_value)
             Q2 = input['independent_variables'][0]['values'][j]['value']
             y = Q2 / ( sqrts * sqrts * x )
-            kin_avg_value = {'index' : index , 'x' : x , 'q2' : Q2 , 'y' : y}
-            kin_avg.append(kin_avg_value)
-            kin_min_value = {'index' : index , 'x' : x , 'q2' : Q2 , 'y' : y}
-            kin_min.append(kin_min_value)
-            kin_max_value = {'index' : index , 'x' : x , 'q2' : Q2 , 'y' : y}
-            kin_max.append(kin_max_value)
+            kin_value = {'x' : {'min': None, 'mid': x , 'max': None}, 'q2' : {'min': None, 'mid': Q2 , 'max': None} , 'y' : {'min': None, 'mid': y , 'max': None}}
+            kin.append(kin_value)
             
-            stat_error_value = {'index' : index, 'value' : input['dependent_variables'][0]['values'][j]['errors'][0]['symerror']}
-            stat_error.append(stat_error_value)
-            sys_error_value  = {'index' : index, 'value' : input['dependent_variables'][0]['values'][j]['errors'][1]['symerror']}
-            sys_error.append(sys_error_value)
-            
-            index = index + 1
+            error_value = {'stat_1': input['dependent_variables'][0]['values'][j]['errors'][0]['symerror'], 'syst_1': input['dependent_variables'][0]['values'][j]['errors'][1]['symerror']}
+            error.append(error_value)
 
-    data_central_yaml = {'data_central' : data_central}
-    kinematics_yaml = {'kin_avg' : kin_avg, 'kin_min' : kin_min, 'kin_max' : kin_max}
-    uncertainties_yaml = {'stat' : stat_error, 'sys_1' : {'mode' : ["ADD", "CORR"] , 'errors' : sys_error} }
+    error_definition = {'stat_1': {'description': "total statistical uncertainty", 'treatment': "ADD", 'type': "UNCORR"},
+                        'syst_1': {'description': "total systematic uncertainty",  'treatment': "ADD", 'type': "CORR"}}
+    
+    data_central_yaml  = { 'data_central' : data_central }
+    kinematics_yaml    = { 'bins' : kin }
+    uncertainties_yaml = { 'definition': error_definition, 'bins' : error }
     
     with open('data.yaml', 'w') as file:
-         yaml.dump(data_central_yaml, file)
+         yaml.dump(data_central_yaml, file, sort_keys=False)
 
     with open('kinematics.yaml', 'w') as file:
-         yaml.dump(kinematics_yaml, file)
+         yaml.dump(kinematics_yaml, file, sort_keys=False)
 
     with open('uncertainties.yaml', 'w') as file:
-        yaml.dump(uncertainties_yaml, file)
-
-    with open('uncertainties_dw.yaml', 'w') as file:
-        yaml.dump(uncertainties_yaml, file)
+        yaml.dump(uncertainties_yaml, file, sort_keys=False)
          
 filter_NMCPD()
