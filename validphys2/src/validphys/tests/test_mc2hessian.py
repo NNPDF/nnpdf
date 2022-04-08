1eaff2b4428531aa74af1462f7ae9e9e598f3923
juacrumar <juacrumar@lairen.eu>
2022-04-08 14:04:23 +0200

    change lhapdf data dir

--- validphys2/src/validphys/tests/test_mc2hessian.py
+++ validphys2/src/validphys/tests/test_mc2hessian.py
@@ -1,12 +1,25 @@
 """
     Test for the mc2hessian module
 """
+import contextlib
+import lhapdf
 from validphys.api import API
 
 NEIG = 5
 
 
-def test_mc2hessian(data_config, tmp, monkeypatch):
+@contextlib.contextmanager
+def temp_lhapdf_path(folder):
+    """Modify the data path for LHAPDF sets"""
+    oldpaths = lhapdf.paths()
+    lhapdf.setPaths([str(folder)])
+    try:
+        yield
+    finally:
+        lhapdf.setPaths(oldpaths)
+
+
+def test_mc2hessian(data_config, tmp):
     """Tests that the generated hessian PDF is indeed marked as such
     and that the metadata is not obviously broken
     """
@@ -18,24 +31,24 @@ def test_mc2hessian(data_config, tmp, monkeypatch):
     API.mc2hessian(**config)
 
     # Save a reference to the original pdf
-    orig_pdf = API.pdf(pdf=config["pdf"])
+    orig_pdf_info = API.pdf(pdf=config["pdf"]).info
 
-    # Now try to read the PDF we just wrote
-    monkeypatch.setenv("LHAPDF_DATA_PATH", tmp.as_posix())
-    pdf = API.pdf(pdf=pdf_hessian_name)
+    # Now try to read the PDF we just wrote to some non_std location
+    with temp_lhapdf_path(tmp.as_posix()):
+        pdf = API.pdf(pdf=pdf_hessian_name)
 
-    # Check the correct changed metadata
-    assert pdf.error_type == "symmhessian"
-    assert len(pdf) == NEIG + 1
+        # Check the correct changed metadata
+        assert pdf.error_type == "symmhessian"
+        assert len(pdf) == NEIG + 1
 
-    # Now check that all info is the same as the original
-    # except for ErrorType that _must_ be different
-    skip_this = ["SetDesc", "NumMembers"]
-    for key, item in orig_pdf.info.items():
-        if key in skip_this:
-            continue
-        new_item = pdf.info[key]
-        if key == "ErrorType":
-            assert item != new_item
-        else:
-            assert item == new_item
+        # Now check that all info is the same as the original
+        # except for ErrorType that _must_ be different
+        skip_this = ["SetDesc", "NumMembers"]
+        for key, item in orig_pdf_info.items():
+            if key in skip_this:
+                continue
+            new_item = pdf.info[key]
+            if key == "ErrorType":
+                assert item != new_item
+            else:
+                assert item == new_item
