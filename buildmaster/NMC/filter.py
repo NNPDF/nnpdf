78a41bc1216ee516d025704f4c8f72ee5750c0cc
Luigi Del Debbio <luigi.del.debbio@ed.ac.uk>
2022-06-17 12:13:11 +0100

    NMC implementation

---
+++ buildmaster/NMC/filter.py
@@ -0,0 +1,55 @@
+#Filter for NMC
+
+import sys
+import yaml
+
+def filter_NMC():
+    with open('metadata.yaml', 'r') as file:
+        metadata = yaml.safe_load(file)
+
+    version = metadata['hepdata']['version']
+    tables  = metadata['hepdata']['tables']
+
+    data_central = []
+    kin = []
+    error = []
+    
+    for i in tables:
+        hepdata_tables="rawdata/HEPData-ins424154-v"+str(version)+"-yaml/Table"+str(i)+".yaml"
+        with open(hepdata_tables, 'r') as file:
+            input = yaml.safe_load(file)
+            
+        values = input['dependent_variables'][0]['values']
+
+        x = float(input['dependent_variables'][0]['qualifiers'][3]['value'])
+        sqrts = float(input['dependent_variables'][0]['qualifiers'][2]['value'])
+
+        for j in range(len(values)):
+            
+            data_central_value = input['dependent_variables'][0]['values'][j]['value']    
+            data_central.append(data_central_value)
+            Q2 = input['independent_variables'][0]['values'][j]['value']
+            y = Q2 / ( sqrts * sqrts * x )
+            kin_value = {'x' : {'min': None, 'mid': x , 'max': None}, 'q2' : {'min': None, 'mid': Q2 , 'max': None} , 'y' : {'min': None, 'mid': y , 'max': None}}
+            kin.append(kin_value)
+            
+            error_value = {'stat_1': input['dependent_variables'][0]['values'][j]['errors'][0]['symerror'], 'syst_1': input['dependent_variables'][0]['values'][j]['errors'][1]['symerror']}
+            error.append(error_value)
+
+    error_definition = {'stat_1': {'description': "total statistical uncertainty", 'treatment': "ADD", 'type': "UNCORR"},
+                        'syst_1': {'description': "total systematic uncertainty",  'treatment': "ADD", 'type': "CORR"}}
+    
+    data_central_yaml  = { 'data_central' : data_central }
+    kinematics_yaml    = { 'bins' : kin }
+    uncertainties_yaml = { 'definition': error_definition, 'bins' : error }
+    
+    with open('data.yaml', 'w') as file:
+         yaml.dump(data_central_yaml, file, sort_keys=False)
+
+    with open('kinematics.yaml', 'w') as file:
+         yaml.dump(kinematics_yaml, file, sort_keys=False)
+
+    with open('uncertainties.yaml', 'w') as file:
+        yaml.dump(uncertainties_yaml, file, sort_keys=False)
+         
+filter_NMC()
