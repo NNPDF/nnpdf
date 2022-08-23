998e2fd61e975a0fffac11821496b2be9e463626
Giacomo Magni <giac.magni@gmail.com>
2022-08-23 09:57:54 +0200

    move evolven3fit to n3fit setup

--- n3fit/src/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="evolven3fit",
-    version="1.0",
-    packages=find_packages(include=["evolven3fit", "evolven3fit.*"]),
-    include_package_data=True,
-    entry_points={"console_scripts": ["evolven3fit=evolven3fit.evolven3fit_cli:cli"]},
-)
