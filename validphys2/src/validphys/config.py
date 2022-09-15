5ececa65ddf0b5974020f155a6de21b55a46b323
Juan M. Cruz-Martinez <juacrumar@lairen.eu>
2022-09-15 15:47:03 +0200

    Re-enable `use_t0` flag.
    See https://github.com/NNPDF/nnpdf/pull/1489#discussion_r971613012

--- validphys2/src/validphys/config.py
+++ validphys2/src/validphys/config.py
@@ -1051,12 +1051,12 @@ class CoreConfig(configparser.Config):
 
     # TODO: Find a good name for this
     def produce_t0set(
-        self, t0pdfset=None, use_t0_sampling=False, use_t0_fitting=True,
+        self, t0pdfset=None, use_t0_sampling=False, use_t0_fitting=True, use_t0=False
     ):
         """Return the t0set if use_t0 is True and None otherwise. Raises an
         error if t0 is requested but no t0set is given.
         """
-        if use_t0_sampling or use_t0_fitting:
+        if use_t0_sampling or use_t0_fitting or use_t0:
             if not t0pdfset:
                 raise ConfigError("Setting use_t0 requires specifying a valid t0pdfset")
             return t0pdfset
