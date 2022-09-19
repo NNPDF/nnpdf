9d355d743c961a875d2be1d424ecc94194fa2222
andreab1997 <andreab1997@yahoo.it>
2022-09-19 12:12:50 +0200

    Add test for eko_utils

--- n3fit/src/n3fit/tests/test_evolven3fit.py
+++ n3fit/src/n3fit/tests/test_evolven3fit.py
@@ -1,9 +1,10 @@
-from evolven3fit import utils
+from evolven3fit import utils, eko_utils
 import pathlib
 from numpy.testing import assert_allclose
 import numpy as np
 import pytest 
 from validphys.pdfbases import PIDS_DICT
+from validphys.loader import Loader
 
 REGRESSION_FOLDER = pathlib.Path(__file__).with_name("regressions")
 
@@ -40,3 +41,17 @@ def test_utils():
     ID = utils.get_theoryID_from_runcard(REGRESSION_FOLDER)
     assert ID == 162
 
+def test_eko_utils():
+    #Testing construct eko cards
+    theoryID = 200
+    theory = Loader().check_theoryID(theoryID).get_description()
+    q_fin = 100
+    q_points = 5
+    x_grid = [1.e-7, 1.e-5, 1.e-3, 0.1, 1.0]
+    t_card, op_card = eko_utils.construct_eko_cards(theoryID, {}, {'Comments' : "Test"}, q_fin, q_points, x_grid)
+    assert t_card['Qref'] == 91.2
+    assert t_card['PTO'] == theory['PTO']
+    assert t_card['Q0'] == theory['Q0']
+    assert t_card['Comments'] == "Test"
+    assert_allclose(op_card["interpolation_xgrid"], x_grid)
+    assert_allclose(op_card["Q2grid"], [2.7224999999999997, 24.2064, 259.30618155454425, 2777.7652105392926, 29756.25])
