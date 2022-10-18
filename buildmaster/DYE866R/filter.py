cf4f4ed835c979dc84a1f99df85d02d5b587baea
tgiani <s1792848@staffmail.ed.ac.uk>
2022-10-18 14:41:18 +0200

    adding kinematic as in old implmenetatio, y rather than xF

--- buildmaster/DYE866R/filter.py
+++ buildmaster/DYE866R/filter.py
@@ -1,59 +1,80 @@
-#Filter for DYE866R
+# Filter for DYE866R
 
 import sys
 import yaml
+import numpy as np
+
 
 def filter_DYE866R():
-    with open('metadata.yaml', 'r') as file:
+    with open("metadata.yaml", "r") as file:
         metadata = yaml.safe_load(file)
 
-    version = metadata['hepdata']['version']
-    tables  = metadata['hepdata']['tables']
+    version = metadata["hepdata"]["version"]
+    tables = metadata["hepdata"]["tables"]
 
     data_central = []
     kin = []
     error = []
-    
-    for i in tables:
-        hepdata_tables="rawdata/HEPData-ins554316-v"+str(version)+"-Table_"+str(i)+".yaml"
-        with open(hepdata_tables, 'r') as file:
-            input = yaml.safe_load(file)
-        
-        values = input['dependent_variables'][0]['values']
-        sqrts = float(input['dependent_variables'][0]['qualifiers'][3]['value'])
-        
-        for j in range(len(values)):
-            
-            data_central_value = values[j]['value']    
-            data_central.append(data_central_value)
-            x2 = input['independent_variables'][0]['values'][j]['value']
-            x2_min = input['independent_variables'][0]['values'][j]['low']
-            x2_max = input['independent_variables'][0]['values'][j]['high']
-            xF = input['independent_variables'][1]['values'][j]['value']
-            M = input['independent_variables'][3]['values'][j]['value']
 
-            kin_value = {'x2' : {'min': x2_min, 'mid': x2 , 'max': x2_max}, 'xF' : {'min': None, 'mid': xF , 'max': None} , 'M' : {'min': None, 'mid': M , 'max': None}}
+    for i in tables:
+        hepdata_tables = (
+            "rawdata/HEPData-ins554316-v" + str(version) + "-Table_" + str(i) + ".yaml"
+        )
+        with open(hepdata_tables, "r") as file:
+            input = yaml.safe_load(file)
+
+        values = input["dependent_variables"][0]["values"]
+        sqrts = float(input["dependent_variables"][0]["qualifiers"][3]["value"])
+
+        for j in range(len(values)):
+
+            data_central_value = values[j]["value"]
+            data_central.append(data_central_value)
+            xF = input["independent_variables"][1]["values"][j]["value"]
+            M = input["independent_variables"][3]["values"][j]["value"]
+            J = np.sqrt(xF**2 + 4 * M**2 / sqrts**2)
+            y = float(0.5 * np.log((J + xF) / (J - xF)))
+
+            kin_value = {
+                "y": {"min": None, "mid": y, "max": None},
+                "M": {"min": None, "mid": M, "max": None},
+                "sqrts": {"min": None, "mid": sqrts, "max": None},
+            }
             kin.append(kin_value)
-            
-            error_value = {'stat_1': input['dependent_variables'][0]['values'][j]['errors'][0]['symerror'], 'syst_1': data_central_value*0.97*1e-2}
+
+            error_value = {
+                "stat_1": input["dependent_variables"][0]["values"][j]["errors"][0][
+                    "symerror"
+                ],
+                "syst_1": data_central_value * 0.97 * 1e-2,
+            }
             error.append(error_value)
 
+    error_definition = {
+        "stat_1": {
+            "description": "total statistical uncertainty",
+            "treatment": "ADD",
+            "type": "UNCORR",
+        },
+        "syst_1": {
+            "description": "total systematic uncertainty, 0.97%",
+            "treatment": "ADD",
+            "type": "CORR",
+        },
+    }
 
-    error_definition = {'stat_1': {'description': "total statistical uncertainty", 'treatment': "ADD", 'type': "UNCORR"},
-                        'syst_1': {'description': "total systematic uncertainty, 0.97%",  'treatment': "ADD", 'type': "CORR"}}
+    data_central_yaml = {"data_central": data_central}
+    kinematics_yaml = {"bins": kin}
+    uncertainties_yaml = {"definition": error_definition, "bins": error}
 
-    
-    data_central_yaml  = { 'data_central' : data_central }
-    kinematics_yaml    = { 'bins' : kin }
-    uncertainties_yaml = { 'definition': error_definition, 'bins' : error }
-    
-    with open('data.yaml', 'w') as file:
-         yaml.dump(data_central_yaml, file, sort_keys=False)
+    with open("data.yaml", "w") as file:
+        yaml.dump(data_central_yaml, file, sort_keys=False)
 
-    with open('kinematics.yaml', 'w') as file:
-         yaml.dump(kinematics_yaml, file, sort_keys=False)
+    with open("kinematics.yaml", "w") as file:
+        yaml.dump(kinematics_yaml, file, sort_keys=False)
 
-    with open('uncertainties.yaml', 'w') as file:
+    with open("uncertainties.yaml", "w") as file:
         yaml.dump(uncertainties_yaml, file, sort_keys=False)
-         
+
+
 filter_DYE866R()
