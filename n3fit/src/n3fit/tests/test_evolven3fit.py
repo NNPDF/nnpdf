778ba10e338075991d2ec26b183123a08fc9523c
Andrea Barontini <andreab1997@yahoo.it>
2022-10-25 14:38:42 +0200

    Update n3fit/src/n3fit/tests/test_evolven3fit.py
    Co-authored-by: Juan M. Cruz-Martinez <juacrumar@lairen.eu>

--- n3fit/src/n3fit/tests/test_evolven3fit.py
+++ n3fit/src/n3fit/tests/test_evolven3fit.py
@@ -1,4 +1,3 @@
-from evolven3fit_new import utils, eko_utils
 import pathlib
 import logging
 from numpy.testing import assert_allclose
@@ -6,6 +5,7 @@ import numpy as np
 import pytest 
 from validphys.pdfbases import PIDS_DICT
 from validphys.loader import Loader
+from evolven3fit_new import utils, eko_utils
 
 REGRESSION_FOLDER = pathlib.Path(__file__).with_name("regressions")
 log = logging.getLogger(__name__)
