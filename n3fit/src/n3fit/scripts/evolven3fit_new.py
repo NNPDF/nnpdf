fb487fe73fdcb6557fa461569b5b8d7d893a296c
andreab1997 <andreab1997@yahoo.it>
2022-10-26 17:38:25 +0200

    Put Xgrid top level

--- n3fit/src/n3fit/scripts/evolven3fit_new.py
+++ n3fit/src/n3fit/scripts/evolven3fit_new.py
@@ -11,119 +11,7 @@ from evolven3fit_new import eko_utils, evolve, utils
 
 log = logging.getLogger(__name__)
 
-
-def main():
-    parser = ArgumentParser(
-        description="evolven3fit_new - a script with tools to evolve PDF fits"
-    )
-    subparsers = parser.add_subparsers(title="actions", dest="actions")
-    evolvefit_parser = subparsers.add_parser(
-        "evolve",
-        help="Evolves the fitted PDFs. The q_grid starts at the Q0 given by the theory but the last point is q_fin and its number of points can be specified by q_points. If a path is given for the dump option, the eko will be dumped in that path after the computation. If a path is given for the load option, the eko to be used for the evolution will be loaded from that path. The two options are mutually exclusive.",
-    )
-    eko_parser = subparsers.add_parser(
-        "produce_eko",
-        help="Produce the eko for the specified theory. The q_grid starts at the Q0 given by the theory but the last point is q_fin and its number of points can be specified by q_points. The x_grid starts at x_grid_ini and ends at 1.0 and contains the provided number of points. The eko will be dumped in the provided path.",
-    )
-    evolvefit_parser.add_argument(
-        "configuration_folder",
-        help="Path to the folder containing the (pre-DGLAP) fit result",
-    )
-    parser.add_argument(
-        "-q",
-        "--q-fin",
-        type=float,
-        default=None,
-        help="Final q-value of the evolution",
-    )
-    parser.add_argument(
-        "-p",
-        "--q-points",
-        type=int,
-        default=None,
-        help="Number of q points for the evolution",
-    )
-    parser.add_argument(
-        "-n",
-        "--n-cores",
-        type=int,
-        default=1,
-        help="Number of cores to be used",
-    )
-    evolvefit_parser.add_argument(
-        "-l",
-        "--load",
-        type=pathlib.Path,
-        default=None,
-        help="Path of the EKO to be loaded",
-    )
-    evolvefit_parser.add_argument(
-        "-d",
-        "--dump",
-        type=pathlib.Path,
-        default=None,
-        help="Path where the EKO is dumped",
-    )
-    evolvefit_parser.add_argument(
-        "-f",
-        "--force",
-        action="store_true",
-        help="Force the evolution to be done even if it has already been done",
-    )
-    eko_parser.add_argument(
-        "theoryID", type=int, help="ID of the theory used to produce the eko"
-    )
-    eko_parser.add_argument(
-        "dump",
-        type=pathlib.Path,
-        help="Path where the EKO is dumped",
-    )
-    eko_parser.add_argument(
-        "-i",
-        "--x-grid-ini",
-        default=None,
-        type=float,
-        help="Starting point of the x-grid",
-    )
-    eko_parser.add_argument(
-        "-p",
-        "--x-grid-points",
-        default=None,
-        type=int,
-        help="Number of points of the x-grid",
-    )
-    args = parser.parse_args()
-    op_card_info = {
-        "ev_op_max_order": 10,
-        "ev_op_iterations": 1,
-        "n_integration_cores": args.n_cores,
-        "backward_inversion": "expanded",
-    }
-    t_card_info = {}
-    if args.actions == "evolve":
-        cli_evolven3fit_new(
-            args.configuration_folder,
-            args.q_fin,
-            args.q_points,
-            op_card_info,
-            t_card_info,
-            args.dump,
-            args.load,
-            args.force,
-        )
-    elif args.actions == "produce_eko":
-        stdout_log = logging.StreamHandler(sys.stdout)
-        stdout_log.setLevel(logging.INFO)
-        stdout_log.setFormatter(
-            logging.Formatter("%(asctime)s %(name)s/%(levelname)s: %(message)s")
-        )
-        for logger_ in (log, *[logging.getLogger("eko")]):
-            logger_.handlers = []
-            logger_.setLevel(logging.INFO)
-            logger_.addHandler(stdout_log)
-        if args.x_grid_ini is None:
-            if args.x_grid_points is None:
-                x_grid = np.array(
+XGRID = np.array(
                     [
                         1.00000000000000e-09,
                         1.29708482343957e-09,
@@ -323,6 +211,119 @@ def main():
                         1.00000000000000e00,
                     ]
                 )
+
+def main():
+    parser = ArgumentParser(
+        description="evolven3fit_new - a script with tools to evolve PDF fits"
+    )
+    subparsers = parser.add_subparsers(title="actions", dest="actions")
+    evolvefit_parser = subparsers.add_parser(
+        "evolve",
+        help="Evolves the fitted PDFs. The q_grid starts at the Q0 given by the theory but the last point is q_fin and its number of points can be specified by q_points. If a path is given for the dump option, the eko will be dumped in that path after the computation. If a path is given for the load option, the eko to be used for the evolution will be loaded from that path. The two options are mutually exclusive.",
+    )
+    eko_parser = subparsers.add_parser(
+        "produce_eko",
+        help="Produce the eko for the specified theory. The q_grid starts at the Q0 given by the theory but the last point is q_fin and its number of points can be specified by q_points. The x_grid starts at x_grid_ini and ends at 1.0 and contains the provided number of points. The eko will be dumped in the provided path.",
+    )
+    evolvefit_parser.add_argument(
+        "configuration_folder",
+        help="Path to the folder containing the (pre-DGLAP) fit result",
+    )
+    parser.add_argument(
+        "-q",
+        "--q-fin",
+        type=float,
+        default=None,
+        help="Final q-value of the evolution",
+    )
+    parser.add_argument(
+        "-p",
+        "--q-points",
+        type=int,
+        default=None,
+        help="Number of q points for the evolution",
+    )
+    parser.add_argument(
+        "-n",
+        "--n-cores",
+        type=int,
+        default=1,
+        help="Number of cores to be used",
+    )
+    evolvefit_parser.add_argument(
+        "-l",
+        "--load",
+        type=pathlib.Path,
+        default=None,
+        help="Path of the EKO to be loaded",
+    )
+    evolvefit_parser.add_argument(
+        "-d",
+        "--dump",
+        type=pathlib.Path,
+        default=None,
+        help="Path where the EKO is dumped",
+    )
+    evolvefit_parser.add_argument(
+        "-f",
+        "--force",
+        action="store_true",
+        help="Force the evolution to be done even if it has already been done",
+    )
+    eko_parser.add_argument(
+        "theoryID", type=int, help="ID of the theory used to produce the eko"
+    )
+    eko_parser.add_argument(
+        "dump",
+        type=pathlib.Path,
+        help="Path where the EKO is dumped",
+    )
+    eko_parser.add_argument(
+        "-i",
+        "--x-grid-ini",
+        default=None,
+        type=float,
+        help="Starting point of the x-grid",
+    )
+    eko_parser.add_argument(
+        "-p",
+        "--x-grid-points",
+        default=None,
+        type=int,
+        help="Number of points of the x-grid",
+    )
+    args = parser.parse_args()
+    op_card_info = {
+        "ev_op_max_order": 10,
+        "ev_op_iterations": 1,
+        "n_integration_cores": args.n_cores,
+        "backward_inversion": "expanded",
+    }
+    t_card_info = {}
+    if args.actions == "evolve":
+        cli_evolven3fit_new(
+            args.configuration_folder,
+            args.q_fin,
+            args.q_points,
+            op_card_info,
+            t_card_info,
+            args.dump,
+            args.load,
+            args.force,
+        )
+    elif args.actions == "produce_eko":
+        stdout_log = logging.StreamHandler(sys.stdout)
+        stdout_log.setLevel(logging.INFO)
+        stdout_log.setFormatter(
+            logging.Formatter("%(asctime)s %(name)s/%(levelname)s: %(message)s")
+        )
+        for logger_ in (log, *[logging.getLogger("eko")]):
+            logger_.handlers = []
+            logger_.setLevel(logging.INFO)
+            logger_.addHandler(stdout_log)
+        if args.x_grid_ini is None:
+            if args.x_grid_points is None:
+                x_grid = XGRID
             else:
                 raise ValueError(
                     "x_grid_ini and x_grid_points must be specified either both or none of them"
