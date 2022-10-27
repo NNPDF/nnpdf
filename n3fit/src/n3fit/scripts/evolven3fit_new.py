c947dfd413931ef314c57d4ef1323e124aad5c76
andreab1997 <andreab1997@yahoo.it>
2022-10-27 12:09:39 +0200

    Move the definitions of the subcommands to external functions

--- n3fit/src/n3fit/scripts/evolven3fit_new.py
+++ n3fit/src/n3fit/scripts/evolven3fit_new.py
@@ -211,23 +211,67 @@ XGRID = np.array(
                         1.00000000000000e00,
                     ]
                 )
+def construct_eko_parser(subparsers):
+    parser = subparsers.add_parser("produce_eko",
+        help="Produce the eko for the specified theory. The q_grid starts at the Q0 given by the theory but the last point is q_fin and its number of points can be specified by q_points. The x_grid starts at x_grid_ini and ends at 1.0 and contains the provided number of points. The eko will be dumped in the provided path.")
+    parser.add_argument(
+        "theoryID", type=int, help="ID of the theory used to produce the eko"
+    )
+    parser.add_argument(
+        "dump",
+        type=pathlib.Path,
+        help="Path where the EKO is dumped",
+    )
+    parser.add_argument(
+        "-i",
+        "--x-grid-ini",
+        default=None,
+        type=float,
+        help="Starting point of the x-grid",
+    )
+    parser.add_argument(
+        "-p",
+        "--x-grid-points",
+        default=None,
+        type=int,
+        help="Number of points of the x-grid",
+    )
+    return parser
+
+def construct_evolven3fit_parser(subparsers):
+    parser = subparsers.add_parser(
+        "evolve",
+        help="Evolves the fitted PDFs. The q_grid starts at the Q0 given by the theory but the last point is q_fin and its number of points can be specified by q_points. If a path is given for the dump option, the eko will be dumped in that path after the computation. If a path is given for the load option, the eko to be used for the evolution will be loaded from that path. The two options are mutually exclusive.",
+    )
+    parser.add_argument(
+        "configuration_folder",
+        help="Path to the folder containing the (pre-DGLAP) fit result",
+    )
+    parser.add_argument(
+        "-l",
+        "--load",
+        type=pathlib.Path,
+        default=None,
+        help="Path of the EKO to be loaded",
+    )
+    parser.add_argument(
+        "-d",
+        "--dump",
+        type=pathlib.Path,
+        default=None,
+        help="Path where the EKO is dumped",
+    )
+    parser.add_argument(
+        "-f",
+        "--force",
+        action="store_true",
+        help="Force the evolution to be done even if it has already been done",
+    )
+    return parser
 
 def main():
     parser = ArgumentParser(
         description="evolven3fit_new - a script with tools to evolve PDF fits"
-    )
-    subparsers = parser.add_subparsers(title="actions", dest="actions")
-    evolvefit_parser = subparsers.add_parser(
-        "evolve",
-        help="Evolves the fitted PDFs. The q_grid starts at the Q0 given by the theory but the last point is q_fin and its number of points can be specified by q_points. If a path is given for the dump option, the eko will be dumped in that path after the computation. If a path is given for the load option, the eko to be used for the evolution will be loaded from that path. The two options are mutually exclusive.",
-    )
-    eko_parser = subparsers.add_parser(
-        "produce_eko",
-        help="Produce the eko for the specified theory. The q_grid starts at the Q0 given by the theory but the last point is q_fin and its number of points can be specified by q_points. The x_grid starts at x_grid_ini and ends at 1.0 and contains the provided number of points. The eko will be dumped in the provided path.",
-    )
-    evolvefit_parser.add_argument(
-        "configuration_folder",
-        help="Path to the folder containing the (pre-DGLAP) fit result",
     )
     parser.add_argument(
         "-q",
@@ -250,48 +294,10 @@ def main():
         default=1,
         help="Number of cores to be used",
     )
-    evolvefit_parser.add_argument(
-        "-l",
-        "--load",
-        type=pathlib.Path,
-        default=None,
-        help="Path of the EKO to be loaded",
-    )
-    evolvefit_parser.add_argument(
-        "-d",
-        "--dump",
-        type=pathlib.Path,
-        default=None,
-        help="Path where the EKO is dumped",
-    )
-    evolvefit_parser.add_argument(
-        "-f",
-        "--force",
-        action="store_true",
-        help="Force the evolution to be done even if it has already been done",
-    )
-    eko_parser.add_argument(
-        "theoryID", type=int, help="ID of the theory used to produce the eko"
-    )
-    eko_parser.add_argument(
-        "dump",
-        type=pathlib.Path,
-        help="Path where the EKO is dumped",
-    )
-    eko_parser.add_argument(
-        "-i",
-        "--x-grid-ini",
-        default=None,
-        type=float,
-        help="Starting point of the x-grid",
-    )
-    eko_parser.add_argument(
-        "-p",
-        "--x-grid-points",
-        default=None,
-        type=int,
-        help="Number of points of the x-grid",
-    )
+    subparsers = parser.add_subparsers(title="actions", dest="actions")
+    eko_parser = construct_eko_parser(subparsers)
+    evolven3fit_parser = construct_evolven3fit_parser(subparsers)
+    
     args = parser.parse_args()
     op_card_info = {
         "ev_op_max_order": 10,
