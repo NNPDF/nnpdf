5826af38d8e7e258fbf1f9b8b0424222c3ba9589
Zahari Kassabov <zk261@cam.ac.uk>
2022-11-09 13:25:51 +0000

    Remove additional t0 flags
    The work on covariance matrices added new flags to control to t0
    setting, namely `use_t0_sampling` and `use_t0_fitting`. These were
    undocumented, and caused bugs in validphys since t0pdfset could be set
    by either of them.
    
    Remove both flags and mandate that the fitting covmat always uses t0
    while the sampling covmat never uses it.
    
    This removes some footguns and its backwards compatible.
    
    It has the minor disadvantage that the fitting covmat implicitly
    requires use_t0=True, which is redundant, but it only really matters in
    n3fit and we set the flag there anyway.

--- n3fit/src/n3fit/scripts/n3fit_exec.py
+++ n3fit/src/n3fit/scripts/n3fit_exec.py
@@ -158,10 +158,8 @@ class N3FitConfig(Config):
             N3FIT_FIXED_CONFIG['use_scalevar_uncertainties'] = thconfig.get('use_scalevar_uncertainties', True) 
         #Sampling flags
         if (sam_t0:=file_content.get('sampling')) is not None:
-            N3FIT_FIXED_CONFIG['use_t0_sampling'] = sam_t0.get('use_t0', False) 
             N3FIT_FIXED_CONFIG['separate_multiplicative'] = sam_t0.get('separate_multiplicative', True) 
         #Fitting flag
-        N3FIT_FIXED_CONFIG['use_t0_fitting'] = file_content.get('fitting').get('use_t0', True)
         file_content.update(N3FIT_FIXED_CONFIG)
         return cls(file_content, *args, **kwargs)
 
