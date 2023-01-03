NNPDF/nnpdf

commit e909c15842f01bf95444ba70cdb64b974d9e7648

bufix: use fakepdf to generate pseudodata instead of t0pdfset

validphys2/src/validphys/config.py
@@ -1041,6 +1041,10 @@ class CoreConfig(configparser.Config):
             return t0pdfset
         return None
 
+    def parse_fakepdf(self, name):
+        """PDF set used to generate the t0 covmat."""
+        return self.parse_pdf(name)
+
     def _parse_lagrange_multiplier(self, kind, theoryid, setdict):
         """ Lagrange multiplier constraints are mappings
         containing a `dataset` and a `maxlambda` argument which

validphys2/src/validphys/filters.py
@@ -94,21 +94,21 @@ def prepare_nnpdf_rng(filterseed:int, rngalgo:int, seed:int):
     RandomGenerator.GetRNG().SetSeed(filterseed)
 
 @check_positive('errorsize')
-def filter_closure_data(filter_path, data, t0pdfset, fakenoise, errorsize, prepare_nnpdf_rng):
+def filter_closure_data(filter_path, data, fakepdf, fakenoise, errorsize, prepare_nnpdf_rng):
     """Filter closure data. In addition to cutting data points, the data is
-    generated from an underlying ``t0pdfset``, applying a shift to the data
+    generated from an underlying ``fakepdf``, applying a shift to the data
     if ``fakenoise`` is ``True``, which emulates the experimental central values
     being shifted away from the underlying law.
 
     """
     log.info('Filtering closure-test data.')
     return _filter_closure_data(
-        filter_path, data, t0pdfset, fakenoise, errorsize)
+        filter_path, data, fakepdf, fakenoise, errorsize)
 
 
 @check_positive("errorsize")
 def filter_closure_data_by_experiment(
-    filter_path, experiments_data, t0pdfset, fakenoise, errorsize, prepare_nnpdf_rng,
+    filter_path, experiments_data, fakepdf, fakenoise, errorsize, prepare_nnpdf_rng,
 ):
     """
     Like :py:func:`filter_closure_data` except filters data by experiment.
@@ -120,7 +120,7 @@ def filter_closure_data_by_experiment(
 
     """
     return [
-        _filter_closure_data(filter_path, exp, t0pdfset, fakenoise, errorsize)
+        _filter_closure_data(filter_path, exp, fakepdf, fakenoise, errorsize)
         for exp in experiments_data
     ]
 
@@ -168,11 +168,11 @@ def _filter_real_data(filter_path, data):
     return total_data_points, total_cut_data_points
 
 
-def _filter_closure_data(filter_path, data, fakepdfset, fakenoise, errorsize):
+def _filter_closure_data(filter_path, data, fakepdf, fakenoise, errorsize):
     """Filter closure test data."""
     total_data_points = 0
     total_cut_data_points = 0
-    fakeset = fakepdfset.legacy_load()
+    fakeset = fakepdf.legacy_load()
     # Load data, don't cache result
     loaded_data = data.load.__wrapped__(data)
     # generate level 1 shift if fakenoise

commit cee347951e01b8723c4f68941b946f133a54273c

fixed bug following from introduction of parse_fakepdf in config.py (#1654)
* fixed bug affecting, for instance, vp-comparefits introduced from commit e909c15842f01bf95444ba70cdb64b974d9e7648

* avoid parsing fakepdf again

* Update validphys2/src/validphys/config.py

* Update validphys2/src/validphys/config.py

* Update validphys2/src/validphys/config.py, fakepdf is a validphys.core.PDF object and already parsed

* Update validphys2/src/validphys/config.py

Co-authored-by: Mark Nestor Costantini <markcostantini@MacBook-Pro-35.local>
Co-authored-by: Mark Nestor Costantini <markcostantini@macbook-pro.home>
Co-authored-by: Zaharid <zk261@cam.ac.uk>
Co-authored-by: Mark Nestor Costantini <mnc33@cam.ac.uk>

validphys2/src/validphys/config.py
@@ -322,7 +322,7 @@ class CoreConfig(configparser.Config):
         """
         with self.set_context(ns=self._curr_ns.new_child({"fit": fit})):
             _, datacuts = self.parse_from_("fit", "closuretest", write=False)
-        underlyinglaw = self.parse_pdf(datacuts["fakepdf"])
+        underlyinglaw = datacuts["fakepdf"]
         return {"pdf": underlyinglaw}
 
     @element_of("hyperscans")