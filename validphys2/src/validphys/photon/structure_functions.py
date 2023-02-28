05392d1a3d204bd9d0a6f8bd4fc7d83cd82ec959
Niccolò Laurenti <niclaurenti@gmail.com>
2023-02-28 12:00:52 +0100

    Restore RectBivariateSpline and remove interp2d

--- validphys2/src/validphys/photon/structure_functions.py
+++ validphys2/src/validphys/photon/structure_functions.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import pineappl
 import numpy as np
-from scipy.interpolate import interp2d
+from scipy.interpolate import RectBivariateSpline
 
 class StructureFunction :
     def __init__(self, path_to_fktable, pdfs):
@@ -15,15 +15,22 @@ class StructureFunction :
     def produce_interpolator(self):
         x = np.unique(self.fktable.bin_left(1))
         q2 = np.unique(self.fktable.bin_left(0))
+        self.xmin = min(x)
+        self.xmax = max(x)
+        self.qmin = min(np.sqrt(q2))
+        self.qmax = max(np.sqrt(q2))
+
         predictions = self.fktable.convolute_with_one(self.pdgid, self.pdfs.xfxQ2)
         # here we require that the (x,Q2) couples that we passed
         # to pinefarm is a rectangular matrix
 
-        grid2D = predictions.reshape(len(x),len(q2)).T
-        self.interpolator = interp2d(x, q2, grid2D, kind='cubic', fill_value=0)
+        grid2D = predictions.reshape(len(x), len(q2))
+        self.interpolator = RectBivariateSpline(x, q2, grid2D)
     
     def FxQ(self, x, Q):
-        return self.interpolator(x, Q**2)[0]
+        if x < self.xmin or x > self.xmax or Q < self.qmin or Q > self.qmax :
+            return 0.
+        return self.interpolator(x, Q**2)[0, 0]
 
 class F2LO :
     def __init__(self, pdfs, theory):
