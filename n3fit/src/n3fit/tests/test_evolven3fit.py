43efd53ee3c41679ae0f3443345b835853244cb2
andreab1997 <andreab1997@yahoo.it>
2023-03-20 18:04:38 +0100

    Remove last inconsistent solve

--- n3fit/src/n3fit/tests/test_evolven3fit.py
+++ n3fit/src/n3fit/tests/test_evolven3fit.py
@@ -76,7 +76,7 @@ def test_eko_utils(tmp_path):
     assert_allclose(op_card_dict["_mugrid"][1], 4.92)
     # Testing computation of eko
     save_path = tmp_path / "ekotest.tar"
-    _ = runner.solve(t_card, op_card, save_path)
+    runner.solve(t_card, op_card, save_path)
     eko_op = EKO.read(save_path)
     assert_allclose(eko_op.operator_card.raw["rotations"]["xgrid"], x_grid)
     assert_allclose(list(eko_op.operator_card.raw["_mugrid"]), op_card_dict["_mugrid"])
