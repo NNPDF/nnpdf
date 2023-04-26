ccb20d939e6ab69ad604b1c8f08981c2c50dccaf
Aron <aronpjansen@gmail.com>
2023-04-26 11:35:22 +0200

    Fix and expand test

--- n3fit/src/n3fit/tests/test_hyperopt.py
+++ n3fit/src/n3fit/tests/test_hyperopt.py
@@ -2,17 +2,21 @@
     Test hyperoptimization features
 """
 
+import numpy as np
 from numpy.testing import assert_approx_equal
 from n3fit.hyper_optimization.rewards import HyperLoss
 
 def test_rewards():
     """ Ensure that rewards continue doing what they are supposed to do """
-    losses = [0.0, 1.0, 2.0]
-    loss_average = HyperLoss("average")
-    assert_approx_equal(loss_average.compute(losses), 1.0)
+    losses = np.array([[0.0, 2.0], [1.0, 3.0], [2.0, 4.0]])
+    loss_average = HyperLoss(replica_statistic="average", fold_statistic="average")
+    assert_approx_equal(loss_average.compute(losses), 2.0)
 
-    loss_best_worst = HyperLoss("best_worst")
-    assert_approx_equal(loss_best_worst.compute(losses), 2.0)
+    loss_best_worst = HyperLoss(replica_statistic="average", fold_statistic="best_worst")
+    assert_approx_equal(loss_best_worst.compute(losses), 3.0)
 
-    loss_std = HyperLoss("std")
+    loss_std = HyperLoss(replica_statistic="average", fold_statistic="std")
     assert_approx_equal(loss_std.compute(losses), 0.816496580927726)
+
+    loss_best_worst_best_worst = HyperLoss(replica_statistic="best_worst", fold_statistic="best_worst")
+    assert_approx_equal(loss_best_worst_best_worst.compute(losses), 4.0)
