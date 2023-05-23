ebfd3c382214f32103a390430cd2479e218a8847
andreab1997 <andreab1997@yahoo.it>
2023-05-23 13:05:13 +0200

    Restore max_M and min_M

--- validphys2/src/validphys/config.py
+++ validphys2/src/validphys/config.py
@@ -1422,14 +1422,12 @@ class CoreConfig(configparser.Config):
         q2min=None,
         w2min=None,
         maxTau=None,
-        max_M=None,
-        min_M=None,
         default_filter_settings=None,
         filter_defaults={},
         default_filter_settings_recorded_spec_=None,
     ):
         """Produce default values for filters taking into account the
-        values of ``q2min``, ` `w2min``, ``maxTau``, ``max_M`` and ``min_M`` defined at namespace
+        values of ``q2min``, ` `w2min`` and ``maxTau`` defined at namespace
         level and those inside a ``filter_defaults`` mapping.
         """
         from validphys.filters import default_filter_settings_input
@@ -1452,20 +1450,6 @@ class CoreConfig(configparser.Config):
             and maxTau != filter_defaults["maxTau"]
         ):
             raise ConfigError("maxTau defined multiple times with different values")
-        
-        if (
-            max_M is not None
-            and "max_M" in filter_defaults
-            and max_M != filter_defaults["max_M"]
-        ):
-            raise ConfigError("max_M defined multiple times with different values")
-        
-        if (
-            min_M is not None
-            and "min_M" in filter_defaults
-            and min_M != filter_defaults["min_M"]
-        ):
-            raise ConfigError("min_M defined multiple times with different values")
 
         if default_filter_settings_recorded_spec_ is not None:
             filter_defaults = default_filter_settings_recorded_spec_[
@@ -1491,14 +1475,6 @@ class CoreConfig(configparser.Config):
         if maxTau is not None and defaults_loaded:
             log.warning("Using maxTau from runcard")
             filter_defaults["maxTau"] = maxTau
-
-        if max_M is not None and defaults_loaded:
-            log.warning("Using max_M from runcard")
-            filter_defaults["max_M"] = max_M
-            
-        if min_M is not None and defaults_loaded:
-            log.warning("Using min_M from runcard")
-            filter_defaults["min_M"] = min_M
             
         return filter_defaults
 
