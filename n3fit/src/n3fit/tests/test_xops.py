577d5d8bf50b60c3d23e95cacb7aca59900722d6
Aron <aronpjansen@gmail.com>
2023-05-25 13:50:52 +0200

    Add test for xDivide

---
+++ n3fit/src/n3fit/tests/test_xops.py
@@ -0,0 +1,33 @@
+"""
+    Test the x operations
+"""
+import numpy as np
+from n3fit.layers import xDivide
+
+
+def test_xdivide_default():
+    """Check that the default xDivide works as expected"""
+    x_div = xDivide()
+    test_input = np.array([1, 2, 3], dtype=np.float32).reshape((1, 3, 1))
+    test_output = x_div(test_input)
+
+    expected_output = np.ones(shape=(1, 3, 14))
+    default_indices = [3, 4, 5, 6]
+    for i in default_indices:
+        expected_output[:, :, i] = 1 / test_input[:, :, 0]
+
+    assert np.allclose(test_output, expected_output)
+
+def test_xdivide_indices():
+    """Check that the default xDivide works as expected"""
+    custom_indices = [0, 1, 7]
+    x_div = xDivide(div_list=custom_indices)
+    test_input = np.array([1, 2, 3], dtype=np.float32).reshape((1, 3, 1))
+    test_output = x_div(test_input)
+
+    expected_output = np.ones(shape=(1, 3, 14))
+    for i in custom_indices:
+        expected_output[:, :, i] = 1 / test_input[:, :, 0]
+
+    assert np.allclose(test_output, expected_output)
+
