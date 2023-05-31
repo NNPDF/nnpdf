1064d359a8df29805e672d0a0a0fd1b4a9c765e3
Giovanni De Crescenzo <giovannidecrescenzo@Air-di-Giovanni.lan>
2023-05-31 17:26:34 +0200

    modified labels

--- validphys2/src/validphys/closuretest/inconsistent_plots.py
+++ validphys2/src/validphys/closuretest/inconsistent_plots.py
@@ -19,32 +19,35 @@ def covmat_trace_ratio(data, inconsistent_datasets, ADD, MULT, CORR, UNCORR, SPE
     type2. 
 
     A type1 reference fit is defined by: 
-    L1_data = L0 + N(0,C_exp)
-    L2_data = L1 + N(0,C_exp)
+    L1_data = L0 + N(0,C+)
+    L2_data = L1 + N(0,C+)
 
     A type2 reference fit is defined by:
 
-    L1_data = L0 + N(0,C+)
-    L2_data = L1 + N(0,C+)
+    L1_data = L0 + N(0,C_exp)
+    L2_data = L1 + N(0,C_exp)
 
     where C+ is (roughly) defined as C_exp*sys_rescaling_factor (usually larger than 1)
 
     in order to see the impact of the inconsistency then:
 
     Type1 inconsistent closure test:
-    L1_data = L0 + N(0,C_exp)
-    L2_data = L1 + N(0,C-)
-
-    Type1 trace percentage diff: 
-
-    tr(C-)/(tr(C_exp))*100
-
-    Type2 inconsistent closure test:
     L1_data = L0 + N(0,C+)
     L2_data = L1 + N(0,C_exp)
 
-    Type2 percentage diff:
+    Type1 trace percentage diff: 
+
     tr(C_exp)/(tr(C+))*100
+
+    Type2 inconsistent closure test:
+    L1_data = L0 + N(0,C_exp)
+    L2_data = L1 + N(0,C-)
+
+    where C- is (roughly) defined as C_exp*sys_rescaling_factor (usually smaller than 1)
+
+
+    Type2 percentage diff:
+    tr(C-)/(tr(C_exp))*100
 
 
     """
@@ -82,21 +85,21 @@ def covmat_trace_ratio(data, inconsistent_datasets, ADD, MULT, CORR, UNCORR, SPE
         )
     modified_trace = np.trace(modified_covmat)
     if type1:   
-        percentage_diff = abs(modified_trace)/(trace)*100
+        percentage_ratio = (trace)/(modified_trace)*100
     if type2:
-        percentage_diff = abs(trace)/(modified_trace)*100
-    return percentage_diff
+        percentage_ratio = modified_trace/trace*100
+    return percentage_ratio
 
 @figure
 def plot_trace_impact(data, inconsistent_datasets, ADD,MULT,CORR,UNCORR,SPECIAL):
-    rs_factors_1 = np.arange(0.1,1.1,0.1)
-    rs_factors_2 = np.arange(1,5,0.1)
+    rs_factors_2 = np.arange(0.1,1.1,0.1)
+    rs_factors_1 = np.arange(1,5,0.1)
     impacts_1 = []
     impacts_2 = []
-    for fac in rs_factors_1:
-        impacts_1.append(covmat_trace_ratio(data, inconsistent_datasets, ADD,MULT,CORR,UNCORR,SPECIAL, fac,type1 = True,type2 = False))
     for fac in rs_factors_2:
         impacts_2.append(covmat_trace_ratio(data, inconsistent_datasets, ADD,MULT,CORR,UNCORR,SPECIAL, fac,type1 = False,type2 = True))
+    for fac in rs_factors_1:
+        impacts_1.append(covmat_trace_ratio(data, inconsistent_datasets, ADD,MULT,CORR,UNCORR,SPECIAL, fac,type1 = True,type2 = False))
     fig, ax = plotutils.subplots()
 
     ax.plot(rs_factors_1,impacts_1, label = "type1 Inconsistent Closure")
@@ -112,5 +115,5 @@ def plot_trace_impact(data, inconsistent_datasets, ADD,MULT,CORR,UNCORR,SPECIAL)
     ax.legend()
     ax.set_title(title)
     ax.set_xlabel("rescaling factor")
-    ax.set_ylabel("percentage diff")
+    ax.set_ylabel("percentage ratio")
     return fig
