4c19a789ffb186535eda4c66cb816b78bb428c53
Niccolò Laurenti <niclaurenti@gmail.com>
2023-06-07 13:24:18 +0200

    Add control on alphaem running in eko_utils

--- n3fit/src/evolven3fit_new/eko_utils.py
+++ n3fit/src/evolven3fit_new/eko_utils.py
@@ -62,6 +62,10 @@ def construct_eko_cards(
     # The Legacy function is able to construct a theory card for eko starting from an NNPDF theory
     legacy_class = runcards.Legacy(theory, {})
     theory_card = legacy_class.new_theory
+    # if Qedref = Qref alphaem is running, otherwise it's fixed
+    if theory["QED"] > 0:
+        if np.isclose(theory["Qref"], theory["Qedref"]):
+            theory_card.couplings.em_running = True
     # construct operator card
     q2_grid = utils.generate_q2grid(
         theory["Q0"],
