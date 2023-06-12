f4db88707dcbf96de31e4e5075e748ff87c41e4a
Mark Nestor Costantini <mnc33@cam.ac.uk>
2023-06-12 17:11:36 +0100

    using collect!

--- validphys2/src/validphys/closuretest/inconsistent_closuretest/multiclosure_inconsistent_output.py
+++ validphys2/src/validphys/closuretest/inconsistent_closuretest/multiclosure_inconsistent_output.py
@@ -12,33 +12,46 @@ from validphys import plotutils
 
 
 @figure
-def plot_replica_central_diff_consistent_vs_inconsistent(
-    consistent_dataset_replica_minus_central,
-    inconsistent_dataset_replica_minus_central,
-    dataset
-):
-    """ 
-    For a given dataset plot the distributions of the replica theory predictions around
-    the central replica prediction.
-    Note:
-        - Comparison is done between consistent and inconsistent fits
-        - average is done over fits and datapoints of one dataset
-
+def plot_replica_central_diff_multi(dataset_replica_minus_central_multi, dataspecs):
     """
-    diff_consistent = consistent_dataset_replica_minus_central
-    diff_inconsistent = inconsistent_dataset_replica_minus_central
-
+    histogram of the distribution of the differences between central replica
+    and replicas.
+    Datapoints are averaged over.
+    """
     fig, ax = plotutils.subplots()
-    ax.hist(
-        diff_consistent, density=True, alpha=0.5, label=f"Consistent, Central-Replica Distribution"
-    )
-    ax.hist(
-        diff_inconsistent,
-        density=True,
-        alpha=0.5,
-        label=f"Inconsistent, Central-Replica Distribution",
-    )
-    ax.set_title(f"Dataset: {dataset}")
+
+    for ds_replica_minus_central, spec in zip(dataset_replica_minus_central_multi, dataspecs):
+        diff, _, _ = ds_replica_minus_central
+
+        label = spec['speclabel']
+
+        ax.hist(
+            diff,
+            bins='auto',
+            density=True,
+            alpha=0.5,
+            label=f"{label}",
+        )
+
     ax.legend()
 
+    return fig
+
+
+@figure
+def plot_variance_distribution_multi(dataset_variance_per_replica_multi, dataspecs):
+    """
+    histogram of the distribution of the variances (k) defined as the
+    distance between central replica and replica (k) in units of the
+    experimental covariance matrix.
+    If more than one group of dataspecs (e.g. consistent and inconsistent)
+    fits are defined, than plot comparison of these.
+    """
+    fig, ax = plotutils.subplots()
+    for variance_dist, spec in zip(dataset_variance_per_replica_multi, dataspecs):
+        label = spec['speclabel']
+
+        ax.hist(variance_dist, bins='auto', density=True, alpha=0.5, label=label)
+
+    ax.legend()
     return fig
