57a5343bdbd7dfe63b46f9151fddd5fc7dae2cd5
Niccolò Laurenti <niclaurenti@gmail.com>
2023-06-16 10:36:23 +0200

    Use test theory in test_compute

--- validphys2/src/validphys/tests/photon/test_compute.py
+++ validphys2/src/validphys/tests/photon/test_compute.py
@@ -1,141 +1,48 @@
 from collections import namedtuple
 from pathlib import Path
 
-import fiatlux
 import numpy as np
 from validphys.photon import structure_functions
 from validphys.photon.compute import Photon, Alpha
 from validphys.core import PDF as PDFset
+from validphys.api import API
 
 from ..conftest import PDF
 from eko.io import EKO
 
+TEST_THEORY = API.theoryid(theoryid=398)
 
-class FakeTheory:
-    def __init__(self):
-        self.path = Path("/fake/path/")
-
-    def get_description(self):
-        return {
-            "alphaqed": 0.01,
-            "Qref": 91.2,
-            "Qedref": 91.2,
-            "mc": 1.3,
-            "mb": 4.92,
-            "mt": 172.0,
-            "kcThr": 1.0,
-            "kbThr": 1.0,
-            "ktThr": 1.0,
-            "MaxNfAs": 5,
-            "MaxNfPdf": 5,
-            "MP": 0.938
-        }
-
-
-fiatlux_runcard = {
+FIATLUX_RUNCARD = {
     "luxset": PDFset(PDF),
-    "additional_errors": False,
-    "luxseed": 123456789
+    "additional_errors": PDFset("LUXqed17_plus_PDF4LHC15_nnlo_100"),
+    "luxseed": 123456789,
+    "eps_base": 1e-2,
 }
 
-photon = namedtuple("photon", ["total", "elastic", "inelastic"])
+def test_parameters_init():
 
-
-class FakeFiatlux:
-    def __init__(self, runcard):
-        self.runcard = runcard
-        self.alphaem = None
-        self.qref = None
-        self.trash1 = None
-        self.trash2 = None
-        self.f2 = None
-        self.fl = None
-        self.f2lo = None
-        self.res = photon(0., 0., 0.)
-
-    def PlugAlphaQED(self, alphaem, qref):
-        self.alphaem = alphaem
-        self.qref = qref
-
-    def InsertInelasticSplitQ(self, args):
-        self.trash1 = args[0]
-        self.trash2 = args[1]
-
-    def PlugStructureFunctions(self, f2, fl, f2lo):
-        self.f2 = f2
-        self.fl = fl
-        self.f2lo = f2lo
-
-    def EvaluatePhoton(self, x, q):
-        return self.res
-
-class FakeEKO:
-    def __init__(self, path):
-        self.path = path
-        self.mu20 = 100**2
-    
-    def __enter__(self):
-        return self
-    
-    def __exit__(self, exc_type: type, _exc_value, _traceback):
-        pass
-
-
-class FakeStructureFunction:
-    def __init__(self, path, pdfs):
-        self.path = path
-        self.pdfs = pdfs
-        self.q2_max = 1e8
-
-    def fxq(self):
-        return 0
-
-
-class FakeF2LO:
-    def __init__(self, pdfs, theory):
-        self.pdfs = pdfs
-        self.theory = theory
-
-    def fxq(self):
-        return 0
-
-
-def test_parameters_init(monkeypatch):
-    monkeypatch.setattr(
-        structure_functions, "InterpStructureFunction", FakeStructureFunction
-    )
-    monkeypatch.setattr(structure_functions, "F2LO", FakeF2LO)
-    monkeypatch.setattr(fiatlux, "FiatLux", FakeFiatlux)
-    monkeypatch.setattr(Photon, "compute_photon_array", lambda *args: np.zeros(196))
-    monkeypatch.setattr(EKO, "read", FakeEKO)
-
-    photon = Photon(FakeTheory(), fiatlux_runcard, [1, 2, 3])
-    alpha = Alpha(FakeTheory().get_description())
+    photon = Photon(TEST_THEORY, FIATLUX_RUNCARD, [1, 2, 3])
+    alpha = Alpha(TEST_THEORY.get_description())
 
     np.testing.assert_equal(photon.replicas, [1, 2, 3])
-    np.testing.assert_equal(photon.luxpdfset._name, fiatlux_runcard["luxset"].name)
-    np.testing.assert_equal(photon.additional_errors, fiatlux_runcard["additional_errors"])
-    np.testing.assert_equal(photon.luxseed, fiatlux_runcard["luxseed"])
+    np.testing.assert_equal(photon.luxpdfset._name, FIATLUX_RUNCARD["luxset"].name)
+    np.testing.assert_equal(photon.additional_errors._name, "LUXqed17_plus_PDF4LHC15_nnlo_100")
+    np.testing.assert_equal(photon.luxseed, FIATLUX_RUNCARD["luxseed"])
+    np.testing.assert_equal(photon.path_to_eko_photon, TEST_THEORY.path / "eko_photon.tar")
+    np.testing.assert_equal(photon.q_in, 100.)
     np.testing.assert_almost_equal(
-        alpha.alpha_em_ref, FakeTheory().get_description()["alphaqed"]
+        alpha.alpha_em_ref, TEST_THEORY.get_description()["alphaqed"]
     )
 
 def test_masses_init():
-    alpha = Alpha(FakeTheory().get_description())
+    alpha = Alpha(TEST_THEORY.get_description())
     np.testing.assert_equal(alpha.thresh_t, np.inf)
     np.testing.assert_almost_equal(alpha.thresh_b, 4.92)
-    np.testing.assert_almost_equal(alpha.thresh_c, 1.3)
+    np.testing.assert_almost_equal(alpha.thresh_c, 1.51)
 
-def test_set_thresholds_alpha_em(monkeypatch):
-    monkeypatch.setattr(
-        structure_functions, "InterpStructureFunction", FakeStructureFunction
-    )
-    monkeypatch.setattr(structure_functions, "F2LO", FakeF2LO)
+def test_set_thresholds_alpha_em():
 
-    monkeypatch.setattr(fiatlux, "FiatLux", FakeFiatlux)
-    monkeypatch.setattr(Photon, "compute_photon_array", lambda *args: np.zeros(196))
-
-    alpha = Alpha(FakeTheory().get_description())
+    alpha = Alpha(TEST_THEORY.get_description())
 
     np.testing.assert_almost_equal(alpha.thresh[5], 91.2)
     np.testing.assert_almost_equal(alpha.thresh[4], 4.92)
@@ -152,7 +59,7 @@ def test_set_thresholds_alpha_em(monkeypatch):
     np.testing.assert_equal(len(alpha.thresh), 3)
 
 def test_betas():
-    alpha = Alpha(FakeTheory().get_description())
+    alpha = Alpha(TEST_THEORY.get_description())
     vec_beta0 = [
         -0.5305164769729844,
         -0.6719875374991137,
