b4850d451374840597bc0c5e7f232e79efb4f04a
juacrumar <juacrumar@gmail.com>
2023-07-28 11:30:31 +0200

    update FPF data; add a temporary theory 397 with the FPF fktables

--- nnpdfcpp/data/add_db.py
+++ nnpdfcpp/data/add_db.py
@@ -6,8 +6,8 @@ c = db.cursor()
 res = c.execute("SELECT * FROM TheoryIndex where ID=400").fetchone()
 # Now change the ID
 new_res = list(res)
-new_res[0] = 399
-new_res[36] = "Copy of ID=400 with low precision grids, cfactors from Th 200"
+new_res[0] = 397
+new_res[36] = "Copy of Theory 400 (28/07/2023) with FPF fktables"
 
 # And insert it
 qmarks = ",".join(["?"]*len(new_res))
