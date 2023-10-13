1c7149e84163502c65b043ad839ba9f1cc90f838
Toon Hasenack <toon.hasenack@outlook.com>
2023-10-13 10:26:05 +0200

    E155 filter.py formatted with Black.

--- buildmaster/E155_NC_9GEV_EP_G1F1RATIO/filter.py
+++ buildmaster/E155_NC_9GEV_EP_G1F1RATIO/filter.py
@@ -7,31 +7,39 @@ import glob
 def read_data(fnames):
     df = pd.DataFrame()
     for fname in fnames:
-        with open(fname, 'r') as file:
+        with open(fname, "r") as file:
             data = yaml.safe_load(file)
 
         x = float(data["dependent_variables"][1]["qualifiers"][4]["value"])
         y = 0.0
-
         Qsub = data["independent_variables"][0]["values"]
         Gsub = data["dependent_variables"][0]["values"]
 
         for i in range(len(Qsub)):
-            df = pd.concat([df, pd.DataFrame({"x": x,
-                                              "y": y,
-                                              "Q2": [Qsub[i]["value"]],
-                                              "G": [Gsub[i]["value"]],
-                                              "stat": [Gsub[i]["errors"][0]["symerror"]],
-                                              "sys": [Gsub[i]["errors"][1]["symerror"]]})],
-                           ignore_index=True)
+            df = pd.concat(
+                [
+                    df,
+                    pd.DataFrame(
+                        {
+                            "x": x,
+                            "y": y,
+                            "Q2": [Qsub[i]["value"]],
+                            "G": [Gsub[i]["value"]],
+                            "stat": [Gsub[i]["errors"][0]["symerror"]],
+                            "sys": [Gsub[i]["errors"][1]["symerror"]],
+                        }
+                    ),
+                ],
+                ignore_index=True,
+            )
 
     return df
 
 
 def write_data(df):
     data_central = []
-    for i in range(len(df['G'])):
-        data_central.append(float(df.loc[i, 'G']))
+    for i in range(len(df["G"])):
+        data_central.append(float(df.loc[i, "G"]))
 
     data_central_yaml = {"data_central": data_central}
     with open("data.yaml", "w") as file:
@@ -39,11 +47,11 @@ def write_data(df):
 
     # Write kin file
     kin = []
-    for i in range(len(df['G'])):
+    for i in range(len(df["G"])):
         kin_value = {
-            "x":  {"min": None, "mid": float(df.loc[i, 'x']), "max": None},
-            "Q2": {"min": None, "mid": float(df.loc[i, 'Q2']), "max": None},
-            "y":  {"min": None, "mid": float(df.loc[i, 'y']), "max": None},
+            "x": {"min": None, "mid": float(df.loc[i, "x"]), "max": None},
+            "Q2": {"min": None, "mid": float(df.loc[i, "Q2"]), "max": None},
+            "y": {"min": None, "mid": float(df.loc[i, "y"]), "max": None},
         }
         kin.append(kin_value)
 
@@ -56,18 +64,22 @@ def write_data(df):
     error = []
     for i in range(len(df)):
         e = {
-            "stat": float(df.loc[i, 'stat']),
-            "sys": float(df.loc[i, 'sys']),
+            "stat": float(df.loc[i, "stat"]),
+            "sys": float(df.loc[i, "sys"]),
         }
         error.append(e)
 
     error_definition = {
-        "stat": {"description": "statistical uncertainty",
-                 "treatment": "ADD",
-                 "type": "UNCORR"},
-        "sys": {"description": "systematic uncertainty",
-                "treatment": "ADD",
-                "type": "UNCORR"}
+        "stat": {
+            "description": "statistical uncertainty",
+            "treatment": "ADD",
+            "type": "UNCORR",
+        },
+        "sys": {
+            "description": "systematic uncertainty",
+            "treatment": "ADD",
+            "type": "UNCORR",
+        },
     }
 
     uncertainties_yaml = {"definitions": error_definition, "bins": error}
