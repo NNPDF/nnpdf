0df151acf97da63ebab8fd263e66d73ac806a50c
Toon Hasenack <Toon.Hasenack@outlook.com>
2023-10-16 15:21:52 +0200

    Initial commit of pDIS data for Electron-Neutron scattering in the E155 experiment.

---
+++ buildmaster/E155_NC_9GEV_EN_G1F1RATIO/filter.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import yaml
+import numpy as np
+import glob
+
+
+def read_data(fnames):
+    df = pd.DataFrame()
+    for fname in fnames:
+        with open(fname, "r") as file:
+            data = yaml.safe_load(file)
+
+        x = float(data["dependent_variables"][1]["qualifiers"][4]["value"])
+        y = 0.0
+        Qsub = data["independent_variables"][0]["values"]
+        Gsub = data["dependent_variables"][1]["values"]
+
+        for i in range(len(Qsub)):
+            df = pd.concat(
+                [
+                    df,
+                    pd.DataFrame(
+                        {
+                            "x": x,
+                            "y": y,
+                            "Q2": [Qsub[i]["value"]],
+                            "G": [Gsub[i]["value"]],
+                            "stat": [Gsub[i]["errors"][0]["symerror"]],
+                            "sys": [Gsub[i]["errors"][1]["symerror"]],
+                        }
+                    ),
+                ],
+                ignore_index=True,
+            )
+
+    return df
+
+
+def write_data(df):
+    data_central = []
+    for i in range(len(df["G"])):
+        data_central.append(float(df.loc[i, "G"]))
+
+    data_central_yaml = {"data_central": data_central}
+    with open("data.yaml", "w") as file:
+        yaml.dump(data_central_yaml, file, sort_keys=False)
+
+    # Write kin file
+    kin = []
+    for i in range(len(df["G"])):
+        kin_value = {
+            "x": {"min": None, "mid": float(df.loc[i, "x"]), "max": None},
+            "Q2": {"min": None, "mid": float(df.loc[i, "Q2"]), "max": None},
+            "y": {"min": None, "mid": float(df.loc[i, "y"]), "max": None},
+        }
+        kin.append(kin_value)
+
+    kinematics_yaml = {"bins": kin}
+
+    with open("kinematics.yaml", "w") as file:
+        yaml.dump(kinematics_yaml, file, sort_keys=False)
+
+    # Write unc file
+    error = []
+    for i in range(len(df)):
+        e = {
+            "stat": float(df.loc[i, "stat"]),
+            "sys": float(df.loc[i, "sys"]),
+        }
+        error.append(e)
+
+    error_definition = {
+        "stat": {
+            "description": "statistical uncertainty",
+            "treatment": "ADD",
+            "type": "UNCORR",
+        },
+        "sys": {
+            "description": "systematic uncertainty",
+            "treatment": "ADD",
+            "type": "UNCORR",
+        },
+    }
+
+    uncertainties_yaml = {"definitions": error_definition, "bins": error}
+
+    with open("uncertainties.yaml", "w") as file:
+        yaml.dump(uncertainties_yaml, file, sort_keys=False)
+
+
+if __name__ == "__main__":
+    fnames = glob.glob("rawdata/*.yaml")
+    df = read_data(fnames)
+    write_data(df)
