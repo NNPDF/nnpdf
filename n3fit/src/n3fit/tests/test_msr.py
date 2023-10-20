e69926d48991d85c0924c0df55f897f5a4f9a794
Aron <aronpjansen@gmail.com>
2023-10-20 08:59:35 +0200

    Fix msr test, adding missing replica dimensions

--- n3fit/src/n3fit/tests/test_msr.py
+++ n3fit/src/n3fit/tests/test_msr.py
@@ -6,9 +6,11 @@ from n3fit.layers import MSR_Normalization
 
 def apply_layer_to_fixed_input(layer):
     np.random.seed(422)
-    pdf_integrated = op.numpy_to_tensor(np.random.normal(size=(1, 14)))
+    pdf_integrated = np.expand_dims(np.random.normal(size=(1, 14)), -1)
+    pdf_integrated = op.numpy_to_tensor(pdf_integrated)
 
-    photon_integral = op.numpy_to_tensor([np.random.normal(size=(1,))])
+    photon_integral = np.expand_dims(np.random.normal(size=(1,)), -1)
+    photon_integral = op.numpy_to_tensor(photon_integral)
 
     return layer(pdf_integrated, photon_integral)
 
@@ -17,22 +19,25 @@ def test_all():
     layer = MSR_Normalization(mode='ALL')
     output = apply_layer_to_fixed_input(layer)
     known_output = op.numpy_to_tensor(
-        [
-            1.0,
-            1.0,
-            6.7740397,
-            -3.8735497,
-            15.276561,
-            5.9522753,
-            8.247783,
-            -3.8735497,
-            -3.8735497,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-        ]
+        np.expand_dims(
+            [
+                1.0,
+                1.0,
+                6.7740397,
+                -3.8735497,
+                15.276561,
+                5.9522753,
+                8.247783,
+                -3.8735497,
+                -3.8735497,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+            ],
+            -1,
+        )
     )
     np.testing.assert_allclose(output, known_output, rtol=1e-5)
 
@@ -41,22 +46,25 @@ def test_msr():
     layer = MSR_Normalization(mode='MSR')
     output = apply_layer_to_fixed_input(layer)
     known_output = op.numpy_to_tensor(
-        [
-            1.0,
-            1.0,
-            6.7740397,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-        ]
+        np.expand_dims(
+            [
+                1.0,
+                1.0,
+                6.7740397,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+            ],
+            -1,
+        )
     )
     np.testing.assert_allclose(output, known_output, rtol=1e-5)
 
@@ -65,21 +73,24 @@ def test_vsr():
     layer = MSR_Normalization(mode='VSR')
     output = apply_layer_to_fixed_input(layer)
     known_output = op.numpy_to_tensor(
-        [
-            1.0,
-            1.0,
-            1.0,
-            -3.8735497,
-            15.276561,
-            5.9522753,
-            8.247783,
-            -3.8735497,
-            -3.8735497,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-        ]
+        np.expand_dims(
+            [
+                1.0,
+                1.0,
+                1.0,
+                -3.8735497,
+                15.276561,
+                5.9522753,
+                8.247783,
+                -3.8735497,
+                -3.8735497,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+            ],
+            -1,
+        )
     )
     np.testing.assert_allclose(output, known_output, rtol=1e-5)
