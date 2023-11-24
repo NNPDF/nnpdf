b3b97cf4280e32a024c65a06e8877d5770fe1c08
juacrumar <juacrumar@lairen.eu>
2023-11-24 15:16:41 +0100

    dont use cmake to install python

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Install n3fit and validphys
+python -m pip install . --no-deps --ignore-installed
+
 mkdir build
 cd build
 echo "build_version=\"${PKG_VERSION}\"" > ../n3fit/src/n3fit/version.py
 echo "build_version=\"${PKG_VERSION}\"" > ../validphys2/src/validphys/version.py
 
-cmake .. -DCMAKE_INSTALL_PREFIX=${PREFIX} -DVP_DEV=OFF -DN3_DEV=OFF -DBURN_TAG=OFF
+cmake .. -DCMAKE_INSTALL_PREFIX=${PREFIX}
 make -j${CPU_COUNT}
 make install
