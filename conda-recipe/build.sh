c19f25c1a24f8ad5315521435b4b34109fcdc08c
juacrumar <juacrumar@lairen.eu>
2023-11-24 16:25:04 +0100

    dont ignore _version while installing

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # Install n3fit and validphys
-python -m pip install . --no-deps --ignore-installed
+python3 -m pip install . --no-deps --ignore-installed
 
 mkdir build
 cd build
-echo "build_version=\"${PKG_VERSION}\"" > ../n3fit/src/n3fit/version.py
-echo "build_version=\"${PKG_VERSION}\"" > ../validphys2/src/validphys/version.py
-
 cmake .. -DCMAKE_INSTALL_PREFIX=${PREFIX}
 make -j${CPU_COUNT}
 make install
