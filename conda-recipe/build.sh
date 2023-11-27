a80be5e79949a9ad3aaa3ed650970d66c9df0915
juacrumar <juacrumar@lairen.eu>
2023-11-27 08:41:37 +0100

    put back the python install within cmake

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Install n3fit and validphys
-python3 -m pip install . --no-deps --ignore-installed
-
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=${PREFIX}
+cmake .. -DCMAKE_INSTALL_PREFIX=${PREFIX} -DNNPDF_DEV=OFF
 make -j${CPU_COUNT}
 make install
