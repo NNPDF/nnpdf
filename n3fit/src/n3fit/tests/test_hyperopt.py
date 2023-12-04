a7696ead65535e1ca7c2f9b22f212b27043cb74e
Cmurilochem <carlosmurilorocha@gmail.com>
2023-12-04 11:30:45 +0100

    Fix in test_hyperopt

--- n3fit/src/n3fit/tests/test_hyperopt.py
+++ n3fit/src/n3fit/tests/test_hyperopt.py
@@ -4,19 +4,18 @@
 
 import numpy as np
 from numpy.testing import assert_approx_equal
+
 from n3fit.hyper_optimization.rewards import HyperLoss
 
+
 def test_rewards():
-    """ Ensure that rewards continue doing what they are supposed to do """
-    losses = np.array([[0.0, 2.0], [1.0, 3.0], [2.0, 4.0]])
-    loss_average = HyperLoss(replica_statistic="average", fold_statistic="average")
-    assert_approx_equal(loss_average.compute(losses), 2.0)
+    """Ensure that rewards continue doing what they are supposed to do"""
+    losses = np.array([1.0, 2.0, 3.0])
+    loss_average = HyperLoss(fold_statistic="average")
+    assert_approx_equal(loss_average.reduce_over_folds(losses), 2.0)
 
-    loss_best_worst = HyperLoss(replica_statistic="average", fold_statistic="best_worst")
-    assert_approx_equal(loss_best_worst.compute(losses), 3.0)
+    loss_std = HyperLoss(fold_statistic="std")
+    assert_approx_equal(loss_std.reduce_over_folds(losses), 0.816496580927726)
 
-    loss_std = HyperLoss(replica_statistic="average", fold_statistic="std")
-    assert_approx_equal(loss_std.compute(losses), 0.816496580927726)
-
-    loss_best_worst_best_worst = HyperLoss(replica_statistic="best_worst", fold_statistic="best_worst")
-    assert_approx_equal(loss_best_worst_best_worst.compute(losses), 4.0)
+    loss_best_worst_best_worst = HyperLoss(fold_statistic="best_worst")
+    assert_approx_equal(loss_best_worst_best_worst.reduce_over_folds(losses), 3.0)
