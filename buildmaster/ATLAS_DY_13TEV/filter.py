d3679dd47d08a2966d5efb6e420a44da10aee713
Radonirinaunimi <rrabeman@nikhef.nl>
2023-12-04 21:47:51 +0100

    Use updated version of the Fiducial ATLAS DY a 13 TeV

--- buildmaster/ATLAS_DY_13TEV/filter.py
+++ buildmaster/ATLAS_DY_13TEV/filter.py
@@ -1,5 +1,8 @@
 import pathlib
 import yaml
+import numpy as np
+
+from validphys.commondata_utils import covmat_to_artunc
 
 
 MZ_VALUE = 91.1876  # GeV
@@ -14,6 +17,7 @@ NB_POINTS = [1, 1, 1]  # [N_W-, N_W+, N_Z]
 # MAP Boson string to M values
 MAP_BOSON = {"Z": MZ_VALUE, "W": MW_VALUE}
 MAP_TABLE = {8: "W", 9: "W", 11: "W"}
+CORRMAT_NDIM = 3
 
 
 def load_yaml(table_id: int, version: int = 1) -> dict:
@@ -75,7 +79,7 @@ def get_kinematics(hepdata: dict, bin_index: list, boson: str = "Z") -> list:
     kinematics = []
     for _ in bin_index:
         kin_value = {
-            "_Zero": {"min": None, "mid": 0.0, "max": None},
+            "_zero": {"min": None, "mid": 0.0, "max": None},
             "mu2": {
                 "min": None,
                 "mid": NORM_FACTOR * MAP_BOSON[boson],
@@ -146,6 +150,58 @@ def get_errors(
     return {"stat": stat, "sys_corr": sys_corr, "sys_lumi": sys_lumi}
 
 
+def compute_correlation_ceoffs(table_id: int) -> np.ndarray:
+    """Extract the correlation coefficients from HepData and compute
+    the correlation matrix.
+
+    Parameters
+    ----------
+    table_id: int
+        table ID that contains the correlation matrix
+
+    Returns
+    -------
+    np.ndarray: (n_corr, n_corr)
+        a square symmetrized matrix containing the correlations
+
+    """
+    hepdata = load_yaml(table_id=table_id)
+    correlation = hepdata["dependent_variables"][0]["values"]
+
+    corrmatrix = np.identity(n=3)
+    corrmatrix[0][1] = correlation[-3]["value"]
+    corrmatrix[0][2] = correlation[-2]["value"]
+    corrmatrix[1][2] = correlation[-1]["value"]
+
+    # Symmetrize the correlation matrix
+    return corrmatrix.T + np.triu(corrmatrix, 1)
+
+
+def multiply_syst(covmat: np.ndarray, sys: list) -> np.ndarray:
+    """Multiply the covmat with the total systematics.
+
+    Parameters
+    ----------
+    covmat: np.ndarray
+        a squared matrix representing the covmat
+    sys: list
+        a vector containing the correlatdd systematics
+
+    Returns
+    -------
+    np.ndarray:
+        covariance matrix multiplied by the total systematics
+
+    """
+    covmat_sys = np.zeros(covmat.shape)
+
+    for i in range(covmat.shape[0]):
+        for j in range(covmat.shape[-1]):
+            covmat_sys[i][j] = covmat[i][j] * sys[i] * sys[j]
+
+    return covmat_sys.flatten()
+
+
 def concatenate_dicts(multidict: list[dict]) -> dict:
     """Given a list of dictionaries combined the values.
 
@@ -168,13 +224,27 @@ def concatenate_dicts(multidict: list[dict]) -> dict:
     return new_dict
 
 
-def format_uncertainties(uncs: dict) -> list:
+def generate_artificial_unc(**kwargs) -> np.ndarray:
+    """A wrapper around `covmat_to_artunc` to return an array.
+
+    Returns
+    -------
+    np.ndarray:
+        a squared matrix with artificial uncertainties
+    """
+    artunc = covmat_to_artunc(**kwargs)
+    return np.array(artunc)
+
+
+def format_uncertainties(uncs: dict, artunc: np.ndarray) -> list:
     """Format the uncertainties to be dumped into the yaml file.
 
     Parameters
     ----------
     uncs: dict
         dictionary containing the various source of uncertainties
+    artunc: no.ndarray
+        array of the artificial systematic uncertainties
 
     Returns
     -------
@@ -184,8 +254,14 @@ def format_uncertainties(uncs: dict) -> list:
     """
     combined_errors = []
 
-    for idat in range(len(uncs['stat'])):
-        error_value = {k: uncs[k][idat] for k in uncs.keys()}
+    artunc = artunc.tolist()
+    for idat in range(len(artunc)):
+        error_value = {}
+        for jdat in range(len(artunc[idat])):
+            error_value[f"sys_corr_{jdat + 1}"] = artunc[idat][jdat]
+
+        error_value["stat"] = uncs["stat"][idat]
+        error_value["sys_lumi"] = uncs["sys_lumi"][idat]
         combined_errors.append(error_value)
 
     return combined_errors
@@ -208,11 +284,12 @@ def dump_commondata(kinematics: list, data: list, errors: list) -> None:
     """
 
     error_definition = {
-        "sys_corr": {
+        f"sys_corr_{i + 1}": {
             "description": "Correlated systematic uncertainties",
             "treatment": "MULT",
             "type": "CORR",
         }
+        for i in range(CORRMAT_NDIM)
     }
 
     error_definition["stat"] = {
@@ -254,6 +331,7 @@ def main_filter() -> None:
 
     """
     version, _, _ = read_metadata()
+    nbpoints = sum(NB_POINTS)
 
     nbp_idx = 0
     comb_kins, comb_data = [], []
@@ -280,7 +358,13 @@ def main_filter() -> None:
             nbp_idx += 1
 
     errors_combined = concatenate_dicts(combined_errors)
-    errors = format_uncertainties(errors_combined)
+    # Compute the Artifical Systematics from Correlation Matrix
+    corrmat = compute_correlation_ceoffs(table_id=24)
+    covmat = multiply_syst(corrmat, errors_combined["sys_corr"])
+    artunc = generate_artificial_unc(
+        ndata=nbpoints, covmat_list=covmat.tolist(), no_of_norm_mat=0
+    )
+    errors = format_uncertainties(errors_combined, artunc)
 
     # Generate all the necessary files
     dump_commondata(comb_kins, comb_data, errors)
