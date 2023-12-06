38275435b4e8ff2bd104c55ce169a038d4e6c712
Cmurilochem <carlosmurilorocha@gmail.com>
2023-12-06 09:58:11 +0100

    New set_initial_state

--- n3fit/src/n3fit/tests/test_hyperopt.py
+++ n3fit/src/n3fit/tests/test_hyperopt.py
@@ -14,10 +14,11 @@ from n3fit.model_gen import pdfNN_layer_generator
 from validphys.api import API
 
 
-def set_initial_state(seed=42):
+def set_initial_state():
     """
     This function sets the initial internal state for the different components of n3fit.
     """
+    seed = 1
     os.environ.setdefault("HYPEROPT_FMIN_SEED", str(seed))
     np.random.seed(seed)
     use_seed = np.random.randint(0, pow(2, 31))
@@ -25,21 +26,25 @@ def set_initial_state(seed=42):
     tf.random.set_seed(use_seed)
 
 
-def generate_pdf(seed=42):
+def generate_pdf(seeds):
     """Generate generic pdf model."""
-    set_initial_state(seed=seed)
     fake_fl = [
         {"fl": i, "largex": [0, 1], "smallx": [1, 2]}
         for i in ["u", "ubar", "d", "dbar", "c", "g", "s", "sbar"]
     ]
+    set_initial_state()
     pdf_model = pdfNN_layer_generator(
-        nodes=[8], activations=["linear"], seed=seed, flav_info=fake_fl, fitbasis="FLAVOUR"
+        nodes=[8], activations=["linear"], seed=seeds, flav_info=fake_fl, fitbasis="FLAVOUR"
     )
+
+    for meta_model in pdf_model:
+        for var in meta_model.variables:
+            print(f"{var.name}: {var.numpy()}")
     return pdf_model
 
 
 def get_experimental_data(dataset_name="NMC", theoryid=400):
-    """Get experimental data set.
+    """Get experimental data set using validphys API.
 
     Returns a tuple defined by the data set as
     `validphys.core.DataSetSpec` and associated covariant matrix.
@@ -59,21 +64,26 @@ def get_experimental_data(dataset_name="NMC", theoryid=400):
         ("chi2", "average", 0.15),
         ("chi2", "best_worst", 0.2),
         ("chi2", "std", 0.05),
-        ("phi2", None, 477.44743668662574),
+        ("phi2", None, 6774.842533913706),
     ],
 )
 def test_compute_per_fold_loss(loss_type, replica_statistic, expected_per_fold_loss):
     """Check that the losses per fold are calculated correctly.
 
-    This example assumes a 2 replica calculation with 3 penalties added.
+    This example assumes a 2 replica calculation with 3 added penalties.
     """
-    pdf_models = generate_pdf(seed=[42, 52])
+    # generate 2 replica pdf model
+    pdf_models = generate_pdf(seeds=[42, 43])
+    # add 3 penalties for a 2 replica model
     penalties = [np.array([0.0, 0.0]), np.array([0.0, 0.0]), np.array([0.0, 0.0])]
+    # experimental losses for each replica
     experimental_loss = np.array([0.1, 0.2])
+    # get experimental data to compare with
     experimental_data = [get_experimental_data()]
 
     loss = HyperLoss(loss_type=loss_type, replica_statistic=replica_statistic)
 
+    # calculate statistic loss for one specific fold
     predicted_per_fold_loss = loss.compute_loss(
         penalties, experimental_loss, pdf_models, experimental_data
     )
@@ -83,6 +93,7 @@ def test_compute_per_fold_loss(loss_type, replica_statistic, expected_per_fold_l
 
 def test_loss_reduce_over_folds():
     """Ensure that the hyper loss statistics over all folds are calculated correctly."""
+    # define losses for 3 folds
     losses = np.array([1.0, 2.0, 3.0])
 
     loss_average = HyperLoss(fold_statistic="average")
