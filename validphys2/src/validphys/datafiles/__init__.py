7c1812bea40c9aa31033d8d5d28c9376533fa4f9
juacrumar <juacrumar@lairen.eu>
2023-12-11 14:13:28 +0100

    error out if path not found

---
+++ validphys2/src/validphys/datafiles/__init__.py
@@ -0,0 +1,5 @@
+import pathlib
+
+path_vpdata = pathlib.Path(__file__).parent
+path_commondata = pathlib.Path(__file__).with_name('commondata')
+path_theorydb = pathlib.Path(__file__).with_name('theory.db')
