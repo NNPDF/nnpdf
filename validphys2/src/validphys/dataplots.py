c944aa7a4bdf6d9956867dbf4f7d2df67d4cee03
RoyStegeman <roystegeman@live.nl>
2023-12-14 14:20:27 +0000

    add ht-based color xQ2 plot

--- validphys2/src/validphys/dataplots.py
+++ validphys2/src/validphys/dataplots.py
@@ -1140,7 +1140,7 @@ def _check_display_cuts_requires_use_cuts(display_cuts, use_cuts):
 
 @make_argcheck
 def _check_marker_by(marker_by):
-    markers = ('process type', 'experiment', 'dataset', 'group')
+    markers = ('process type', 'experiment', 'dataset', 'group', 'kinematics')
     if marker_by not in markers:
         raise CheckError("Unknown marker_by value", marker_by, markers)
 
@@ -1199,7 +1199,8 @@ def plot_xq2(
     will be displaed and marked.
 
     The points are grouped according to the `marker_by` option. The possible
-    values are: "process type", "experiment", "group" or "dataset".
+    values are: "process type", "experiment", "group" or "dataset" for discrete
+    colors, or "kinematics" for coloring by 1/(Q²(1-x)).
 
     Some datasets can be made to appear highlighted in the figure: Define a key
     called ``highlight_datasets`` containing the names of the datasets to be
@@ -1344,6 +1345,8 @@ def plot_xq2(
         elif marker_by == "group":
             # if group is None then make sure that shows on legend.
             key = str(group)
+        elif marker_by == "kinematics":
+            key = None
         else:
             raise ValueError('Unknown marker_by value')
 
@@ -1373,7 +1376,13 @@ def plot_xq2(
         else:
             # This is to get the label key
             coords = [], []
-        ax.plot(*coords, label=key, markeredgewidth=1, markeredgecolor=None, **key_options[key])
+        if marker_by == "kinematics":
+            ht_magnitude = np.log(1 / (coords[1] * (1 - coords[0])))  # 1/(Q2(1-x))
+            out = ax.scatter(*coords, marker='.', c=ht_magnitude, cmap="viridis")
+            clb = fig.colorbar(out)
+            clb.ax.set_title(r'$log(1/(Q^2(1-x)))$')
+        else:
+            ax.plot(*coords, label=key, markeredgewidth=1, markeredgecolor=None, **key_options[key])
 
     # Iterate again so highlights are printed on top.
     for key in xh:
