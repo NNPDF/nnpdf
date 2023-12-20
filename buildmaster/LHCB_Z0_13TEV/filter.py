7b58940926ee6e0cb6e6c54c7c7cfee24492e677
Tanjona Rabemananjara <rrabeman@nikhef.nl>
2023-12-20 07:53:21 +0100

    replace sqrt_s -> sqrts

--- buildmaster/LHCB_Z0_13TEV/filter.py
+++ buildmaster/LHCB_Z0_13TEV/filter.py
@@ -86,7 +86,7 @@ def get_kinematics(hepdata: dict, bin_index: list) -> list:
                 "max": ymax,
             },
             "M2": {"min": None, "mid": MZ_VALUE**2, "max": None},
-            "sqrt_s": {"min": None, "mid": SQRT_S, "max": None},
+            "sqrts": {"min": None, "mid": SQRT_S, "max": None},
         }
         kinematics.append(kin_value)
 
