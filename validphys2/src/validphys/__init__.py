8cb04b30a01994eea29c20003e6d8ad9c05dc3a8
juacrumar <juacrumar@lairen.eu>
2024-01-23 18:06:55 +0100

    add a helpful message for people who installed last before 4.0.8

--- validphys2/src/validphys/__init__.py
+++ validphys2/src/validphys/__init__.py
@@ -1,1 +1,4 @@
-from ._version import __version__
+try:
+    from ._version import __version__
+except ModuleNotFoundError:
+    raise ModuleNotFoundError("`_version` not found, you might need to reinstall nnpdf (e.g., `pip install .`) if you installed last before nnpdf 4.0.8")
