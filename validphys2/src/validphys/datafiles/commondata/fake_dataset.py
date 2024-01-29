a9a8bc31b30244498e5a36c1d9564c4a3fccc8c6
andreab1997 <andreab1997@yahoo.it>
2024-01-29 16:31:52 +0100

    Final data version maybe

--- validphys2/src/validphys/datafiles/commondata/fake_dataset.py
+++ validphys2/src/validphys/datafiles/commondata/fake_dataset.py
@@ -10,6 +10,7 @@ def split_string(test_str):
 FILE_TO_IMPLEMENT = pathlib.Path(sys.argv[1])
 FILE_TO_DUMP = pathlib.Path(sys.argv[2])
 TYPE_PROC = sys.argv[3]
+NAME = FILE_TO_IMPLEMENT.stem
 
 ###############
 #READ THE FILE
@@ -31,10 +32,13 @@ LENGHT = x.__len__()
 
 TYPE_list = [TYPE_PROC for i in range(LENGHT)]
 INDEX_list = [i+1 for i in range(LENGHT)]
-SYS_LIST = [0.0 for i in range(LENGHT)]
+STAT_LIST = [0.0 for i in range(LENGHT)]
+SYS_LIST_ADD = [0.0 for i in range(LENGHT)]
+SYS_LIST_MULT = [0.0 for i in range(LENGHT)]
 
-FINAL_DATAFRAME = pd.DataFrame.from_dict({0:INDEX_list,1:TYPE_list,2:x,3:Q2s,4:y,5:cross_sections,6:SYS_LIST})
-
-FINAL_DATAFRAME.to_csv(FILE_TO_DUMP, sep="\t", header=None, index=False)
+FINAL_DATAFRAME = pd.DataFrame.from_dict({0:INDEX_list,1:TYPE_list,2:x,3:Q2s,4:y,5:cross_sections,6:STAT_LIST, 7:SYS_LIST_ADD, 8:SYS_LIST_MULT})
+with open(FILE_TO_DUMP, 'a') as file:
+    file.write(f'{NAME}\t1\t{LENGHT}\n')
+    FINAL_DATAFRAME.to_csv(file, sep="\t", header=False, index=False)
 
 
