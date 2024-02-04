42523310107e9da32d87a659f61fd27d029168c0
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-02-04 15:53:36 +0000

    implemented CMS W DY data

---
+++ buildmaster/CMS_W_13TEV_ETA/filter.py
@@ -0,0 +1,115 @@
+import yaml
+
+from filter_utils import get_kinematics, get_data_values, get_systematics
+
+# all the ones not specified below are assumed to be CORR
+UNCORRELATED_SYS = [
+    "Relative uncertainty due to statistical uncertainty in data",
+    "Relative uncertainty due to statistical uncertainty on lepton efficiency",
+    "Relative uncertainty due to statistical uncertainty in simulation",
+]
+
+def filter_CMS_W_13TEV_data_kinetic(figure):
+    """
+    writes data central values and kinematics
+    to respective .yaml file
+    """
+    with open("metadata.yaml", "r") as file:
+        metadata = yaml.safe_load(file)
+    
+    version = metadata["hepdata"]["version"]
+    tables = metadata["implemented_observables"][0]["tables"]
+    
+    kin = get_kinematics(version, figure)
+    central_values = get_data_values(version, figure)
+
+
+    data_central_yaml = {"data_central": central_values}
+    kinematics_yaml = {"bins": kin}
+
+    # write central values and kinematics to yaml file
+    if figure == "17a":
+        with open("data_WP.yaml", "w") as file:
+            yaml.dump(data_central_yaml, file, sort_keys=False)
+
+        with open("kinematics_WP.yaml", "w") as file:
+            yaml.dump(kinematics_yaml, file, sort_keys=False)
+
+    elif figure == "17b":
+        with open("data_WM.yaml", "w") as file:
+            yaml.dump(data_central_yaml, file, sort_keys=False)
+
+        with open("kinematics_WM.yaml", "w") as file:
+            yaml.dump(kinematics_yaml, file, sort_keys=False)
+
+
+def filter_CMS_W_13TEV_uncertainties(figure):
+    """
+    writes uncertainties to respective .yaml file
+    """
+
+    with open("metadata.yaml", "r") as file:
+        metadata = yaml.safe_load(file)
+
+    version = metadata["hepdata"]["version"]
+    
+    tables = metadata["implemented_observables"][0]["tables"]
+    
+    systematics = get_systematics(version, figure)
+    
+    # error definition
+    error_definitions = {}
+    errors = []
+    
+
+    for sys in systematics:
+        
+        if sys[0]['name'] in UNCORRELATED_SYS:
+            error_definitions[sys[0]['name']] = {
+                "description": f"{sys[0]['name']}",
+                "treatment": "MULT",
+                "type": "UNCORR",
+            }
+        else:
+            error_definitions[sys[0]['name']] = {
+                "description": f"{sys[0]['name']}",
+                "treatment": "MULT",
+                "type": "CORR",
+            }
+
+    # 
+    for i in range(metadata['implemented_observables'][0]['ndata']):
+        error_value = {}
+        
+        for sys in systematics:
+            error_value[sys[0]['name']] = float(sys[0]['values'][i])
+
+        errors.append(error_value)
+
+    uncertainties_yaml = {"definitions": error_definitions, "bins": errors}
+    
+    # write uncertainties
+    if figure == "A23a":
+        with open(f"uncertainties_WP.yaml", 'w') as file:
+            yaml.dump(uncertainties_yaml, file, sort_keys=False)
+    elif figure == "A23b":
+        with open(f"uncertainties_WM.yaml", 'w') as file:
+            yaml.dump(uncertainties_yaml, file, sort_keys=False)
+        
+
+            
+    
+
+
+
+
+if __name__ == "__main__":
+    # WP data 
+    filter_CMS_W_13TEV_data_kinetic(figure="17a")
+    filter_CMS_W_13TEV_uncertainties(figure="A23a")
+
+    # WM data
+    filter_CMS_W_13TEV_data_kinetic(figure="17b")
+    filter_CMS_W_13TEV_uncertainties(figure="A23b")
+    
+    
---
+++ buildmaster/CMS_W_13TEV_ETA/filter_utils.py
@@ -0,0 +1,120 @@
+import yaml
+
+
+
+def get_kinematics(version, figure):
+    """
+    returns the relevant kinematics values.
+
+    Parameters
+    ----------
+    version : int
+            integer read from metadata.yaml that
+            indicated the version of the hepdata
+            tables
+    
+    figure : str
+        eg. 17a or 17b
+
+    Returns
+    -------
+    list
+        list containing the kinematic values for all
+        hepdata tables
+    """
+    kin = []
+
+    hepdata_table = f"rawdata/HEPData-ins1810913-v{version}-Figure_{figure}.yaml"
+
+    with open(hepdata_table, 'r') as file:
+        input = yaml.safe_load(file)
+
+    for eta in input["independent_variables"][0]['values']:
+        kin_value = {
+            'eta': {'min': eta['low'], 'mid': 0.5 * (eta['low'] + eta['high']), 'max': eta['high']},
+            'mZ_2': {'min': None, 'mid': 6460.5, 'max': None},
+            'sqrt_s': {'min': None, 'mid': 13000.0, 'max': None},
+        }
+
+        kin.append(kin_value)
+
+    return kin
+
+
+
+def get_data_values(version, figure):
+    """
+    returns the central data.
+
+    Parameters
+    ----------
+    version : int
+            integer read from metadata.yaml that
+            indicated the version of the hepdata
+            tables
+
+    figure : str
+        eg. 17a or 17b
+
+    Returns
+    -------
+    list
+        list containing the central values for all
+        hepdata tables
+
+    """
+
+    data_central = []
+
+    hepdata_table = f"rawdata/HEPData-ins1810913-v{version}-Figure_{figure}.yaml"
+
+    with open(hepdata_table, 'r') as file:
+        input = yaml.safe_load(file)
+
+    values = input['dependent_variables'][0]['values']
+      
+    for value in values:
+        data_central.append(value['value'])
+
+    return data_central
+
+
+def get_systematics(version, figure):
+    """
+    Get the systematics from the hepdata tables.
+
+    Parameters
+    ----------
+    version : int
+            integer read from metadata.yaml that
+            indicated the version of the hepdata
+            tables
+    
+    figure : str
+        string indicating the figure number for the impacts
+        can be A23a or A23b
+
+    Returns
+    -------
+    list
+        list containing the systematics
+    """
+    uncertainties = []
+
+    hepdata_table = f"rawdata/HEPData-ins1810913-v{version}-Impacts_Figure_{figure}.yaml"
+    
+    with open(hepdata_table, 'r') as file:
+        input = yaml.safe_load(file)
+
+    dependent_vars = input['dependent_variables']
+    
+    for dep_var in dependent_vars:
+    
+        name = dep_var['header']['name'] 
+        values = [d['value'] for d in dep_var['values']]
+        uncertainties.append([{"name":name, "values":values}])
+        
+    return uncertainties
+
+if __name__ == "__main__":
+    get_systematics(version=1, figure="A23a")
