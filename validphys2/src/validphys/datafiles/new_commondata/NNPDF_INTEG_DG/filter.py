8a14dd9b2157e9629d8f742ed9892f5f2175c548
Radonirinaunimi <rrabeman@nikhef.nl>
2024-02-10 19:50:09 +0100

    fix POS and INTEG by removing data and uncs

--- validphys2/src/validphys/datafiles/new_commondata/NNPDF_INTEG_DG/filter.py
+++ validphys2/src/validphys/datafiles/new_commondata/NNPDF_INTEG_DG/filter.py
@@ -1,25 +1,16 @@
 import yaml
 
-Q2_POS = 1.0  # GeV^2
+Q2_INTEG = 1.0  # GeV^2
 
 XVALUES_POS = [1e-5]
 
 
 def write_data():
-    data_central = []
-    for _ in range(len(XVALUES_POS)):
-        data_central.append(0.0)
-
-    data_central_yaml = {"data_central": data_central}
-    with open("data.yaml", "w") as file:
-        yaml.dump(data_central_yaml, file, sort_keys=False)
-
-    # Write kin file
     kin = []
     for xvalue in XVALUES_POS:
         kin_value = {
             "x": {"min": None, "mid": xvalue, "max": None},
-            "Q2": {"min": None, "mid": Q2_POS, "max": None},
+            "Q2": {"min": None, "mid": Q2_INTEG, "max": None},
             "y": {"min": None, "mid": 0.0, "max": None},
         }
         kin.append(kin_value)
@@ -28,27 +19,6 @@ def write_data():
 
     with open("kinematics.yaml", "w") as file:
         yaml.dump(kinematics_yaml, file, sort_keys=False)
-
-    # Write unc file
-    error = []
-    for _ in range(len(XVALUES_POS)):
-        e = {
-            "stat": 0.0,
-        }
-        error.append(e)
-
-    error_definition = {
-        "stat": {
-            "description": "statistical uncertainty",
-            "treatment": "ADD",
-            "type": "UNCORR",
-        },
-    }
-
-    uncertainties_yaml = {"definitions": error_definition, "bins": error}
-
-    with open("uncertainties.yaml", "w") as file:
-        yaml.dump(uncertainties_yaml, file, sort_keys=False)
 
 
 if __name__ == "__main__":
--- validphys2/src/validphys/datafiles/new_commondata/NNPDF_POS_DDOWN/filter.py
+++ validphys2/src/validphys/datafiles/new_commondata/NNPDF_POS_DDOWN/filter.py
@@ -1,6 +1,6 @@
 import yaml
 
-Q2_POS = 1.0  # GeV^2
+Q2_POS = 5.0  # GeV^2
 
 XVALUES_POS = [
     0.001,
@@ -27,15 +27,6 @@ XVALUES_POS = [
 
 
 def write_data():
-    data_central = []
-    for _ in range(len(XVALUES_POS)):
-        data_central.append(0.0)
-
-    data_central_yaml = {"data_central": data_central}
-    with open("data.yaml", "w") as file:
-        yaml.dump(data_central_yaml, file, sort_keys=False)
-
-    # Write kin file
     kin = []
     for xvalue in XVALUES_POS:
         kin_value = {
@@ -49,27 +40,6 @@ def write_data():
 
     with open("kinematics.yaml", "w") as file:
         yaml.dump(kinematics_yaml, file, sort_keys=False)
-
-    # Write unc file
-    error = []
-    for _ in range(len(XVALUES_POS)):
-        e = {
-            "stat": 0.0,
-        }
-        error.append(e)
-
-    error_definition = {
-        "stat": {
-            "description": "statistical uncertainty",
-            "treatment": "ADD",
-            "type": "UNCORR",
-        },
-    }
-
-    uncertainties_yaml = {"definitions": error_definition, "bins": error}
-
-    with open("uncertainties.yaml", "w") as file:
-        yaml.dump(uncertainties_yaml, file, sort_keys=False)
 
 
 if __name__ == "__main__":
