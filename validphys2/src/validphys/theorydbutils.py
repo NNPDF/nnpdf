44d5fee5e122941b5274062cfe3e1911c849e488
juacrumar <juacrumar@lairen.eu>
2024-03-10 16:45:26 +0100

    make the theorydb into a yamldb
    Update validphys2/src/validphys/datafiles/disp_theory.py
    
    Co-authored-by: Felix Hekhorn <felixhekhorn@users.noreply.github.com>
    
    apply comments and rebase
    
    cast explicitly as float

--- validphys2/src/validphys/theorydbutils.py
+++ validphys2/src/validphys/theorydbutils.py
@@ -5,65 +5,80 @@ theorydbutils.py
 low level utilities for querying the theory database file and representing the
 data as a python object.
 """
+from dataclasses import asdict, dataclass
+from functools import lru_cache
 from pathlib import Path
 
-# Keep the sqlite3 dependence in one location
-import sqlite3
-
 import pandas as pd
+
+from validphys.utils import parse_yaml_inp
+
+
+@dataclass(frozen=True)
+class _TheoryCard:
+    ID: int
+    PTO: int
+    FNS: str
+    DAMP: int
+    IC: int
+    ModEv: str
+    XIR: float
+    XIF: float
+    NfFF: int
+    MaxNfAs: int
+    MaxNfPdf: int
+    Q0: float
+    alphas: float
+    Qref: float
+    QED: int
+    alphaqed: float
+    Qedref: float
+    SxRes: int
+    SxOrd: str
+    HQ: str
+    mc: float
+    Qmc: float
+    kcThr: float
+    mb: float
+    Qmb: float
+    kbThr: float
+    mt: float
+    Qmt: float
+    ktThr: float
+    CKM: list[float]
+    MZ: float
+    MW: float
+    GF: float
+    SIN2TW: float
+    TMC: int
+    MP: float
+    Comments: str
+    global_nx: int
+    EScaleVar: int
 
 
 class TheoryNotFoundInDatabase(Exception):
     pass
 
 
-def make_query(query: str, dbpath: Path):
-    """Base level function which executes a `query` given the path to a sqlite3
-    database
-
-    Parameters
-    ----------
-    query: str
-        valid sqlite3 database query
-    dbpath: Path
-        pathlib.Path object pointing to sqlite3 database
-
-    Returns
-    -------
-    res: sqlite3.Cursor
-        an sqlite cursor with relevant data saved
-
-    Example
-    -------
-    >>> from pathlib import Path as p
-    >>> from validphys.theorydbutils import make_query
-    >>> query = "SELECT * FROM TheoryIndex WHERE ID=53;"
-    >>> dbpath = p("./validphys2/src/validphys/datafiles/theory.db")
-    >>> res = make_query(query, dbpath)
-    >>> val = res.fetchone()
-    >>> theory_info_dict = {k[0]: v for k, v in zip(res.description, val)}
-
-    See Also
-    --------
-    sqlite3: https://docs.python.org/3/library/sqlite3.html
+@lru_cache
+def parse_theory_card(theory_card):
+    """Read the theory card using validobj parsing
+    Returns the theory as a dictionary
     """
-    # python 3.6 still requires path as string, but 3.7 onwards can take
-    # pathlib.Path. This could be changed if we stop supporting 3.6
-    conn = sqlite3.connect(str(dbpath))
-    with conn:
-        cursor = conn.cursor()
-        res = cursor.execute(query)
-    return res
+    if theory_card.exists():
+        return asdict(parse_yaml_inp(theory_card, _TheoryCard))
+    raise TheoryNotFoundInDatabase(f"Theory card {theory_card} not found")
 
 
 def fetch_theory(theory_database: Path, theoryID: int):
-    """Looks in the theory database and returns a dictionary of theory info for the
+    """Looks in the theory card folder and returns a dictionary of theory info for the
     theory number specified by `theoryID`.
 
     Parameters
     ----------
     theory_database: Path
-        pathlib.Path pointing to theorydb.db
+        pathlib.Path pointing to the folder with the theory cards
     theoryID: int
         numeric identifier of theory to query info
 
@@ -71,14 +86,18 @@ def fetch_theory(theory_database: Path, theoryID: int):
     -------
     theory_info_dict: dict
         dictionary filled with relevant entry from theory database
+
+    Example
+    ------
+    >>> from validphys.datafiles import theory_cards
+    >>> from validphys.theorydbutils import fetch_theory
+    >>> theory = fetch_theory(theory_cards, 700)
     """
-    # int casting is intentional to avoid malformed querys.
-    query = f"SELECT * FROM TheoryIndex WHERE ID={int(theoryID)};"
-    res = make_query(query, theory_database)
-    val = res.fetchone()
-    if not val:
-        raise TheoryNotFoundInDatabase(f"ID {theoryID} not found in database.")
-    return {k[0]: v for k, v in zip(res.description, val)}
+    filepath = theory_database / f"{theoryID}.yaml"
+    tdict = parse_theory_card(filepath)
+    if tdict["ID"] != int(theoryID):
+        raise ValueError(f"The theory ID in {filepath} doesn't correspond with its ID entry")
+    return tdict
 
 
 def fetch_all(theory_database: Path):
@@ -88,16 +107,21 @@ def fetch_all(theory_database: Path):
     Parameters
     ----------
     theory_database: Path
-        pathlib.Path pointing to theorydb.db
+        pathlib.Path pointing to the folder with theory cards
 
     Returns
     -------
     theory_info_dataframe: pd.Dataframe
         dataframe filled with all entries in theorydb file
+
+    Example
+    ------
+    >>> from validphys.datafiles import theory_cards
+    >>> from validphys.theorydbutils import fetch_all
+    >>> theory_df = fetch_all(theory_cards)
     """
-    query = "SELECT * FROM TheoryIndex;"
-    res = make_query(query, theory_database)
-    val = res.fetchall()
-    cols = [k[0] for k in res.description]
-    df = pd.DataFrame(val, columns=cols)
-    return df.set_index(['ID'])
+    theories = []
+    for theory_path in theory_database.glob("*.yaml"):
+        theories.append(parse_theory_card(theory_path))
+    df = pd.DataFrame(theories)
+    return df.set_index(['ID']).sort_index()
