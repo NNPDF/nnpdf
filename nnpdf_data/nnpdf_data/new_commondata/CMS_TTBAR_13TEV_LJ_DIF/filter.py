d885b474f8fdc1acd8c39c2849d1c4450739bc36
t7phy <t7phy.in@gmail.com>
2024-03-25 22:35:03 +0100

    utils now depend on ATLAS_TTBAR_13TEV_HADR_DIF

--- nnpdf_data/nnpdf_data/new_commondata/CMS_TTBAR_13TEV_LJ_DIF/filter.py
+++ nnpdf_data/nnpdf_data/new_commondata/CMS_TTBAR_13TEV_LJ_DIF/filter.py
@@ -1,5 +1,5 @@
 import yaml
-from validphys.commondata_utils import covmat_to_artunc as cta
+from nnpdf_data.new_commondata.ATLAS_TTBAR_13TEV_HADR_DIF.utils import covmat_to_artunc as cta
 
 def processData():
     with open('metadata.yaml', 'r') as file:
