c3d277c38dd88af86db7381db0be759e3e9ffcc3
RoyStegeman <roystegeman@live.nl>
2024-04-16 15:04:37 +0100

    replace .format with f-string

--- validphys2/src/validphys/tests/conftest.py
+++ validphys2/src/validphys/tests/conftest.py
@@ -207,7 +207,7 @@ def pytest_runtest_setup(item):
     supported_platforms = ALL.intersection(mark.name for mark in item.iter_markers())
     plat = sys.platform
     if supported_platforms and plat not in supported_platforms:
-        pytest.skip("cannot run on platform {}".format(plat))
+        pytest.skip(f"cannot run on platform {plat}")
 
 
 def pytest_configure(config):
