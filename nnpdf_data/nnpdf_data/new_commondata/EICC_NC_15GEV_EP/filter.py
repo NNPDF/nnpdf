2ac1b8151f56fa26e7eb0c4e0a5c0f102e0cdd7e
Radonirinaunimi <rrabeman@nikhef.nl>
2024-05-24 18:35:49 +0200

    add flat syst for EIcC and include NLO & NNLO variants

--- nnpdf_data/nnpdf_data/new_commondata/EICC_NC_15GEV_EP/filter.py
+++ nnpdf_data/nnpdf_data/new_commondata/EICC_NC_15GEV_EP/filter.py
@@ -15,6 +15,18 @@ np.random.seed(1234567890)
 if __name__ == "__main__":
     input_txt = Path("./rawdata/EICC_3_20_A1c_100fb-1.txt")
     df = read_txt_data(input_txt)
-    cv_preds = read_central_values(Path("./rawdata/EICC_NC_15GEV_EP.yaml"))
-    fluctuated_cv = fluctuate_data(cv_preds, df["abs"].values)
-    write_data(df, asym=False, abserr=fluctuated_cv, add_fluctuate=True)
+
+    for pto in ["NLO", "NNLO"]:
+        cv_preds = read_central_values(Path(f"./rawdata/EICC_NC_15GEV_EP_{pto}.yaml"))
+        fluctuated_cv = fluctuate_data(cv_preds, df["abs"].values)
+        # Flat systematic error of 3.2% due to beam polarization
+        # from Yuxiang suggestion
+        sys_error = 0.050
+        write_data(
+            df,
+            asym=False,
+            abserr=fluctuated_cv,
+            add_fluctuate=True,
+            sys_error=sys_error,
+            suffix=pto.lower(),
+        )
