04cfd57bf4de62603bf7e77f60082593e3e57d03
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-06-06 14:38:44 +0100

    added init file and float prettifier to filter utils

--- nnpdf_data/nnpdf_data/filter_utils/utils.py
+++ nnpdf_data/nnpdf_data/filter_utils/utils.py
@@ -3,6 +3,7 @@ Module contains general utility functions for general use in implementing datase
 """
 
 import numpy as np
+import yaml
 
 
 def correlation_to_covariance(correlation, uncertainties):
@@ -34,3 +35,27 @@ def decompose_covmat(covmat):
     lamb, mat = np.linalg.eig(covmat)
     sys = np.multiply(np.sqrt(lamb), mat)
     return sys
+
+
+def prettify_float(dumper, value):
+    """
+    Override the default yaml representer:
+    https://github.com/yaml/pyyaml/blob/48838a3c768e3d1bcab44197d800145cfd0719d6/lib/yaml/representer.py#L189
+
+    This function is used to prettify the float representation in the yaml file.
+    If the float has more than 8 digits, it will be represented in scientific notation with 8 digits.
+
+    Note:
+    -----
+    When importing yaml in a module,
+
+    yaml.add_representer(float, prettify_float)
+
+    must be called to use this function.
+    """
+
+    ret = dumper.represent_float(value)
+    if len(ret.value) > 8:
+        ret_str = f"{value:.8e}"
+        ret = dumper.represent_scalar('tag:yaml.org,2002:float', ret_str)
+    return ret
