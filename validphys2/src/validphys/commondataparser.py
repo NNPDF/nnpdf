71271be249fc4c5d86afd2270136957554d2a3e1
toonhasenack <toon.hasenack@outlook.com>
2024-06-14 13:25:26 +0200

    pass minmax kinematics when loading data

--- validphys2/src/validphys/commondataparser.py
+++ validphys2/src/validphys/commondataparser.py
@@ -857,7 +857,7 @@ def load_commondata_new(metadata):
     # the uncertainties
     uncertainties_df = metadata.load_uncertainties()
     # and the kinematics
-    kin_df = metadata.load_kinematics()
+    kin_df = metadata.load_kinematics(drop_minmax=False)
 
     # Once we have loaded all uncertainty files, let's check how many sys we have
     nsys = len(
@@ -915,7 +915,7 @@ def load_commondata_new(metadata):
         setname=metadata.name,
         ndata=metadata.ndata,
         commondataproc=procname,
-        nkin=3,
+        nkin=9,
         nsys=nsys,
         commondata_table=commondata_table,
         systype_table=systype_table,
