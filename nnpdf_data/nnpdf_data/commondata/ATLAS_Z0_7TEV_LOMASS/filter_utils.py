2e0f1b7f34f239236ceae62b722ceaa04cf14023
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-10-11 13:53:27 +0100

    added get sys to filter utils

--- nnpdf_data/nnpdf_data/commondata/ATLAS_Z0_7TEV_LOMASS/filter_utils.py
+++ nnpdf_data/nnpdf_data/commondata/ATLAS_Z0_7TEV_LOMASS/filter_utils.py
@@ -25,7 +25,11 @@ def get_kinematics():
 
         kin_value = {
             'y': {'min': None, 'mid': y_values[i], 'max': None},
-            'M2': {'min': M['low'] ** 2, 'mid': 0.5 * (M['low'] + M['high']), 'max': M['high']},
+            'M2': {
+                'min': M['low'] ** 2,
+                'mid': (0.5 * (M['low'] + M['high'])) ** 2,
+                'max': M['high'] ** 2,
+            },
             'sqrts': {'min': None, 'mid': 7000.0, 'max': None},
         }
 
@@ -36,7 +40,7 @@ def get_kinematics():
 
 def get_data_values():
     """
-    returns the central data values in the form of a numpy array.
+    returns the central data values in the form of a list.
     """
 
     data_central = []
@@ -52,19 +56,33 @@ def get_data_values():
         # store data central and convert the units
         data_central.append(value['value'] * 1000)
 
-    return np.array(data_central)
+    return data_central
 
 
-def get_systematics():
+def get_systematics_dataframe():
     """
     returns the absolute systematic uncertainties in the form of a pandas dataframe.
     """
     sys_rawdata_path = "rawdata/ATLASLOMASSDY11EXT.csv"
 
     df = pd.read_csv(sys_rawdata_path)
-    data_central = get_data_values()
+    data_central = np.array(get_data_values())
 
     # convert (MULT) percentage unc to absolute unc
     abs_unc_df = (df.T[3:] * data_central).T / 100
 
     return abs_unc_df
+
+
+def get_systematics():
+    """ """
+    abs_unc_df = get_systematics_dataframe()
+
+    uncertainties = []
+
+    for i, unc_dp in enumerate(abs_unc_df.values.T):
+        name = f"{abs_unc_df.columns[i]}"
+        values = [unc_dp[j] for j in range(len(unc_dp))]
+        uncertainties.append([{"name": name, "values": values}])
+
+    return uncertainties
