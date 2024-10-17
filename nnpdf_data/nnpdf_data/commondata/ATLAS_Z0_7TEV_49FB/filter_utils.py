37624d485555f65a5acad7522f4937890eac34db
ecole41 <ellagcole02@gmail.com>
2024-10-17 14:47:40 +0100

    Added kinematics to filter

--- nnpdf_data/nnpdf_data/commondata/ATLAS_Z0_7TEV_49FB/filter_utils.py
+++ nnpdf_data/nnpdf_data/commondata/ATLAS_Z0_7TEV_49FB/filter_utils.py
@@ -1,5 +1,5 @@
 """
-This module contains helper functions that are used to extract the uncertainties, kinematics and data values 
+This module contains helper functions that are used to extract the data values 
 from the rawdata files.
 """
 
@@ -25,6 +25,63 @@ def get_data_values():
         data_central.append(value['value'] * 1000)
 
     return data_central
+
+
+def get_kinematics():
+    """
+    returns the kinematics in the form of a list of dictionaries.
+    """
+    kin = []
+
+    hepdata_table = f"rawdata/HEPData-ins1234228-v1-Table_1.yaml"
+
+    with open(hepdata_table, 'r') as file:
+        input = yaml.safe_load(file)
+
+    for i, M in enumerate(input["independent_variables"][0]['values']):
+
+        kin_value = {
+            'y': {'min': None, 'mid': (0.5 * (M['low'] + M['high'])), 'max': None},  # y~M(EE)?
+            'M2': {
+                'min': M['low'] ** 2,
+                'mid': (0.5 * (M['low'] + M['high'])) ** 2,
+                'max': M['high'] ** 2,
+            },
+            'sqrts': {'min': None, 'mid': 7000.0, 'max': None},
+        }
+
+        kin.append(kin_value)
+
+    return kin
+
+
+def get_systematics_dataframe():
+    """
+    returns the absolute systematic uncertainties in the form of a pandas dataframe.
+    """
+    sys_rawdata_path = "rawdata/ATLAS-49fb-Zhighmass.sys"
+
+    df = pd.read_csv(sys_rawdata_path)
+    data_central = np.array(get_data_values())
+
+    # convert (MULT) percentage unc to absolute unc
+    abs_unc_df = (df.T[3:] * data_central).T / 100
+
+    return abs_unc_df
+
+
+def get_systematics():
+    """ """
+    abs_unc_df = get_systematics_dataframe()
+
+    uncertainties = []
+
+    for i, unc_dp in enumerate(abs_unc_df.values.T):
+        name = f"{abs_unc_df.columns[i]}"
+        values = [unc_dp[j] for j in range(len(unc_dp))]
+        uncertainties.append([{"name": name, "values": values}])
+
+    return uncertainties
 
 
 if __name__ == "__main__":
