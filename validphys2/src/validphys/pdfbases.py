42ec877b865d1ce976076689345b6ef98ab0aa1e
enocera <emanueleroberto.nocera@unito.it>
2024-10-31 12:48:53 +0100

    Added basis to display PDG-style PDF plots for polarised PDFs

--- validphys2/src/validphys/pdfbases.py
+++ validphys2/src/validphys/pdfbases.py
@@ -702,6 +702,16 @@ r'\bar{d}': {'dbar':1},
 'c': {'c':1},
 })
 
+pdg_pol = LinearBasis.from_mapping({
+'\Delta g': {'g':1},
+'\Delta u_{v}': {'u':1, 'ubar':-1},
+'\Delta d_{v}': {'d':1, 'dbar': -1},
+'\Delta s': {'s':1},
+r'\Delta\bar{u}': {'ubar':1},
+r'\Delta\bar{d}': {'dbar':1},
+'\Delta c': {'c':1},
+})
+
 @scalar_function_transformation(label="u_V")
 def u_valence(func, xmat, qmat):
     gv = func([2, -2], xmat, qmat)
