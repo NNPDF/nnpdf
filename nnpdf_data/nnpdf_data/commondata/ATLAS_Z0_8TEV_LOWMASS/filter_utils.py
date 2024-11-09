7a07e5f7e9d2de33fd0733bbb65f72afd409bcaf
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-11-09 10:40:39 +0000

    added data and kinematics

---
+++ nnpdf_data/nnpdf_data/commondata/ATLAS_Z0_8TEV_LOWMASS/filter_utils.py
@@ -0,0 +1,92 @@
+"""
+This module contains helper functions that are used to extract the uncertainties, kinematics and data values 
+from the rawdata files.
+"""
+
+import yaml
+import pandas as pd
+import numpy as np
+
+
+def get_kinematics():
+    """
+    returns the kinematics in the form of a list of dictionaries.
+    """
+    kin = []
+
+    hepdata_table = f"rawdata/HEPData-ins1630886-v3-Table_5.yaml"
+
+    with open(hepdata_table, 'r') as file:
+        input = yaml.safe_load(file)
+
+    for indep_var1, indep_var2 in zip(input["independent_variables"][1]['values'], input["independent_variables"][2]['values']):
+
+        kin_value = {
+            'y': {'min': indep_var1['low'], 'mid': 0.5 * (indep_var1['low']+indep_var1['high']), 'max': indep_var1['high']},
+            'M2': {
+                'min': indep_var2['low'] ** 2,
+                'mid': (0.5 * (indep_var2['low'] + indep_var2['high'])) ** 2,
+                'max': indep_var2['high'] ** 2,
+            },
+            'sqrts': {'min': None, 'mid': 8000.0, 'max': None},
+        }
+
+        kin.append(kin_value)
+    
+    return kin
+
+
+def get_data_values():
+    """
+    returns the central data values in the form of a list.
+    """
+
+    data_central = []
+
+    hepdata_table = f"rawdata/HEPData-ins1630886-v3-Table_5.yaml"
+
+    with open(hepdata_table, 'r') as file:
+        input = yaml.safe_load(file)
+
+    values = input['dependent_variables'][0]['values']
+
+    for value in values:
+        # store data central and convert the units
+        data_central.append(value['value'] * 1000)
+
+    return data_central
+
+
+def get_systematics_dataframe():
+    """
+    returns the absolute systematic uncertainties in the form of a pandas dataframe.
+    """
+    sys_rawdata_path = "rawdata/ATLASLOMASSDY11EXT.csv"
+
+    df = pd.read_csv(sys_rawdata_path)
+    data_central = np.array(get_data_values())
+
+    # convert (MULT) percentage unc to absolute unc
+    abs_unc_df = (df.T[3:] * data_central).T / 100
+
+    return abs_unc_df
+
+
+def get_systematics():
+    """ """
+    abs_unc_df = get_systematics_dataframe()
+
+    uncertainties = []
+
+    for i, unc_dp in enumerate(abs_unc_df.values.T):
+        name = f"{abs_unc_df.columns[i]}"
+        values = [unc_dp[j] for j in range(len(unc_dp))]
+        uncertainties.append([{"name": name, "values": values}])
+
+    return uncertainties
+
+if __name__ == '__main__':
+    get_kinematics()
+    # get_data_values()
+    # get_systematics_dataframe()
+    # get_systematics()
