f148b218184147344bb3684d63319b482133e99d
Roy Stegeman <roystegeman@live.nl>
2024-11-14 16:26:24 +0000

    format a bit

--- validphys2/src/validphys/commondata.py
+++ validphys2/src/validphys/commondata.py
@@ -6,9 +6,12 @@ found in :py:mod:`validphys.commondataparser`, and returns objects from
 :py:mod:`validphys.coredata`
 
 """
+
+import functools
+
 from reportengine import collect
 from validphys.commondataparser import load_commondata
-import functools
+
 
 @functools.lru_cache
 def loaded_commondata_with_cuts(commondata, cuts):
@@ -37,5 +40,5 @@ groups_dataset_inputs_loaded_cd_with_cuts = collect(
 )
 
 groups_dataset_inputs_loaded_cd_with_cuts_byprocess = collect(
-     "loaded_commondata_with_cuts", ("group_dataset_inputs_by_process", "data")
- )
+    "loaded_commondata_with_cuts", ("group_dataset_inputs_by_process", "data")
+)
