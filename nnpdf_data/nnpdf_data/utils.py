5367ccfe0fc6de716011a1e185503fbeae7d61ff
juacrumar <juacrumar@lairen.eu>
2024-11-23 16:59:55 -0500

    first version of the nnpdf data package , with versioning
    add utility function to read metadata just from dataset name
    
    deprecate a bunch of functions
    
    fix include

--- nnpdf_data/nnpdf_data/utils.py
+++ nnpdf_data/nnpdf_data/utils.py
@@ -6,9 +6,19 @@ import yaml
 
 yaml_rt = YAML(typ="rt")
 try:
+    # If libyaml is available, use the C loader
     Loader = yaml.CLoader
 except AttributeError:
+    # fallback to the slow loader
     Loader = yaml.Loader
+
+
+def quick_yaml_load(filepath):
+    """If libyaml is available, use the C loader to speed up some of the read
+    https://pyyaml.org/wiki/LibYAML
+    libyaml is available for most linux distributions
+    """
+    return yaml.load(filepath.read_text(encoding="utf-8"), Loader=Loader)
 
 
 def parse_yaml_inp(input_yaml, spec):
@@ -19,7 +29,7 @@ def parse_yaml_inp(input_yaml, spec):
     https://validobj.readthedocs.io/en/latest/examples.html#yaml-line-numbers
     """
     input_yaml = pathlib.Path(input_yaml)
-    inp = yaml.load(input_yaml.read_text(encoding="utf-8"), Loader=Loader)
+    inp = quick_yaml_load(input_yaml)
     try:
         return parse_input(inp, spec)
     except ValidationError as e:
