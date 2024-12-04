7d307affd11359c9e31ac8ac2cf12f7b69d97a4d
achiefa <amedeochiefa@gmail.com>
2024-12-04 09:27:13 +0000

    Remove log in the filter filter

--- nnpdf_data/nnpdf_data/commondata/ATLAS_WCHARM_7TEV/filter.py
+++ nnpdf_data/nnpdf_data/commondata/ATLAS_WCHARM_7TEV/filter.py
@@ -1,21 +1,5 @@
 '''
 Filter script for ATLAS_WJ_8TEV
-
-Log:
-- Central data and kinematics implemented from hepdata
-- Central data and kinematics checked with legacy: passed
-- The bin values in `uncertainties_legacy_WX-PT.yaml` and `uncertainties_legacy_WX-PT_sys_ATLAS.yaml`
-  are exactly the same (checked with notebook). The difference between
-  these two variants is in the definition of the uncertainties. See notebook for
-  differences.
-- For the treatment of systematic uncertainties, refer to https://arxiv.org/pdf/2112.11266.
-  There are 50 sources of CORRELATED systematic uncertainty.
-
-29 / 11 / 2024
-- Implemented uncertaintiy definitions
-- Construction of the uncertainty file is still missing. Remember that
-  for the symmetrised case you account for the shifts.
-- Xq2 is missing (?)
 '''
 
 import logging
