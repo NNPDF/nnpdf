a7bc4516142c3108fcf3f940fa57e17d153d1ee1
achiefa <amedeochiefa@gmail.com>
2024-12-05 12:27:21 +0000

    Update metadata and filter for CMS_Z0_7TEV_DIMUON

--- nnpdf_data/nnpdf_data/commondata/CMS_Z0_7TEV_DIMUON/filter.py
+++ nnpdf_data/nnpdf_data/commondata/CMS_Z0_7TEV_DIMUON/filter.py
@@ -77,7 +77,7 @@ def get_kinematics(hepdata: pd.DataFrame, bin_index: list, boson: str = "Z") -> 
     for bins in bin_index:
         kin_value = {
             "y": {"min": None, "mid": float(y[bins]), "max": None},
-            "M2": {"min": None, "mid": float(M[bins]) ** 2, "max": None},
+            "m_Z2": {"min": None, "mid": float(M[bins]) ** 2, "max": None},
             "sqrts": {"min": None, "mid": SQRT_S, "max": None},
         }
         kinematics.append(kin_value)
