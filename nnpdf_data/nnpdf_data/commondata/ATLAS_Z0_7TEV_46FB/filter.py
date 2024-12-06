b9a07fafb806fd7130d9afe85b518a4c856e1893
ecole41 <ellagcole02@gmail.com>
2024-12-06 10:41:39 +0000

    Added function to produce kinematics.yaml file and edited metadata, not sure on process type

--- nnpdf_data/nnpdf_data/commondata/ATLAS_Z0_7TEV_46FB/filter.py
+++ nnpdf_data/nnpdf_data/commondata/ATLAS_Z0_7TEV_46FB/filter.py
@@ -4,22 +4,27 @@ file will be created in the `nnpdf_data/commondata/ATLAS_WPWM_7TEV_46FB` directo
 """
 
 import yaml
-from filter_utils import get_data_values
+from filter_utils import get_data_values, get_kinematics
 
 from nnpdf_data.filter_utils.utils import prettify_float
 
 yaml.add_representer(float, prettify_float)
 
 
-def filter_ATLAS_Z0_7TEV_46FB_data_central():
+def filter_ATLAS_Z0_7TEV_46FB_data_kinematic():
     """
-    This function writes the central values to yaml files.
+    This function writes the systematics to yaml files.
     """
 
     central_values_cc, central_values_cf = get_data_values()
 
+    kin_cc, kin_cf = get_kinematics()
+
     data_central_yaml_cc = {"data_central": central_values_cc}
     data_central_yaml_cf = {"data_central": central_values_cf}
+
+    kinematics_yaml_cc = {"bins": kin_cc}
+    kinematics_yaml_cf = {"bins": kin_cf}
 
     # write central values and kinematics to yaml file
     with open("data_cc.yaml", "w") as file:
@@ -28,6 +33,12 @@ def filter_ATLAS_Z0_7TEV_46FB_data_central():
     with open("data_cf.yaml", "w") as file:
         yaml.dump(data_central_yaml_cf, file, sort_keys=False)
 
+    with open("kinematics_cc.yaml", "w") as file:
+        yaml.dump(kinematics_yaml_cc, file, sort_keys=False)
+
+    with open("kinematics_cf.yaml", "w") as file:
+        yaml.dump(kinematics_yaml_cf, file, sort_keys=False)
+
 
 if __name__ == "__main__":
-    filter_ATLAS_Z0_7TEV_46FB_data_central()
+    filter_ATLAS_Z0_7TEV_46FB_data_kinematic()
--- nnpdf_data/nnpdf_data/commondata/ATLAS_Z0_7TEV_46FB/filter_utils.py
+++ nnpdf_data/nnpdf_data/commondata/ATLAS_Z0_7TEV_46FB/filter_utils.py
@@ -40,5 +40,49 @@ def get_data_values():
     return data_central_cc, data_central_cf
 
 
+def get_kinematics():
+    """
+    returns the kinematics in the form of a list of dictionaries.
+    """
+    kin_cc = []
+    kin_cf = []
+    cc_tables = [11, 12, 13]
+    cf_tables = [14, 15]
+
+    # Define a mapping for table numbers to av_m_ll2 values
+    av_m_ll2_mapping = {11: 56**2, 12: 91**2, 13: 133**2, 14: 91**2, 15: 133**2}
+
+    for table in cc_tables:
+        hepdata_table = f"rawdata/HEPData-ins1502620-v1-Table_{table}.yaml"
+        av_m_ll2 = av_m_ll2_mapping[table]
+        with open(hepdata_table, 'r') as file:
+            input = yaml.safe_load(file)
+
+        for i, M in enumerate(input["independent_variables"][0]['values']):
+            kin_value = {
+                'abs_eta': {'min': None, 'mid': (0.5 * (M['low'] + M['high'])), 'max': None},
+                'm_ll2': {'min': None, 'mid': av_m_ll2, 'max': None},
+                'sqrts': {'min': None, 'mid': 7000.0, 'max': None},
+            }
+            kin_cc.append(kin_value)
+
+    for table in cf_tables:
+        hepdata_table = f"rawdata/HEPData-ins1502620-v1-Table_{table}.yaml"
+        av_m_ll2 = av_m_ll2_mapping[table]
+        with open(hepdata_table, 'r') as file:
+            input = yaml.safe_load(file)
+
+        for i, M in enumerate(input["independent_variables"][0]['values']):
+            kin_value = {
+                'abs_eta': {'min': None, 'mid': (0.5 * (M['low'] + M['high'])), 'max': None},
+                'm_ll2': {'min': None, 'mid': av_m_ll2, 'max': None},
+                'sqrts': {'min': None, 'mid': 7000.0, 'max': None},
+            }
+            kin_cf.append(kin_value)
+
+    return kin_cc, kin_cf
+
+
 if __name__ == "__main__":
     get_data_values()
+    get_kinematics()
