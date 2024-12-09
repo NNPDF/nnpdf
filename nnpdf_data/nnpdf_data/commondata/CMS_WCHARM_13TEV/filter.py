ce22f265a13dd028a45c4da6b71fe535b55c9f99
achiefa <amedeochiefa@gmail.com>
2024-12-09 18:56:13 +0000

    Implementation in the new format - WIP

---
+++ nnpdf_data/nnpdf_data/commondata/CMS_WCHARM_13TEV/filter.py
@@ -0,0 +1,17 @@
+'''
+Filter script for CMS_WCHARM_13TEV
+'''
+
+import logging
+
+from filter_utils import Extractor
+import numpy as np
+import yaml
+
+logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
+
+
+if __name__ == "__main__":
+    CMS_WCHARM = Extractor("./metadata.yaml", "WPWM-TOT-UNNORM", mult_factor=1000)
+    _, _, _ = CMS_WCHARM.generate_data(variant='default', save_to_yaml=True)
+    _, _, _ = CMS_WCHARM.generate_data(variant='sys_10', save_to_yaml=True)
