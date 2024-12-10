e8447efeee2577cd4a88f7aaca3dfb5633edd78f
achiefa <amedeochiefa@gmail.com>
2024-12-10 14:18:31 +0000

    Correct bug

--- nnpdf_data/nnpdf_data/commondata/CMS_WCHARM_7TEV/filter.py
+++ nnpdf_data/nnpdf_data/commondata/CMS_WCHARM_7TEV/filter.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     CMS_WCHARM_TOT = Extractor("./metadata.yaml", "WPWM-TOT", mult_factor=1000)
     _, _, _ = CMS_WCHARM_TOT.generate_data(variant='default', save_to_yaml=True)
 
-    CMS_WCHARM_RATIO = Extractor("./metadata.yaml", "WPWM-RATIO", mult_factor=1000)
+    CMS_WCHARM_RATIO = Extractor("./metadata.yaml", "WPWM-RATIO", mult_factor=1.0)
     _, _, _ = CMS_WCHARM_RATIO.generate_data(variant='default', save_to_yaml=True)
