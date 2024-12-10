e4cb7bd32efa66f98f7e3886864e8ffb3584aa70
achiefa <amedeochiefa@gmail.com>
2024-12-10 14:13:12 +0000

    Add filter files

---
+++ nnpdf_data/nnpdf_data/commondata/CMS_WCHARM_7TEV/filter.py
@@ -0,0 +1,18 @@
+'''
+Filter script for CMS_WCHARM_7TEV
+'''
+
+import logging
+
+from filter_utils import Extractor
+import numpy as np
+
+logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
+
+
+if __name__ == "__main__":
+    CMS_WCHARM_TOT = Extractor("./metadata.yaml", "WPWM-TOT", mult_factor=1000)
+    _, _, _ = CMS_WCHARM_TOT.generate_data(variant='default', save_to_yaml=True)
+
+    CMS_WCHARM_RATIO = Extractor("./metadata.yaml", "WPWM-RATIO", mult_factor=1000)
+    _, _, _ = CMS_WCHARM_RATIO.generate_data(variant='default', save_to_yaml=True)
