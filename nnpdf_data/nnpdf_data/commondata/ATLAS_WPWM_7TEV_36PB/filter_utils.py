c53fe29c0be086711f820ab00f64b62903849836
ecole41 <ellagcole02@gmail.com>
2024-12-17 14:26:52 +0000

    Added kinematics function and edited metadata kinematics

--- nnpdf_data/nnpdf_data/commondata/ATLAS_WPWM_7TEV_36PB/filter_utils.py
+++ nnpdf_data/nnpdf_data/commondata/ATLAS_WPWM_7TEV_36PB/filter_utils.py
@@ -15,7 +15,7 @@ def get_data_values():
 
     data_central = []
 
-    tables = [5,3]
+    tables = [5, 3]
 
     for table in tables:
         hepdata_table = f"rawdata/HEPData-ins928289-v1-Table_{table}.yaml"
@@ -32,5 +32,31 @@ def get_data_values():
     return data_central
 
 
+def get_kinematics():
+    """
+    returns the kinematics in the form of a list of dictionaries.
+    """
+    kin = []
+
+    tables = [5, 3]
+
+    for table in tables:
+        hepdata_table = f"rawdata/HEPData-ins928289-v1-Table_{table}.yaml"
+
+        with open(hepdata_table, 'r') as file:
+            input = yaml.safe_load(file)
+
+        for i, M in enumerate(input["independent_variables"][0]['values']):
+            kin_value = {
+                'abs_eta': {'min': None, 'mid': (0.5 * (M['low'] + M['high'])), 'max': None},
+                'm_W2': {'min': None, 'mid': 6463.838404, 'max': None},
+                'sqrts': {'min': None, 'mid': 7000.0, 'max': None},
+            }
+            kin.append(kin_value)
+
+    return kin
+
+
 if __name__ == "__main__":
     get_data_values()
+    get_kinematics()
