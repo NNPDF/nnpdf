415123742ff0b7cfa7149ce3cc5530decdca59e2
Amedeo Chiefa <103528316+achiefa@users.noreply.github.com>
2024-12-18 11:00:02 +0100

    Remove `save_to_yaml` option.

--- nnpdf_data/nnpdf_data/commondata/CMS_Z0J_8TEV/filter.py
+++ nnpdf_data/nnpdf_data/commondata/CMS_Z0J_8TEV/filter.py
@@ -12,5 +12,5 @@ logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
 
 if __name__ == "__main__":
     CMS_Z0J_8TEV = Extractor("./metadata.yaml", "PT-Y", mult_factor=1000)
-    CMS_Z0J_8TEV.generate_data(variant='default', save_to_yaml=True)
-    CMS_Z0J_8TEV.generate_data(variant='sys_10', save_to_yaml=True)
+    CMS_Z0J_8TEV.generate_data(variant='default')
+    CMS_Z0J_8TEV.generate_data(variant='sys_10')
