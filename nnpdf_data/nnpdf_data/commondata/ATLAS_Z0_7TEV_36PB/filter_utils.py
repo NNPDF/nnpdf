1512d268122ed1aadf9eb9b4fb0bcfa135615dfb
ecole41 <ellagcole02@gmail.com>
2024-12-18 11:39:54 +0000

    Added function to produce data.yaml

---
+++ nnpdf_data/nnpdf_data/commondata/ATLAS_Z0_7TEV_36PB/filter_utils.py
@@ -0,0 +1,33 @@
+"""
+This module contains helper functions that are used to extract the data values 
+from the rawdata files.
+"""
+
+import yaml
+import pandas as pd
+import numpy as np
+
+
+def get_data_values():
+    """
+    returns the central data values in the form of a list.
+    """
+
+    data_central = []
+
+    hepdata_table = f"rawdata/HEPData-ins928289-v1-Table_1.yaml"
+
+    with open(hepdata_table, 'r') as file:
+        input = yaml.safe_load(file)
+
+    values = input['dependent_variables'][0]['values']
+
+    for value in values:
+        # store data central and convert the units and apply the correction factor
+        data_central.append(value['value'] * 1000 * 1.0187)
+
+    return data_central
+
+
+if __name__ == "__main__":
+    get_data_values()
