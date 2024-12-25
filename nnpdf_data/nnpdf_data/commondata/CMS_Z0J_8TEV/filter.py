936fdaff20fff788d0ef5592f930ab5be04a7165
Roy Stegeman <roystegeman@live.nl>
2024-12-25 17:18:20 +0100

    clean up CMS_Z0J_8TEV filter files

--- nnpdf_data/nnpdf_data/commondata/CMS_Z0J_8TEV/filter.py
+++ nnpdf_data/nnpdf_data/commondata/CMS_Z0J_8TEV/filter.py
@@ -3,14 +3,15 @@ Filter script for CMS_Z0J_8TEV
 '''
 
 import logging
+import os
 
 from filter_utils import Extractor
-import numpy as np
-import yaml
 
 logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
 if __name__ == "__main__":
-    CMS_Z0J_8TEV = Extractor("./metadata.yaml", "PT-Y", mult_factor=1000)
+    CMS_Z0J_8TEV = Extractor(f"{current_dir}/metadata.yaml", "PT-Y", mult_factor=1000)
     CMS_Z0J_8TEV.generate_data(variant='default')
     CMS_Z0J_8TEV.generate_data(variant='sys_10')
--- nnpdf_data/nnpdf_data/commondata/CMS_Z0J_8TEV/filter_utils.py
+++ nnpdf_data/nnpdf_data/commondata/CMS_Z0J_8TEV/filter_utils.py
@@ -1,17 +1,19 @@
+import functools
 import logging
 import os
 
 import numpy as np
-import pandas as pd
 import yaml
 
-from nnpdf_data.filter_utils.utils import prettify_float, symmetrize_errors
+from nnpdf_data.filter_utils.utils import prettify_float
 
 yaml.add_representer(float, prettify_float)
 
-MZ2_low = 81.0**2  # GeV2
-MZ2_high = 101.0**2  # GeV2
-MZ2_mid = (MZ2_low + MZ2_high) * 0.5  # GeV2
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+MZ2_LOW = 81.0**2  # GeV2
+MZ2_HIGH = 101.0**2  # GeV2
+MZ2_MID = (MZ2_LOW + MZ2_HIGH) / 2  # GeV2
 CMSLUMI12 = 2.6  # %
 
 ABS_RAP_BINS = [
@@ -25,23 +27,29 @@ ABS_RAP_BINS = [
 STAT_ART_LABEL = 'art_corr_unc'
 TABLE_TOKEN = 'Table'
 
+
 class Extractor:
     """
     Extracts kinematics, central data, and uncertainties for a given dataset
-
-    Parameters
-    ----------
-    metadata_file: str
-      Path to the metadata file
-    observable: str
-      The name of the observable for which the data is extracted. The name must
-      be listed in the metadata file.
     """
 
     def __init__(self, metadata_file, observable, mult_factor=1.0):
+        """
+        Parameters
+        ----------
+        metadata_file: str
+            Path to the metadata file
+        observable: str
+            The name of the observable for which the data is extracted. The name
+            must be listed in the metadata file.
+        mult_factor: float
+            Multiplication factor to apply to the central data points. This is
+            useful to convert the data in the metadata file to the desired
+            units.
+        """
 
         # Open metadata and select process
-        with open(metadata_file, 'r') as file:
+        with open(metadata_file) as file:
             metadata = yaml.safe_load(file)
             self.metadata = next(
                 (
@@ -52,192 +60,146 @@ class Extractor:
                 None,
             )
             if self.metadata is None:
-                raise Exception(f"{observable} is not listed in the metadata file.")
+                raise ValueError(f"{observable} is not listed in the metadata file.")
 
-        # Initialise dict of tables
-        self.tables = {}
         self.observable = observable
         self.mult_factor = mult_factor
-        self.kin_labels = self.metadata['kinematic_coverage']
-        self.ndata = self.metadata['ndata']
 
-    def __extract_kinematics(self, table: dict):
+    @functools.cache
+    def _retrieve_table(self, table_id):
         """
-        Extracts the kinematic variables of the single differential
-        distribution given a table.
-
-        For each bin, it computes the max, min, and mid value of the transverse
-        momentum of the boson.
+        Implementation of the loading for the table.
 
         Parameters
         ----------
-        table: dict
-          Dictionary containing the bins in the transverse momentum
+        table_id: int
+          Index that specifies the table.
 
         Return
         ------
-        List of bins containing min, max, and mid values for each of the kinematic
-        observables listed in the `kinematic_coverage` of the metadata file.
-
+        The table specified by `table_id`.
         """
-        data = table['independent_variables'][0]
-        label = self.kin_labels
+        with open(f'{current_dir}/rawdata/{TABLE_TOKEN}{table_id}.yaml') as tab:
+            tab_dict = yaml.safe_load(tab)
+        return tab_dict
+
+    def _generate_kinematics(self):
+        """
+        Function that generates the kinematics by taking it from the table with
+        measured cross sections. The kinematics are generated in the format of a
+        list of dictionaries with the following keys: 'pT', 'abs_eta', 'M_Z2'.
+        The values of the keys are dictionaries with the keys 'min', 'mid', and
+        'max'.
+        """
+        logging.info(f"Generating kinematics for CMS_{self.observable}...")
+
+        table = self.metadata["tables"][0]
+        tab_dict = self._retrieve_table(table)
+
+        data = tab_dict['independent_variables'][0]
+        label = self.metadata['kinematic_coverage']
         kinematics = []
         for rap_bin in ABS_RAP_BINS:
-            for bin in data['values']:
-                pT_min = bin['low']
-                pT_max = bin['high']
+            for pT_bin in data['values']:
+                pT_min = pT_bin['low']
+                pT_max = pT_bin['high']
                 abs_eta_low = rap_bin['low']
                 abs_eta_high = rap_bin['high']
                 kin_bin = {
                     label[0]: {'min': pT_min, 'mid': (pT_max + pT_min) / 2, 'max': pT_max},
                     label[1]: {
                         'min': abs_eta_low,
-                        'mid': (abs_eta_low + abs_eta_high) * 0.5,
+                        'mid': (abs_eta_low + abs_eta_high) / 2,
                         'max': abs_eta_high,
                     },
-                    label[2]: {'min': MZ2_low, 'mid': MZ2_mid, 'max': MZ2_high},
+                    label[2]: {'min': MZ2_LOW, 'mid': MZ2_MID, 'max': MZ2_HIGH},
                 }
                 kinematics.append(kin_bin)
-        return kinematics
-
-    def __retrieve_table(self, table_id):
-        """
-        Implementation of the lazy loading for the tables. If the table
-        is loaded for the first time, it is stored into an internal
-        container of the class, so that it will not be loaded each time.
-
-        When called, this functions checks if the table has already been stored
-        and, if that is the case, returns the stored table.
-
-        Parameters
-        ----------
-        table_id: int
-          Index that specifies the table
-
-        Return
-        ------
-        The table specified by `table_id`. If not previously loaded, it is also
-        stored into the internal container for future use.
-        """
-        try:
-            table = self.tables[str(table_id)]
-        except KeyError:
-            logging.debug(
-                f'Table {table_id} has not already been used or stored.' f' Storing the table...'
-            )
-            with open(f'./rawdata/{TABLE_TOKEN}{table_id}.yaml', 'r') as tab:
-                tab_dict = yaml.safe_load(tab)
-                self.tables[str(table_id)] = tab_dict
-                table = tab_dict
-        return table
-
-    def generate_kinematics(self):
-        """
-        Function that generates the kinematics by looping over all the
-        tables specified in the metadata file. The resulting kinematics
-        is then saved to a yaml file. It relies on the method
-        `__extract_kinematics`.
-        """
-        logging.info(f"Generating kinematics for CMS_{self.observable}...")
-
-        # Initialise kinematics list
-        kinematics = []
-        ndata = 0
-        table = self.metadata["tables"][0]
-        tab_dict = self.__retrieve_table(table)
-        kin = self.__extract_kinematics(tab_dict)
-        kinematics = np.concatenate([kinematics, kin])
-        ndata += len(kin)
 
         # Check number of data agrees with metadata
-        try:
-            assert self.metadata['ndata'] is not None
-            assert self.metadata['ndata'] == ndata
-        except AssertionError as e:
-            logging.warning(
-                f"The number of data in the metafile is either wrong or unspecified."
-                f" The correct number is {ndata}. Please, update the metafile."
+        ndata = len(kinematics)
+        if not self.metadata['ndata'] == ndata:
+            raise ValueError(
+                f"Mismatch in 'ndata': expected {self.metadata['ndata']}, but got {ndata}"
             )
-            return
-        return kinematics.tolist()
 
-    def generate_data_and_unc(self, mult_factor=1.0):
+        return kinematics
+
+    def _generate_data_and_unc(self):
         """
-        Same as `generate_kinematics`, but for central data points and uncertainties.
+        Returns a list with central data points and a list with corresponding uncertainties.
         """
         logging.info(f"Generating central data for CMS_{self.observable}...")
-        dat_central = []
-        dat_unc = []
         table = self.metadata['tables'][0]
-        tab_dict = self.__retrieve_table(table)
+        tab_dict = self._retrieve_table(table)
         tab_dict = tab_dict['dependent_variables']
 
-        # Loop over rap bins
+        # Loop over kinematic bins
+        dat_central = []
+        dat_unc = []
         for rap_bin in tab_dict:
             for pt_bin in rap_bin['values']:
-                dat_central.append(pt_bin['value'] * mult_factor)
-                dat_unc.append(pt_bin['errors'][0]['symerror'] * mult_factor)
+                dat_central.append(pt_bin['value'] * self.mult_factor)
+                dat_unc.append(pt_bin['errors'][0]['symerror'] * self.mult_factor)
+
         return dat_central, dat_unc
 
-    def build_covmat(self):
+    def _build_covmat(self):
         '''
         Construct the covarianc matrix from the list of entries provided in HepData.
         '''
         ndata = self.metadata['ndata']
         table_id = self.metadata['tables'][1]
-        with open(f'./rawdata/{TABLE_TOKEN}{table_id}.yaml', 'r') as tab:
-            raw_dict = yaml.load(tab, yaml.Loader)
+        raw_dict = self._retrieve_table(table_id)
+
         matlist = [val['value'] for val in raw_dict['dependent_variables'][0]['values']]
-        covmat = np.zeros((ndata, ndata))
-        for i in range(ndata):
-            for j in range(ndata):
-                covmat[i, j] = matlist[i + ndata * j]  # Col-major
+        covmat = np.array(matlist).reshape(ndata, ndata)
+
+        if not np.allclose(covmat, covmat.T):
+            raise ValueError('Covariance matrix is not symmetric.')
+
         return covmat
 
-    def __build_unc_definitions(self, variant='default'):
+    def _build_unc_definitions(self, variant):
         '''
-        Build the dictionary containing the definitions of the uncertainties
-        to be used in the uncertainty data file.
+        Build the dictionary containing the definitions of the uncertainties to
+        be used in the uncertainty data file.
 
         Parameters
         ----------
         variant: str
             Name of the variant to be implemented.
-        
+
         Return
         ------
-        Dictionary containing the definition of each uncertainties. Each element
-        in the dictionary contains the name of the uncertainty, its definition,
-        the type, and the treatment. The format is the one used in the
-        commondata.
+        Dict of dicts containing the specifications of each of the
+        uncertainties. Each sub-dictionary contains the name of the uncertainty,
+        its description, the type, and the treatment. The format is the one used
+        in the commondata.
         '''
         unc_definitions = {}
 
         # Statistical uncertainties are always the same
-        for idx in range(self.ndata):
+        for idx in range(self.metadata['ndata']):
             unc_definitions[STAT_ART_LABEL + f'_{idx + 1}'] = {
-                'description': f'Artificial uncertainty {idx + 1}',
+                'description': f'Artificial uncertainty {idx + 1}, corresponding to a covmat in eigenvector basis',
                 'treatment': 'ADD',
                 'type': 'CORR',
             }
 
         # Add lumi uncertainty
         unc_definitions['corr_lumi_unc'] = {
-            'description': f'Luminosity uncertainty 2.6%',
+            'description': 'Luminosity uncertainty 2.6%',
             'treatment': 'MULT',
             'type': 'CMSLUMI12',
         }
 
         if variant == 'sys_10':
             unc_definitions['uncorr_mc_unc'] = {
-                'description': f'MC uncertainty',
+                'description': 'MC uncertainty',
                 'treatment': 'MULT',
                 'type': 'UNCORR',
             }
-        elif variant != 'default':
-
-            raise ValueError(f'The variant {variant} is not implemented.')
 
         return unc_definitions
 
@@ -246,65 +208,69 @@ class Extractor:
         Collect central data, kinematics, and uncertainties and combine them
         in the format used in the commondata.
 
-        Paramters
+        Parameters
         ---------
         variant: str
             Name of the dataset variant to generate.
         '''
+
+        # Check if the variant is one of two supported options
+        if variant not in ['default', 'sys_10']:
+            raise ValueError(f'The variant {variant} is not implemented.')
+
         # Get central data and kinematics
-        central_data, _ = self.generate_data_and_unc(self.mult_factor)
-        kinematics = self.generate_kinematics()
+        central_data, _ = self._generate_data_and_unc()
+        kinematics = self._generate_kinematics()
 
         # Uncertainty definitions
-        unc_definitions = self.__build_unc_definitions(variant=variant)
+        unc_definitions = self._build_unc_definitions(variant=variant)
 
-        # Get statistical (artidicial uncertainties)
-        covmat = self.build_covmat()
+        # Get statistical uncertainties. They are represented in a covmat in
+        # eigenvector basis, hence they are called "artificial uncertainties".
+        # The original covmat can be reconstruted as covat = art_stat.T @ art_stat
+        covmat = self._build_covmat()
         eigvals, eigvecs = np.linalg.eig(covmat)
         art_stat = np.sqrt(eigvals) * eigvecs * self.mult_factor
-        sys_artificial = []  # Initialize vector of artificial uncertainties
 
+        unc_vals = []  # Initialize vector of uncertainties
         for data_idx, data in enumerate(central_data):
             unc_dict = {}
-            # Add artificial uncertainties
             for unc_idx, unc_type in enumerate(unc_definitions.keys()):
                 if STAT_ART_LABEL in unc_type:
                     # Add statistical uncertainties
                     unc_dict[unc_type] = float(art_stat[data_idx, unc_idx])
                 elif unc_type == 'corr_lumi_unc':
                     unc_dict[unc_type] = data * CMSLUMI12 * 0.01
-                elif unc_type == 'uncorr_mc_unc' and variant == 'sys_10':
+                elif unc_type == 'uncorr_mc_unc':
                     unc_dict[unc_type] = data * 0.01
                 else:
                     raise ValueError(f'Uncertainty type {unc_type} is not known.')
-            sys_artificial.append(unc_dict)
-        
-        # Local path for yaml files
-        path='./'
+            unc_vals.append(unc_dict)
 
         # Save kinematics into file
         logging.info("Dumping kinematics to file...")
         kinematics_yaml = {'bins': kinematics}
-        with open(path + self.metadata['kinematics']['file'], 'w') as kin_out_file:
-            yaml.dump(kinematics_yaml, kin_out_file, sort_keys=False)
+        kins_file_name = self.metadata['kinematics']['file']
+        with open(current_dir + '/' + kins_file_name, 'w') as file:
+            yaml.dump(kinematics_yaml, file, sort_keys=False)
         logging.info("Done!")
 
         # Save central data into file
-        logging.info("Dumping kinematics to file...")
+        logging.info("Dumping central data to file...")
         dat_central_yaml = {'data_central': central_data}
-        file_name = self.metadata['data_central']
-        with open(path + file_name, 'w') as dat_out_file:
-            yaml.dump(dat_central_yaml, dat_out_file, sort_keys=False)
+        data_file_name = self.metadata['data_central']
+        with open(current_dir + '/' + data_file_name, 'w') as file:
+            yaml.dump(dat_central_yaml, file, sort_keys=False)
         logging.info("Done!")
 
         # Save unertainties
-        logging.info("Dumping kinematics to file...")
-        uncertainties_yaml = {'definitions': unc_definitions, 'bins': sys_artificial}
-        file_name = (
+        logging.info("Dumping uncertainties to file...")
+        uncertainties_yaml = {'definitions': unc_definitions, 'bins': unc_vals}
+        unc_file_name = (
             self.metadata['data_uncertainties'][0]
             if variant == 'default'
             else self.metadata['variants'][variant]['data_uncertainties'][0]
         )
-        with open(path + file_name, 'w') as dat_out_file:
-            yaml.dump(uncertainties_yaml, dat_out_file, sort_keys=False)
+        with open(current_dir + '/' + unc_file_name, 'w') as file:
+            yaml.dump(uncertainties_yaml, file, sort_keys=False)
         logging.info("Done!")
