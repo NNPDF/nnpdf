410faeda22dd680057e62d2e207096103f982821
juacrumar <juacrumar@lairen.eu>
2025-04-24 19:29:59 +0200

    make _generatenn act on single replicas

--- n3fit/src/n3fit/tests/test_modelgen.py
+++ n3fit/src/n3fit/tests/test_modelgen.py
@@ -6,31 +6,40 @@ It checks that both the number of layers and the shape
 of the weights of the layers are what is expected
 """
 
-from n3fit.backends import NN_PREFIX
+from n3fit.backends import NN_PREFIX, Input
 from n3fit.model_gen import _generate_nn
 
 INSIZE = 16
 OUT_SIZES = (4, 3)
-BASIS_SIZE = 3
+BASIS_SIZE = OUT_SIZES[-1]
 
-COMMON_ARGS = {
-    "nodes_in": INSIZE,
-    "nodes": OUT_SIZES,
-    "activations": ["sigmoid", "tanh"],
-    "initializer_name": "glorot_uniform",
-    "replica_seeds": [0],
-    "dropout": 0.0,
-    "regularizer": None,
-    "regularizer_args": {},
-    "last_layer_nodes": BASIS_SIZE,
-}
+
+def _common_generation(architecture, dropout=0.0):
+    """Generate a NN with shared configuration with only
+    some free parameters"""
+    config = {
+        "architecture": architecture,
+        "nodes": OUT_SIZES,
+        "activations": ["sigmoid", "tanh"],
+        "initializer": "glorot_uniform",
+        "seed": 27,
+        "dropout_rate": dropout,
+        "regularizer": None,
+        "regularizer_args": {},
+    }
+    xin = Input(shape=(None, INSIZE), batch_size=1)
+    return _generate_nn(xin, **config)
 
 
 def test_generate_dense_network():
-    nn = _generate_nn("dense", **COMMON_ARGS)
+    nn_w_dropout = _common_generation("dense", dropout=0.4)
+    nn = _common_generation("dense")
 
     # The number of layers should be input layer + len(OUT_SIZES)
     assert len(nn.layers) == len(OUT_SIZES) + 1
+    # And one more with dropout
+    assert len(nn_w_dropout.layers) == len(OUT_SIZES) + 2
+
     # Check that the number of parameters is as expected
     expected_sizes = [(INSIZE, OUT_SIZES[0]), (OUT_SIZES[0]), (*OUT_SIZES,), (OUT_SIZES[1])]
     for weight, esize in zip(nn.weights, expected_sizes):
@@ -38,7 +47,7 @@ def test_generate_dense_network():
 
 
 def test_generate_dense_per_flavour_network():
-    nn = _generate_nn("dense_per_flavour", **COMMON_ARGS).get_layer(f"{NN_PREFIX}_0")
+    nn = _common_generation("dense_per_flavour")
 
     # The number of layers should be input + BASIS_SIZE*len(OUT_SIZES) + concatenate
     assert len(nn.layers) == BASIS_SIZE * len(OUT_SIZES) + 2
