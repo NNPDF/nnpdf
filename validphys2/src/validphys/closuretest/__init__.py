500ce4d8466dd4d6a4d4071b1d2dd9fc69bcc91e
Mark Nestor Costantini <mnc33@cam.ac.uk>
2025-04-25 14:16:09 +0200

    added typing to central_predictions function

--- validphys2/src/validphys/closuretest/__init__.py
+++ validphys2/src/validphys/closuretest/__init__.py
@@ -13,6 +13,22 @@ from validphys.closuretest.multiclosure_pdf_output import *
 from validphys.closuretest.multiclosure_preprocessing import *
 from validphys.closuretest.multiclosure_pseudodata import *
 from validphys.closuretest.inconsistent_closuretest.multiclosure_inconsistent_output import *
-from validphys.closuretest.multiclosure_nsigma_helpers import *
-from validphys.closuretest.multiclosure_nsigma import *
-from validphys.closuretest.multiclosure_nsigma_output import *
+from validphys.closuretest.multiclosure_nsigma_helpers import (
+    fits_datasets_chi2_nsigma_deviation,
+    fits_data,
+    is_weighted,
+    n_fits,
+)
+from validphys.closuretest.multiclosure_nsigma import (
+    set_1,
+    set_2,
+    comp_set_1,
+    set_3,
+    probability_inconsistent,
+)
+from validphys.closuretest.multiclosure_nsigma_output import (
+    plot_all_sets,
+    plot_1_minus_all_sets,
+    plot_probability_inconsistent,
+    plot_probability_consistent,
+)
