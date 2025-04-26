75945697177458f535bb761cd908c01a939764be
t7phy <99052355+t7phy@users.noreply.github.com>
2025-04-26 02:47:59 +0200

    aleph ka impl

---
+++ nnpdf_data/nnpdf_data/commondata/ALEPH_SIA-KA_91P2GEV_EE-KASUM/filter_core.py
@@ -0,0 +1,63 @@
+import yaml
+
+from nnpdf_data.filter_utils.utils import percentage_to_absolute as pta
+from nnpdf_data.filter_utils.utils import prettify_float
+
+yaml.add_representer(float, prettify_float)
+
+
+def magic(table, ndat, var_name):
+    with open(table, 'r') as f:
+        input = yaml.safe_load(f)
+
+    data_central = []
+    kin = []
+    error = []
+
+    values = input['dependent_variables'][0]['values']
+
+    for i in range(ndat):
+        kin_min = input['independent_variables'][0]['values'][i]['low']
+        kin_max = input['independent_variables'][0]['values'][i]['high']
+        if 'value' in input['independent_variables'][0]['values'][i]:
+            kin_mid = input['independent_variables'][0]['values'][i]['value']
+        else:
+            kin_mid = (kin_min + kin_max) / 2
+
+        kin_value = {var_name: {'min': kin_min, 'mid': kin_mid, 'max': kin_max}}
+
+        data_central_value = values[i]['value']
+        error_value = {}
+        error_value['stat'] = values[i]['errors'][0]['symerror']
+        error_value['sys_1'] = values[i]['errors'][1]['symerror']
+        if i < 8:
+            error_value['sys_2'] = pta(5, data_central_value)
+        else:
+            error_value['sys_2'] = pta(3, data_central_value)
+
+        kin.append(kin_value)
+        data_central.append(data_central_value)
+        error.append(error_value)
+
+    error_definition = {}
+    error_definition['stat'] = {
+        'definition': 'statistical uncertainty',
+        'treatment': 'ADD',
+        'type': 'UNCORR',
+    }
+    error_definition['sys_1'] = {
+        'definition': 'systematic uncertainty 1',
+        'treatment': 'MULT',
+        'type': 'CORR',
+    }
+    error_definition['sys_2'] = {
+        'definition': 'systematic uncertainty 2',
+        'treatment': 'MULT',
+        'type': 'CORR',
+    }
+
+    data_central_yaml = {'data_central': data_central}
+    kin_yaml = {'bins': kin}
+    uncertainties_yaml = {'definitions': error_definition, 'bins': error}
+
+    return data_central_yaml, kin_yaml, uncertainties_yaml
