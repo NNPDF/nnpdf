ee1fc0c85e32779c8c8a8f8b38274e6681e81678
t7phy <99052355+t7phy@users.noreply.github.com>
2025-05-01 22:22:29 +0200

    delphi ka impl

---
+++ nnpdf_data/nnpdf_data/commondata/DELPHI_SIA-KA_91P2GEV_EE-KASUM/filter.py
@@ -0,0 +1,35 @@
+from filter_core import magic
+import yaml
+
+table_q = "rawdata/Table20.yaml"
+table_b = "rawdata/Table28.yaml"
+table_uds = "rawdata/Table36.yaml"
+ndat = 23
+var_name = 'ph'
+
+data_q, kin_q, err_q = magic(table_q, ndat, var_name)
+
+with open('data_q.yaml', 'w') as f:
+    yaml.dump(data_q, f, sort_keys=False)
+with open('kinematics_q.yaml', 'w') as f:
+    yaml.dump(kin_q, f, sort_keys=False)
+with open('uncertainties_q.yaml', 'w') as f:
+    yaml.dump(err_q, f, sort_keys=False)
+
+data_b, kin_b, err_b = magic(table_b, ndat, var_name)
+
+with open('data_b.yaml', 'w') as f:
+    yaml.dump(data_b, f, sort_keys=False)
+with open('kinematics_b.yaml', 'w') as f:
+    yaml.dump(kin_b, f, sort_keys=False)
+with open('uncertainties_b.yaml', 'w') as f:
+    yaml.dump(err_b, f, sort_keys=False)
+
+data_uds, kin_uds, err_uds = magic(table_uds, ndat, var_name)
+
+with open('data_uds.yaml', 'w') as f:
+    yaml.dump(data_uds, f, sort_keys=False)
+with open('kinematics_uds.yaml', 'w') as f:
+    yaml.dump(kin_uds, f, sort_keys=False)
+with open('uncertainties_uds.yaml', 'w') as f:
+    yaml.dump(err_uds, f, sort_keys=False)
