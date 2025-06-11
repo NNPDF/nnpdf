7e0176fd884ad0a3ca43709b28f98a2ad4ddf5b9
juacrumar <juacrumar@lairen.eu>
2025-06-11 17:49:03 +0200

    accept PIDs for the exportgrid

--- n3fit/src/evolven3fit/cli.py
+++ n3fit/src/evolven3fit/cli.py
@@ -1,12 +1,8 @@
 from . import evolve, utils
 
 
-def cli_evolven3fit(
-    configuration_folder, force, load, hessian
-):
+def cli_evolven3fit(configuration_folder, force, load, hessian):
     """Evolves the fitted PDFs."""
-    
+
     utils.check_nnfit_folder(configuration_folder)
-    return evolve.evolve_fit(
-        configuration_folder, force, load, hessian
-    )
+    return evolve.evolve_fit(configuration_folder, force, load, hessian)
