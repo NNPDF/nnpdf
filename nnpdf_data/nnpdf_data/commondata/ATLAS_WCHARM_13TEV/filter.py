68e203d5c70ab35d99e7212abc92887b7a7d76e4
ecole41 <ellagcole02@gmail.com>
2025-06-25 13:51:38 +0100

    Added kinematics

--- nnpdf_data/nnpdf_data/commondata/ATLAS_WCHARM_13TEV/filter.py
+++ nnpdf_data/nnpdf_data/commondata/ATLAS_WCHARM_13TEV/filter.py
@@ -4,7 +4,7 @@ file will be created in the `nnpdf_data/commondata/ATLAS_WPWM_7TEV_46FB` directo
 """
 
 import yaml
-from filter_utils import get_data_values
+from filter_utils import get_data_values, get_kinematics
 from nnpdf_data.filter_utils.utils import prettify_float
 
 yaml.add_representer(float, prettify_float)
@@ -17,18 +17,19 @@ def filter_ATLAS_WCHARM_13TEV_data_kinematic():
 
     central_values = get_data_values()
 
-    # kin = get_kinematics()
+    kin = get_kinematics()
 
     data_central_yaml = {"data_central": central_values}
 
-    # kinematics_yaml = {"bins": kin}
+    kinematics_yaml = {"bins": kin}
 
     # write central values and kinematics to yaml file
     with open("data.yaml", "w") as file:
         yaml.dump(data_central_yaml, file, sort_keys=False)
 
-    # with open("kinematics.yaml", "w") as file:
-    #     yaml.dump(kinematics_yaml, file, sort_keys=False)
+    with open("kinematics.yaml", "w") as file:
+        yaml.dump(kinematics_yaml, file, sort_keys=False)
+
 
 if __name__ == "__main__":
     filter_ATLAS_WCHARM_13TEV_data_kinematic()
--- nnpdf_data/nnpdf_data/commondata/ATLAS_WCHARM_13TEV/filter_utils.py
+++ nnpdf_data/nnpdf_data/commondata/ATLAS_WCHARM_13TEV/filter_utils.py
@@ -15,7 +15,7 @@ def get_data_values():
 
     data_central = []
 
-    for i in range(19,23):
+    for i in range(19, 23):
         hepdata_table = f"rawdata/HEPData-ins2628732-v1-Table_{i}.yaml"
 
         with open(hepdata_table, 'r') as file:
@@ -28,3 +28,26 @@ def get_data_values():
             data_central.append(value['value'])
 
     return data_central
+
+
+def get_kinematics():
+    """
+    returns the kinematics in the form of a list of dictionaries.
+    """
+    kin = []
+
+    for i in range(19, 23):
+        hepdata_table = f"rawdata/HEPData-ins2628732-v1-Table_{i}.yaml"
+
+        with open(hepdata_table, 'r') as file:
+            input = yaml.safe_load(file)
+
+        for i, M in enumerate(input["independent_variables"][0]['values']):
+            kin_value = {
+                'abs_eta': {'min': None, 'mid': (0.5 * (M['low'] + M['high'])), 'max': None},
+                'm_W2': {'min': None, 'mid': 6.46046213e03, 'max': None},
+                'sqrts': {'min': None, 'mid': 13000.0, 'max': None},
+            }
+            kin.append(kin_value)
+
+    return kin
