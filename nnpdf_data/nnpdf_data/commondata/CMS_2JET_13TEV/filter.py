53c492ef337f64849f60ad8b725a9f71d72f8786
enocera <emanueleroberto.nocera@unito.it>
2025-08-22 16:47:58 +0200

    Defined the new process required for the dijet 3D distribution; corrected a bug in the rawdata

--- nnpdf_data/nnpdf_data/commondata/CMS_2JET_13TEV/filter.py
+++ nnpdf_data/nnpdf_data/commondata/CMS_2JET_13TEV/filter.py
@@ -54,7 +54,7 @@ def get_all(obs=None):
                 'ymax': {'min': kin_2_values[i]['low'],
                          'mid': 0.5 * (kin_2_values[i]['low'] + kin_2_values[i]['high']),
                          'max': kin_2_values[i]['high']},
-                'mjj': {'min': kin_1_values[i]['low'],
+                'm_jj': {'min': kin_1_values[i]['low'],
                          'mid': 0.5 * (kin_1_values[i]['low'] + kin_1_values[i]['high']),
                          'max': kin_1_values[i]['high']},
                 'sqrts': {'min': None, 'mid': 13000.0, 'max': None},}
@@ -64,9 +64,9 @@ def get_all(obs=None):
                        'mid': 0.5 * (kin_2_values[i]['low'] + kin_2_values[i]['high']),
                        'max': kin_2_values[i]['high']},
                 'ystar': {'min': kin_3_values[i]['low'],
-                           'mid': 0.5 * (kin_3_values[i]['low'] + kin_3_values[i]['high']),
-                           'max': kin_3_values[i]['high']},
-                'mjj': {'min': kin_1_values[i]['low'],
+                          'mid': 0.5 * (kin_3_values[i]['low'] + kin_3_values[i]['high']),
+                          'max': kin_3_values[i]['high']},
+                'm_jj': {'min': kin_1_values[i]['low'],
                          'mid': 0.5 * (kin_1_values[i]['low'] + kin_1_values[i]['high']),
                          'max': kin_1_values[i]['high']},}
         kinematics.append(kin)
