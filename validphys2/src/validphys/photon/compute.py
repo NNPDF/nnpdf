Fix shape in photon computation

validphys2/src/validphys/photon/compute.py
@@ -168,7 +168,9 @@ class Photon:
           lux.InsertInelasticSplitQ([mb_thr, mt_thr])
           lux.PlugStructureFunctions(f2.fxq, fl.fxq, f2lo.fxq)
 
-          photon_qin = np.array(lux.EvaluatePhoton(x, self.q_in**2).total for x in XGRID)
+          photon_qin = np.array(
+            [self.lux[replica].EvaluatePhoton(x, self.q_in**2).total for x in XGRID]
+        )
           photon_qin += self.generate_errors(replica)
 
           # fiatlux computes x * gamma(x)