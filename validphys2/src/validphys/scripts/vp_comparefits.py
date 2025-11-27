8a4fefdd190d62a603891bef74fcf737497bcab1
Jelle Koorn <jkoorn@nikhef.nl>
2025-11-27 15:13:15 +0100

    faster function by Juan

--- validphys2/src/validphys/scripts/vp_comparefits.py
+++ validphys2/src/validphys/scripts/vp_comparefits.py
@@ -1,3 +1,4 @@
+import time
 import logging
 import os
 import sys
@@ -267,8 +268,11 @@ class CompareFitApp(App):
 
 
 def main():
+    time1 = time.time()
     a = CompareFitApp()
     a.main()
+    time2 = time.time()
+    print("TIME TAKEN: ", time2-time1)
 
 
 if __name__ == '__main__':
