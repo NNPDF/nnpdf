4c18730a5c7655528ac427b01ad8698e388d83e8
Andrew Pietraszkiewicz <a.pietraszkiewicz@ed.ac.uk>
2026-09-15 18:07:18 +0100

    Update fitdata.py

--- validphys2/src/validphys/fitdata.py
+++ validphys2/src/validphys/fitdata.py
@@ -463,7 +463,7 @@ def print_different_variants(fits, test_for_same_variants):
             "The following datasets are included in both fits but use different variants:\n\n"
         )
         for ds, (first, second) in test_for_same_variants.items():
-            res.write(f"    * {ds}: {first_fit} uses variant {first}, while {second_fit} uses variant {second}.\n")
+            res.write(f" - {ds}: {first_fit} uses variant {first}, while {second_fit} uses variant {second}.\n")
         res.write('\n')
 
     return res.getvalue()
